e779a51648637deda840079b7877e85dc8b60025
Omar Rizwan <omar.rizwan@gmail.com>
2011-05-16 19:28:10 -0700

    Add radians and degrees exercise and angles util file. Also modify the choice check logic to work even when MathJax transforms the HTML in the choices.

--- utils/math.js
+++ utils/math.js
@@ -2,5 +2,20 @@ $.extend(KhanUtil, {
 	// A simple random number picker
 	rand: function( num ) {
 		return Math.round( num * Math.random() );
+	},
+
+	// Get a random integer between min and max, inclusive
+	randRange: function( min, max ) {
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	},
+	
+	// Returns a random member of the given array
+	randFromArray: function( arr ) {
+		return arr[this.rand(arr.length - 1)];
+	},
+
+	// Round a number to a certain number of decimal places
+	roundTo: function( precision, num ) {
+		return Math.round(num*Math.pow(10,precision))/Math.pow(10,precision);
 	}
 });
