fc4b7a1274d1ccfc8b54d755e2b2923f55fe99b5
Rob Yates <robertyates@gmail.com>
2011-06-07 21:35:34 -0400

    undoing Revert "formatting"
    This reverts commit 3fcb2342ce32beca7332ca82a1887327ba5f77f3.

--- utils/calculus.js
+++ utils/calculus.js
@@ -1,267 +1,267 @@
 jQuery.extend(KhanUtil, {
-    trigFuncs: ["sin", "cos", "tan"],
-    ddxTrigFuncs: {
-        "sin": function(expr) {
-            return ["cos", expr];
-        },
-        "cos": function(expr) {
-            return ["-", ["sin", expr]];
-        },
-        "tan": function(expr) {
-            return ["^", ["sec", expr], 2];
-        }
+	trigFuncs: ["sin", "cos", "tan"],
+	ddxTrigFuncs: {
+		"sin": function( expr ) {
+			return ["cos", expr];
+		},
+		"cos": function( expr ) {
+			return ["-", ["sin", expr]];
+		},
+		"tan": function( expr ) {
+			return ["^", ["sec", expr], 2];
+		}
+	},
+
+	generateFunction: function( variable ) {
+		// Generate a differentiable expression object
+		// {fofx, ddxF, wrongs}
+		// x being the name of the variable we differentiate with respect to
+		// ensure that the function isn"t just 0 as well
+		var f;
+		do {
+			f = new KhanUtil.randFromArray(KhanUtil.CalcFunctions)( variable );
+		} while (f.f === "0");
+		return f;
+	},
+
+	generateSpecialFunction: function( variable ) {
+		// Different emphasis from normal generateFunction
+		// For the special_derivatives exercise
+		var f;
+		do {
+			var r = KhanUtil.rand(10);
+			if ( r < 2 ) { // 20% chance of power rule
+				f = new KhanUtil.CalcFunctions[0]( variable );
+			} else if ( r < 6 ) { // 40% chance of trig
+				f = new KhanUtil.CalcFunctions[1]( variable );
+			} else if ( r < 10 ) { // 40% chance of e^x / ln x
+				f = new KhanUtil.CalcFunctions[3]( variable );
+			}
+		} while (f.f === "0");
+		return f;
+	},
+
+	ddxPolynomial: function( poly ) {
+		var ddxCoefs = [];
+
+		for (var i = poly.maxDegree; i >= poly.minDegree; i--) {
+			ddxCoefs[i - 1] = i * poly.coefs[i];
+		}
+		
+		return new KhanUtil.Polynomial(poly.minDegree - 1, poly.maxDegree - 1, ddxCoefs, poly.variable);
+	},
+
+	// doesn't decrement exponents
+	ddxPolynomialWrong1: function( poly ) {
+		var ddxCoefs = [];
+		
+		for (var i = poly.maxDegree; i >= poly.minDegree; i--) {
+			ddxCoefs[i] = i * poly.coefs[i];
+		}
+		
+		return new KhanUtil.Polynomial( poly.minDegree, poly.maxDegree, ddxCoefs, poly.variable );
+	},
+
+	// increments negative exponents
+	ddxPolynomialWrong2: function( poly ) {
+		var ddxCoefs = [];
+		
+		for (var i = poly.maxDegree; i >= poly.minDegree; i--) {
+			if (i < 0) {
+				ddxCoefs[i + 1] = i * poly.coefs[i];
+			} else {
+				ddxCoefs[i - 1] = i * poly.coefs[i];
+			}
+		}
+		
+		return new KhanUtil.Polynomial( poly.minDegree, poly.maxDegree, ddxCoefs, poly.variable );
+	},
+
+	// reversed signs on all terms
+	ddxPolynomialWrong3: function( poly ) {
+		var ddxCoefs = [];
+		
+		for (var i = poly.maxDegree; i >= poly.minDegree; i--) {
+			ddxCoefs[i - 1] = -1 * i * poly.coefs[i];
+		}
+		
+		return new KhanUtil.Polynomial( poly.minDegree - 1, poly.maxDegree - 1, ddxCoefs, poly.variable );
+	},
+
+	// doesn't multiply coefficients
+	ddxPolynomialWrong4: function( poly ) {
+		var ddxCoefs = [];
+		
+		for (var i = poly.maxDegree; i >= poly.minDegree; i--) {
+			ddxCoefs[i - 1] = poly.coefs[i];
+		}
+		
+		return new KhanUtil.Polynomial( poly.minDegree - 1, poly.maxDegree - 1, ddxCoefs, poly.variable );
+	},
+
+	// original with flipped signs
+	ddxPolynomialWrong5: function( poly ) {
+		var ddxCoefs = [];
+		
+		for (var i = poly.maxDegree; i >= poly.minDegree; i--) {
+			ddxCoefs[i] = poly.coefs[i] * -1;
+		}
+		
+		return new KhanUtil.Polynomial( poly.minDegree, poly.maxDegree, ddxCoefs, poly.variable );
+	},
+
+	funcNotation: function( variable ) {
+		if (!variable) {
+			variable = "x";
+		}
+		
+		var notations = [
+			["y", "\\frac{dy}{d"+variable+"}"],
+			["f("+variable+")", "f'("+variable+")"],
+			["g("+variable+")", "g'("+variable+")"],
+			["y", "y'"],
+			["f("+variable+")", "\\frac{d}{d"+variable+"} f("+variable+")"],
+			["a", "a'"],
+			["a", "\\frac{da}{d"+variable+"}"]
+		];
+		var n_idx = KhanUtil.rand( notations.length );
+		return {
+			f: notations[n_idx][0],
+			ddxF: notations[n_idx][1]
+		};
+	},
+
+    PowerRule: function(minDegree, maxDegree, coefs, variable, funcNotation ){
+			// power rule, polynomials
+			var minDegree = (typeof minDegree == "number") ? minDegree : KhanUtil.randRange(-2, 2);
+			var maxDegree = (typeof maxDegree == "number") ? maxDegree : KhanUtil.randRange(2, 4);
+			var coefs = (typeof coefs == "object") ? coefs : KhanUtil.randCoefs(minDegree, maxDegree);
+			var poly = new KhanUtil.Polynomial(minDegree, maxDegree, coefs, variable);
+
+            this.f = poly;
+
+            this.fText = poly.text();
+
+            this.ddxF = KhanUtil.ddxPolynomial(poly);
+
+            this.ddxFText = this.ddxF.text();
+
+            this.hints = "<p>one</p><p>two</p>";
+
+        	this.wrongs = [
+				KhanUtil.ddxPolynomialWrong1(poly),
+				KhanUtil.ddxPolynomialWrong2(poly),
+				KhanUtil.ddxPolynomialWrong3(poly),
+				KhanUtil.ddxPolynomialWrong4(poly),
+				KhanUtil.ddxPolynomialWrong5(poly)
+			];
+
+            this.wrongsText = jQuery.map(this.wrongs, function( value, index ) {
+				return value.text();
+			});
+
+            this.notation = (typeof funcNotation == "object") ? funcNotation : KhanUtil.funcNotation(variable);
+			return this;
     },
 
-    generateFunction: function(variable) {
-        // Generate a differentiable expression object
-        // {fofx, ddxF, wrongs}
-        // x being the name of the variable we differentiate with respect to
-        // ensure that the function isn"t just 0 as well
-        var f;
-        do {
-            f = new KhanUtil.randFromArray(KhanUtil.CalcFunctions)(variable);
-        } while (f.f === "0");
-        return f;
-    },
+	CalcFunctions: [
+		function( variable ) {
+			// power rule, polynomials
+			var minDegree = KhanUtil.randRange(-2, 2);
+			var maxDegree = KhanUtil.randRange(2, 4);
+            return KhanUtil.PowerRule(minDegree, maxDegree, KhanUtil.randCoefs(minDegree, maxDegree), variable);
+		},
+		function( variable ) {
+			// random trig func
+			var idx = KhanUtil.rand(3); // 0 - 2 in trig funcs
 
-    generateSpecialFunction: function(variable) {
-        // Different emphasis from normal generateFunction
-        // For the special_derivatives exercise
-        var f;
-        do {
-            var r = KhanUtil.rand(10);
-            if (r < 2) { // 20% chance of power rule
-                f = new KhanUtil.CalcFunctions[0](variable);
-            } else if (r < 6) { // 40% chance of trig
-                f = new KhanUtil.CalcFunctions[1](variable);
-            } else if (r < 10) { // 40% chance of e^x / ln x
-                f = new KhanUtil.CalcFunctions[3](variable);
-            }
-        } while (f.f === "0");
-        return f;
-    },
+			this.wrongs = [];
+			
+			this.wrongs[0] = ["sin", variable];
+			this.wrongs[1] = ["csc", variable];
+			this.wrongs[2] = ["sec", variable];
+			this.wrongs[3] = ["tan", variable];
+			this.wrongs[4] = ["-", ["sec", variable]];
+			this.wrongs[5] = ["-", ["cos", variable]];
+			
+			this.f = [ KhanUtil.trigFuncs[idx], variable ];
+			this.ddxF = KhanUtil.ddxTrigFuncs[ KhanUtil.trigFuncs[idx] ](variable);
 
-    ddxPolynomial: function(poly) {
-        var ddxCoefs = [];
+			this.fText = KhanUtil.expr( this.f );
+			this.ddxFText = KhanUtil.expr( this.ddxF );
+			
+			this.wrongsText = jQuery.map(this.wrongs, function( value, index ) {
+				return KhanUtil.expr( value );
+			});
 
-        for (var i = poly.maxDegree; i >= poly.minDegree; i--) {
-            ddxCoefs[i - 1] = i * poly.coefs[i];
-        }
+			return this;
+		},
 
-        return new KhanUtil.Polynomial(poly.minDegree - 1, poly.maxDegree - 1, ddxCoefs, poly.variable);
-    },
+		function( variable ) {
+			// basic x^power, simplified version of polynomials in [0]
+			// kept KhanUtil around mainly for easy wrong answer generation
+			var maxDegree = KhanUtil.randRange(2, 6);
+			var minDegree = maxDegree;
+			
+			var coefs = [];
+			coefs[maxDegree] = 1;
 
-    // doesn't decrement exponents
-    ddxPolynomialWrong1: function(poly) {
-        var ddxCoefs = [];
+			var poly = new KhanUtil.Polynomial(minDegree, maxDegree, coefs, variable);
+			
+			this.f = poly.expr();
+			this.ddxF = KhanUtil.ddxPolynomial(poly).expr();
+			
+			this.wrongs = [
+				KhanUtil.ddxPolynomialWrong1(poly).expr(),
+				KhanUtil.ddxPolynomialWrong2(poly).expr(),
+				KhanUtil.ddxPolynomialWrong3(poly).expr(),
+				KhanUtil.ddxPolynomialWrong4(poly).expr(),
+				KhanUtil.ddxPolynomialWrong5(poly).expr()
+			];
 
-        for (var i = poly.maxDegree; i >= poly.minDegree; i--) {
-            ddxCoefs[i] = i * poly.coefs[i];
-        }
+			this.fText = KhanUtil.expr( this.f );
+			this.ddxFText = KhanUtil.expr( this.ddxF );
+			
+			this.wrongsText = jQuery.map(this.wrongs, function( value, index ) {
+				return KhanUtil.expr( value );
+			});
 
-        return new KhanUtil.Polynomial(poly.minDegree, poly.maxDegree, ddxCoefs, poly.variable);
-    },
+			return this;
+		},
 
-    // increments negative exponents
-    ddxPolynomialWrong2: function(poly) {
-        var ddxCoefs = [];
+		function( variable ) {
+			// ln x and e^x, combined in one because these should not be too likely
+			this.wrongs = [];
 
-        for (var i = poly.maxDegree; i >= poly.minDegree; i--) {
-            if (i < 0) {
-                ddxCoefs[i + 1] = i * poly.coefs[i];
-            } else {
-                ddxCoefs[i - 1] = i * poly.coefs[i];
-            }
-        }
+			if (KhanUtil.rand(2)) {
+				this.wrongs[0] = ["frac", 1, ["ln", variable]];
+				this.wrongs[1] = ["^", "e", variable];
+				this.wrongs[2] = ["frac", 1, ["^", "e", variable]];
+				this.wrongs[3] = ["ln", variable];
+				this.wrongs[4] = ["frac", 1, ["^", variable, 2]];
+				this.wrongs[5] = variable;
 
-        return new KhanUtil.Polynomial(poly.minDegree, poly.maxDegree, ddxCoefs, poly.variable);
-    },
+				this.f = ["ln", variable];
+				this.ddxF = ["frac", 1, variable];
+			} else {
+				this.wrongs[0] = ["*", variable, ["^", "e", ["-", variable, 1]]];
+				this.wrongs[1] = ["frac", 1, variable];
+				this.wrongs[2] = ["*", variable, ["^", "e", variable]];
+				this.wrongs[3] = ["^", "e", ["-", variable, 1]];
+				this.wrongs[4] = ["^", ["-", "e", variable], variable];
+				this.wrongs[5] = ["frac", "e", variable];
+				
+				this.f = ["^", "e", variable];
+				this.ddxF = ["^", "e", variable];
+			}
 
-    // reversed signs on all terms
-    ddxPolynomialWrong3: function(poly) {
-        var ddxCoefs = [];
-
-        for (var i = poly.maxDegree; i >= poly.minDegree; i--) {
-            ddxCoefs[i - 1] = -1 * i * poly.coefs[i];
-        }
-
-        return new KhanUtil.Polynomial(poly.minDegree - 1, poly.maxDegree - 1, ddxCoefs, poly.variable);
-    },
-
-    // doesn't multiply coefficients
-    ddxPolynomialWrong4: function(poly) {
-        var ddxCoefs = [];
-
-        for (var i = poly.maxDegree; i >= poly.minDegree; i--) {
-            ddxCoefs[i - 1] = poly.coefs[i];
-        }
-
-        return new KhanUtil.Polynomial(poly.minDegree - 1, poly.maxDegree - 1, ddxCoefs, poly.variable);
-    },
-
-    // original with flipped signs
-    ddxPolynomialWrong5: function(poly) {
-        var ddxCoefs = [];
-
-        for (var i = poly.maxDegree; i >= poly.minDegree; i--) {
-            ddxCoefs[i] = poly.coefs[i] * -1;
-        }
-
-        return new KhanUtil.Polynomial(poly.minDegree, poly.maxDegree, ddxCoefs, poly.variable);
-    },
-
-    funcNotation: function(variable) {
-        if (!variable) {
-            variable = "x";
-        }
-
-        var notations = [
-            ["y", "\\frac{dy}{d" + variable + "}"],
-            ["f(" + variable + ")", "f'(" + variable + ")"],
-            ["g(" + variable + ")", "g'(" + variable + ")"],
-            ["y", "y'"],
-            ["f(" + variable + ")", "\\frac{d}{d" + variable + "} f(" + variable + ")"],
-            ["a", "a'"],
-            ["a", "\\frac{da}{d" + variable + "}"]
-        ];
-        var n_idx = KhanUtil.rand(notations.length);
-        return {
-            f: notations[n_idx][0],
-            ddxF: notations[n_idx][1]
-        };
-    },
-
-    PowerRule: function(minDegree, maxDegree, coefs, variable, funcNotation) {
-        // power rule, polynomials
-        var minDegree = (typeof minDegree == "number") ? minDegree : KhanUtil.randRange(-2, 2);
-        var maxDegree = (typeof maxDegree == "number") ? maxDegree : KhanUtil.randRange(2, 4);
-        var coefs = (typeof coefs == "object") ? coefs : KhanUtil.randCoefs(minDegree, maxDegree);
-        var poly = new KhanUtil.Polynomial(minDegree, maxDegree, coefs, variable);
-
-        this.f = poly;
-
-        this.fText = poly.text();
-
-        this.ddxF = KhanUtil.ddxPolynomial(poly);
-
-        this.ddxFText = this.ddxF.text();
-
-        this.hints = "<p>one</p><p>two</p>";
-
-        this.wrongs = [
-            KhanUtil.ddxPolynomialWrong1(poly),
-            KhanUtil.ddxPolynomialWrong2(poly),
-            KhanUtil.ddxPolynomialWrong3(poly),
-            KhanUtil.ddxPolynomialWrong4(poly),
-            KhanUtil.ddxPolynomialWrong5(poly)
-        ];
-
-        this.wrongsText = jQuery.map(this.wrongs, function(value, index) {
-            return value.text();
-        });
-
-        this.notation = (typeof funcNotation == "object") ? funcNotation : KhanUtil.funcNotation(variable);
-        return this;
-    },
-
-    CalcFunctions: [
-            function(variable) {
-                // power rule, polynomials
-                var minDegree = KhanUtil.randRange(-2, 2);
-                var maxDegree = KhanUtil.randRange(2, 4);
-                return KhanUtil.PowerRule(minDegree, maxDegree, KhanUtil.randCoefs(minDegree, maxDegree), variable);
-            },
-            function(variable) {
-                // random trig func
-                var idx = KhanUtil.rand(3); // 0 - 2 in trig funcs
-
-                this.wrongs = [];
-
-                this.wrongs[0] = ["sin", variable];
-                this.wrongs[1] = ["csc", variable];
-                this.wrongs[2] = ["sec", variable];
-                this.wrongs[3] = ["tan", variable];
-                this.wrongs[4] = ["-", ["sec", variable]];
-                this.wrongs[5] = ["-", ["cos", variable]];
-
-                this.f = [ KhanUtil.trigFuncs[idx], variable ];
-                this.ddxF = KhanUtil.ddxTrigFuncs[ KhanUtil.trigFuncs[idx] ](variable);
-
-                this.fText = KhanUtil.expr(this.f);
-                this.ddxFText = KhanUtil.expr(this.ddxF);
-
-                this.wrongsText = jQuery.map(this.wrongs, function(value, index) {
-                    return KhanUtil.expr(value);
-                });
-
-                return this;
-            },
-
-            function(variable) {
-                // basic x^power, simplified version of polynomials in [0]
-                // kept KhanUtil around mainly for easy wrong answer generation
-                var maxDegree = KhanUtil.randRange(2, 6);
-                var minDegree = maxDegree;
-
-                var coefs = [];
-                coefs[maxDegree] = 1;
-
-                var poly = new KhanUtil.Polynomial(minDegree, maxDegree, coefs, variable);
-
-                this.f = poly.expr();
-                this.ddxF = KhanUtil.ddxPolynomial(poly).expr();
-
-                this.wrongs = [
-                    KhanUtil.ddxPolynomialWrong1(poly).expr(),
-                    KhanUtil.ddxPolynomialWrong2(poly).expr(),
-                    KhanUtil.ddxPolynomialWrong3(poly).expr(),
-                    KhanUtil.ddxPolynomialWrong4(poly).expr(),
-                    KhanUtil.ddxPolynomialWrong5(poly).expr()
-                ];
-
-                this.fText = KhanUtil.expr(this.f);
-                this.ddxFText = KhanUtil.expr(this.ddxF);
-
-                this.wrongsText = jQuery.map(this.wrongs, function(value, index) {
-                    return KhanUtil.expr(value);
-                });
-
-                return this;
-            },
-
-            function(variable) {
-                // ln x and e^x, combined in one because these should not be too likely
-                this.wrongs = [];
-
-                if (KhanUtil.rand(2)) {
-                    this.wrongs[0] = ["frac", 1, ["ln", variable]];
-                    this.wrongs[1] = ["^", "e", variable];
-                    this.wrongs[2] = ["frac", 1, ["^", "e", variable]];
-                    this.wrongs[3] = ["ln", variable];
-                    this.wrongs[4] = ["frac", 1, ["^", variable, 2]];
-                    this.wrongs[5] = variable;
-
-                    this.f = ["ln", variable];
-                    this.ddxF = ["frac", 1, variable];
-                } else {
-                    this.wrongs[0] = ["*", variable, ["^", "e", ["-", variable, 1]]];
-                    this.wrongs[1] = ["frac", 1, variable];
-                    this.wrongs[2] = ["*", variable, ["^", "e", variable]];
-                    this.wrongs[3] = ["^", "e", ["-", variable, 1]];
-                    this.wrongs[4] = ["^", ["-", "e", variable], variable];
-                    this.wrongs[5] = ["frac", "e", variable];
-
-                    this.f = ["^", "e", variable];
-                    this.ddxF = ["^", "e", variable];
-                }
-
-                this.fText = KhanUtil.expr(this.f);
-                this.ddxFText = KhanUtil.expr(this.ddxF);
-
-                this.wrongsText = jQuery.map(this.wrongs, function(value, index) {
-                    return KhanUtil.expr(value);
-                });
-
-                return this;
-            } ]
+			this.fText = KhanUtil.expr( this.f );
+			this.ddxFText = KhanUtil.expr( this.ddxF );
+			
+			this.wrongsText = jQuery.map(this.wrongs, function( value, index ) {
+				return KhanUtil.expr( value );
+			});
+			
+			return this;
+		} ]
 });
--- utils/polynomials.js
+++ utils/polynomials.js
@@ -1,151 +1,152 @@
 jQuery.extend(KhanUtil, {
-    Polynomial: function(minDegree, maxDegree, coefs, variable, name) {
-        var term = function(coef, vari, degree) {
-            // sort of a weird error behavior
-            if (typeof coef === "undefined") {
-                coef = 1;
-            } else if (coef == 0) {
-                return null;
-            }
+	Polynomial: function( minDegree, maxDegree, coefs, variable, name ) {
+		var term = function( coef, vari, degree ) {
+			// sort of a weird error behavior
+			if ( typeof coef === "undefined" ) {
+				coef = 1;
+			} else if ( coef == 0 ) {
+				return null;
+			}
 
-            if (degree == 0) {
-                return coef;
-            } else if (degree == 1) {
-                return ["*", coef, vari];
-            } else {
-                return ["*", coef, ["^", vari, degree]];
-            }
-        };
+			if ( degree == 0 ) {
+				return coef;
+			} else if ( degree == 1 ) {
+				return ["*", coef, vari];
+			} else {
+				return ["*", coef, ["^", vari, degree]];
+			}
+		};
 
-        this.minDegree = minDegree;
-        this.maxDegree = maxDegree;
-        if (!coefs) {
-            this.coefs = KhanUtil.randCoefs(minDegree, maxDegree);
-        } else {
-            this.coefs = coefs;
-        }
-        this.variable = (typeof variable !== "undefined") ? variable : "x";
+		this.minDegree = minDegree;
+		this.maxDegree = maxDegree;
+		if (!coefs) {
+			this.coefs = KhanUtil.randCoefs( minDegree, maxDegree );
+		} else {
+			this.coefs = coefs;
+		}
+		this.variable = (typeof variable !== "undefined") ? variable : "x";
 
-        if (name) {
-            this.name = name;
-        } else {
-            this.name = "f";
-        }
+		if ( name ) {
+			this.name = name;
+		} else {
+			this.name = "f";
+		}
 
-        this.expr = function(vari) {
-            if (typeof vari === "undefined") {
-                vari = this.variable;
-            }
+		this.expr = function( vari ) {
+			if ( typeof vari === "undefined" ) {
+				vari = this.variable;
+			}
+			
+			var expr = ["+"];
 
-            var expr = ["+"];
+			for ( var i = this.maxDegree; i >= this.minDegree; i-- ) {
+				var theTerm = term( this.coefs[i], vari, i );
 
-            for (var i = this.maxDegree; i >= this.minDegree; i--) {
-                var theTerm = term(this.coefs[i], vari, i);
+				if ( theTerm != null ) {
+					expr.push( theTerm );
+				}
+			}
 
-                if (theTerm != null) {
-                    expr.push(theTerm);
-                }
-            }
+			return expr;
+		};
 
-            return expr;
-        };
+		this.text = function() {
+			return KhanUtil.expr( this.expr( this.variable ) );
+		};
 
-        this.text = function() {
-            return KhanUtil.expr(this.expr(this.variable));
-        };
+		this.toString = this.text;
 
-        this.toString = this.text;
+		this.hintEvalOf = function( val ) {
+			return KhanUtil.expr( this.expr( val ) );
+		};
 
-        this.hintEvalOf = function(val) {
-            return KhanUtil.expr(this.expr(val));
-        };
+		this.evalOf = function( val ) {
+			return KhanUtil.expr( this.expr( val ), true );
+		};
 
-        this.evalOf = function(val) {
-            return KhanUtil.expr(this.expr(val), true);
-        };
+		this.hint = function( val ) {
+			var hint = "<p><code>" + this.name+"("+val+") = " + this.hintEvalOf( val ) + "</code></p>";
+			hint += "<p><code>" + this.name+"("+val+") = " +  + this.evalOf( val ) + "</code></p>";
 
-        this.hint = function(val) {
-            var hint = "<p><code>" + this.name + "(" + val + ") = " + this.hintEvalOf(val) + "</code></p>";
-            hint += "<p><code>" + this.name + "(" + val + ") = " + + this.evalOf(val) + "</code></p>";
+			return hint;
+		};
+		
+		return this;
+	},
 
-            return hint;
-        };
+	CompositePolynomial: function( minDegree, maxDegree, coefs, variable, name,
+								   composed, composedCoef ) {
+		var base = new KhanUtil.Polynomial( minDegree, maxDegree, coefs, variable, name );
 
-        return this;
-    },
+		jQuery.extend(this, base);
 
-    CompositePolynomial: function(minDegree, maxDegree, coefs, variable, name, composed, composedCoef) {
-        var base = new KhanUtil.Polynomial(minDegree, maxDegree, coefs, variable, name);
+		if ( !composedCoef ) {
+			composedCoef = KhanUtil.randRangeNonZero( -5, 5 );
+		}
+		var composedFunc = composed.name+"("+this.variable+")";
 
-        jQuery.extend(this, base);
+		var tackOn = function( expr, tack ) {
+			expr = jQuery.merge( [], expr );
 
-        if (!composedCoef) {
-            composedCoef = KhanUtil.randRangeNonZero(-5, 5);
-        }
-        var composedFunc = composed.name + "(" + this.variable + ")";
+			if ( expr[0] === "+" ) {
+				expr.push( tack );
+			} else {
+				expr = [ "+", expr, tack ];
+			}
 
-        var tackOn = function(expr, tack) {
-            expr = jQuery.merge([], expr);
+			return expr;
+		}
+		
+		this.expr = function( vari ) {
+			return tackOn( base.expr( vari ), ["*", composedCoef, composedFunc] );
+		};
 
-            if (expr[0] === "+") {
-                expr.push(tack);
-            } else {
-                expr = [ "+", expr, tack ];
-            }
+		this.text = function() {
+			return KhanUtil.expr( this.expr( this.variable ) ); 
+		};
 
-            return expr;
-        }
+		this.toString = this.text;
 
-        this.expr = function(vari) {
-            return tackOn(base.expr(vari), ["*", composedCoef, composedFunc]);
-        };
+		this.hintEvalOf = function( val, evalInner ) {
+			if ( evalInner ) {
+				return KhanUtil.expr( tackOn( base.expr( val ), ["*", composedCoef, composed.evalOf( val )] ) );
+			} else {
+				return KhanUtil.expr( tackOn( base.expr( val ), ["*", composedCoef, composed.name+"("+val+")"] ) );
+			}
+		};
 
-        this.text = function() {
-            return KhanUtil.expr(this.expr(this.variable));
-        };
+		this.evalOf = function( val ) {
+			return base.evalOf( val ) + composedCoef * composed.evalOf( val );
+		};
 
-        this.toString = this.text;
+		this.hint = function( val ) {
+			var hint = "<p><code>" + this.name+"("+val+") = " + this.hintEvalOf(val) + "</code></p>";
 
-        this.hintEvalOf = function(val, evalInner) {
-            if (evalInner) {
-                return KhanUtil.expr(tackOn(base.expr(val), ["*", composedCoef, composed.evalOf(val)]));
-            } else {
-                return KhanUtil.expr(tackOn(base.expr(val), ["*", composedCoef, composed.name + "(" + val + ")"]));
-            }
-        };
+			var composedFuncWithVal = composed.name+"("+val+")";
+			
+			hint += "<p>To solve for the value of <code>" + this.name + "</code>,"
+				+ "we need to solve for the value of <code>"
+				+ composedFuncWithVal + "</code>.</p>";
 
-        this.evalOf = function(val) {
-            return base.evalOf(val) + composedCoef * composed.evalOf(val);
-        };
+			hint += composed.hint( val );
 
-        this.hint = function(val) {
-            var hint = "<p><code>" + this.name + "(" + val + ") = " + this.hintEvalOf(val) + "</code></p>";
+			hint += "<p>Okay, so <code>" + composedFuncWithVal+" = " + composed.evalOf(val) + "</code>.</p>";
 
-            var composedFuncWithVal = composed.name + "(" + val + ")";
+			hint += "<p>That means <code>" + this.name+"("+val+") = " + this.hintEvalOf(val, true) + "</code></p>";
 
-            hint += "<p>To solve for the value of <code>" + this.name + "</code>,"
-                    + "we need to solve for the value of <code>"
-                    + composedFuncWithVal + "</code>.</p>";
+			hint += "<p><code>" + this.name+"("+val+") = " + this.evalOf( val ) + "</code></p>";
 
-            hint += composed.hint(val);
+			return hint;
+		};
 
-            hint += "<p>Okay, so <code>" + composedFuncWithVal + " = " + composed.evalOf(val) + "</code>.</p>";
+		return this;
+	},
 
-            hint += "<p>That means <code>" + this.name + "(" + val + ") = " + this.hintEvalOf(val, true) + "</code></p>";
-
-            hint += "<p><code>" + this.name + "(" + val + ") = " + this.evalOf(val) + "</code></p>";
-
-            return hint;
-        };
-
-        return this;
-    },
-
-    randCoefs: function(minDegree, maxDegree) {
-        var coefs = [];
-        for (var i = maxDegree; i >= minDegree; i--) {
-            coefs[i] = KhanUtil.randRange(-7, 7);
-        }
-        return coefs;
-    }
+	randCoefs: function( minDegree, maxDegree ) {
+		var coefs = [];
+		for ( var i = maxDegree; i >= minDegree; i-- ) {
+			coefs[i] = KhanUtil.randRange( -7, 7 );
+		}
+		return coefs;
+	}
 });
