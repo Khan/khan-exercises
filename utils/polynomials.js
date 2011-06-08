3fcb2342ce32beca7332ca82a1887327ba5f77f3
Rob Yates <robertyates@gmail.com>
2011-06-07 21:29:09 -0400

    formatting

--- utils/polynomials.js
+++ utils/polynomials.js
@@ -1,152 +1,151 @@
 jQuery.extend(KhanUtil, {
-	Polynomial: function( minDegree, maxDegree, coefs, variable, name ) {
-		var term = function( coef, vari, degree ) {
-			// sort of a weird error behavior
-			if ( typeof coef === "undefined" ) {
-				coef = 1;
-			} else if ( coef == 0 ) {
-				return null;
-			}
+    Polynomial: function(minDegree, maxDegree, coefs, variable, name) {
+        var term = function(coef, vari, degree) {
+            // sort of a weird error behavior
+            if (typeof coef === "undefined") {
+                coef = 1;
+            } else if (coef == 0) {
+                return null;
+            }
 
-			if ( degree == 0 ) {
-				return coef;
-			} else if ( degree == 1 ) {
-				return ["*", coef, vari];
-			} else {
-				return ["*", coef, ["^", vari, degree]];
-			}
-		};
+            if (degree == 0) {
+                return coef;
+            } else if (degree == 1) {
+                return ["*", coef, vari];
+            } else {
+                return ["*", coef, ["^", vari, degree]];
+            }
+        };
 
-		this.minDegree = minDegree;
-		this.maxDegree = maxDegree;
-		if (!coefs) {
-			this.coefs = KhanUtil.randCoefs( minDegree, maxDegree );
-		} else {
-			this.coefs = coefs;
-		}
-		this.variable = (typeof variable !== "undefined") ? variable : "x";
+        this.minDegree = minDegree;
+        this.maxDegree = maxDegree;
+        if (!coefs) {
+            this.coefs = KhanUtil.randCoefs(minDegree, maxDegree);
+        } else {
+            this.coefs = coefs;
+        }
+        this.variable = (typeof variable !== "undefined") ? variable : "x";
 
-		if ( name ) {
-			this.name = name;
-		} else {
-			this.name = "f";
-		}
+        if (name) {
+            this.name = name;
+        } else {
+            this.name = "f";
+        }
 
-		this.expr = function( vari ) {
-			if ( typeof vari === "undefined" ) {
-				vari = this.variable;
-			}
-			
-			var expr = ["+"];
+        this.expr = function(vari) {
+            if (typeof vari === "undefined") {
+                vari = this.variable;
+            }
 
-			for ( var i = this.maxDegree; i >= this.minDegree; i-- ) {
-				var theTerm = term( this.coefs[i], vari, i );
+            var expr = ["+"];
 
-				if ( theTerm != null ) {
-					expr.push( theTerm );
-				}
-			}
+            for (var i = this.maxDegree; i >= this.minDegree; i--) {
+                var theTerm = term(this.coefs[i], vari, i);
 
-			return expr;
-		};
+                if (theTerm != null) {
+                    expr.push(theTerm);
+                }
+            }
 
-		this.text = function() {
-			return KhanUtil.expr( this.expr( this.variable ) );
-		};
+            return expr;
+        };
 
-		this.toString = this.text;
+        this.text = function() {
+            return KhanUtil.expr(this.expr(this.variable));
+        };
 
-		this.hintEvalOf = function( val ) {
-			return KhanUtil.expr( this.expr( val ) );
-		};
+        this.toString = this.text;
 
-		this.evalOf = function( val ) {
-			return KhanUtil.expr( this.expr( val ), true );
-		};
+        this.hintEvalOf = function(val) {
+            return KhanUtil.expr(this.expr(val));
+        };
 
-		this.hint = function( val ) {
-			var hint = "<p><code>" + this.name+"("+val+") = " + this.hintEvalOf( val ) + "</code></p>";
-			hint += "<p><code>" + this.name+"("+val+") = " +  + this.evalOf( val ) + "</code></p>";
+        this.evalOf = function(val) {
+            return KhanUtil.expr(this.expr(val), true);
+        };
 
-			return hint;
-		};
-		
-		return this;
-	},
+        this.hint = function(val) {
+            var hint = "<p><code>" + this.name + "(" + val + ") = " + this.hintEvalOf(val) + "</code></p>";
+            hint += "<p><code>" + this.name + "(" + val + ") = " + + this.evalOf(val) + "</code></p>";
 
-	CompositePolynomial: function( minDegree, maxDegree, coefs, variable, name,
-								   composed, composedCoef ) {
-		var base = new KhanUtil.Polynomial( minDegree, maxDegree, coefs, variable, name );
+            return hint;
+        };
 
-		jQuery.extend(this, base);
+        return this;
+    },
 
-		if ( !composedCoef ) {
-			composedCoef = KhanUtil.randRangeNonZero( -5, 5 );
-		}
-		var composedFunc = composed.name+"("+this.variable+")";
+    CompositePolynomial: function(minDegree, maxDegree, coefs, variable, name, composed, composedCoef) {
+        var base = new KhanUtil.Polynomial(minDegree, maxDegree, coefs, variable, name);
 
-		var tackOn = function( expr, tack ) {
-			expr = jQuery.merge( [], expr );
+        jQuery.extend(this, base);
 
-			if ( expr[0] === "+" ) {
-				expr.push( tack );
-			} else {
-				expr = [ "+", expr, tack ];
-			}
+        if (!composedCoef) {
+            composedCoef = KhanUtil.randRangeNonZero(-5, 5);
+        }
+        var composedFunc = composed.name + "(" + this.variable + ")";
 
-			return expr;
-		}
-		
-		this.expr = function( vari ) {
-			return tackOn( base.expr( vari ), ["*", composedCoef, composedFunc] );
-		};
+        var tackOn = function(expr, tack) {
+            expr = jQuery.merge([], expr);
 
-		this.text = function() {
-			return KhanUtil.expr( this.expr( this.variable ) ); 
-		};
+            if (expr[0] === "+") {
+                expr.push(tack);
+            } else {
+                expr = [ "+", expr, tack ];
+            }
 
-		this.toString = this.text;
+            return expr;
+        }
 
-		this.hintEvalOf = function( val, evalInner ) {
-			if ( evalInner ) {
-				return KhanUtil.expr( tackOn( base.expr( val ), ["*", composedCoef, composed.evalOf( val )] ) );
-			} else {
-				return KhanUtil.expr( tackOn( base.expr( val ), ["*", composedCoef, composed.name+"("+val+")"] ) );
-			}
-		};
+        this.expr = function(vari) {
+            return tackOn(base.expr(vari), ["*", composedCoef, composedFunc]);
+        };
 
-		this.evalOf = function( val ) {
-			return base.evalOf( val ) + composedCoef * composed.evalOf( val );
-		};
+        this.text = function() {
+            return KhanUtil.expr(this.expr(this.variable));
+        };
 
-		this.hint = function( val ) {
-			var hint = "<p><code>" + this.name+"("+val+") = " + this.hintEvalOf(val) + "</code></p>";
+        this.toString = this.text;
 
-			var composedFuncWithVal = composed.name+"("+val+")";
-			
-			hint += "<p>To solve for the value of <code>" + this.name + "</code>,"
-				+ "we need to solve for the value of <code>"
-				+ composedFuncWithVal + "</code>.</p>";
+        this.hintEvalOf = function(val, evalInner) {
+            if (evalInner) {
+                return KhanUtil.expr(tackOn(base.expr(val), ["*", composedCoef, composed.evalOf(val)]));
+            } else {
+                return KhanUtil.expr(tackOn(base.expr(val), ["*", composedCoef, composed.name + "(" + val + ")"]));
+            }
+        };
 
-			hint += composed.hint( val );
+        this.evalOf = function(val) {
+            return base.evalOf(val) + composedCoef * composed.evalOf(val);
+        };
 
-			hint += "<p>Okay, so <code>" + composedFuncWithVal+" = " + composed.evalOf(val) + "</code>.</p>";
+        this.hint = function(val) {
+            var hint = "<p><code>" + this.name + "(" + val + ") = " + this.hintEvalOf(val) + "</code></p>";
 
-			hint += "<p>That means <code>" + this.name+"("+val+") = " + this.hintEvalOf(val, true) + "</code></p>";
+            var composedFuncWithVal = composed.name + "(" + val + ")";
 
-			hint += "<p><code>" + this.name+"("+val+") = " + this.evalOf( val ) + "</code></p>";
+            hint += "<p>To solve for the value of <code>" + this.name + "</code>,"
+                    + "we need to solve for the value of <code>"
+                    + composedFuncWithVal + "</code>.</p>";
 
-			return hint;
-		};
+            hint += composed.hint(val);
 
-		return this;
-	},
+            hint += "<p>Okay, so <code>" + composedFuncWithVal + " = " + composed.evalOf(val) + "</code>.</p>";
 
-	randCoefs: function( minDegree, maxDegree ) {
-		var coefs = [];
-		for ( var i = maxDegree; i >= minDegree; i-- ) {
-			coefs[i] = KhanUtil.randRange( -7, 7 );
-		}
-		return coefs;
-	}
+            hint += "<p>That means <code>" + this.name + "(" + val + ") = " + this.hintEvalOf(val, true) + "</code></p>";
+
+            hint += "<p><code>" + this.name + "(" + val + ") = " + this.evalOf(val) + "</code></p>";
+
+            return hint;
+        };
+
+        return this;
+    },
+
+    randCoefs: function(minDegree, maxDegree) {
+        var coefs = [];
+        for (var i = maxDegree; i >= minDegree; i--) {
+            coefs[i] = KhanUtil.randRange(-7, 7);
+        }
+        return coefs;
+    }
 });
