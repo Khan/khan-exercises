403ab7763a74403eb236f647cd6ad0cd75da33d6
John Resig <jeresig@gmail.com>
2011-06-15 17:41:18 -0700

    Fix issues with KhanUtil.random() not existing for tests.

--- khan-exercise.js
+++ khan-exercise.js
@@ -88,9 +88,25 @@ var Khan = {
 
 		});
 	},
+	
+	randomSeed: parseFloat( Khan.query.seed ) || ( new Date().getTime() & 0xffffffff ),
 
 	// Populate this with modules
-	Util: {},
+	Util: {
+		// http://burtleburtle.net/bob/hash/integer.html
+		// This is also used as a PRNG in the V8 benchmark suite
+		random: function() {
+			// Robert Jenkins' 32 bit integer hash function.
+			var seed = Khan.randomSeed;
+			seed = ( ( seed + 0x7ed55d16 ) + ( seed << 12 ) ) & 0xffffffff;
+			seed = ( ( seed ^ 0xc761c23c ) ^ ( seed >>> 19 ) ) & 0xffffffff;
+			seed = ( ( seed + 0x165667b1 ) + ( seed << 5 ) ) & 0xffffffff;
+			seed = ( ( seed + 0xd3a2646c ) ^ ( seed << 9 ) ) & 0xffffffff;
+			seed = ( ( seed + 0xfd7046c5 ) + ( seed << 3 ) ) & 0xffffffff;
+			seed = ( ( seed ^ 0xb55a4f09 ) ^ ( seed >>> 16 ) ) & 0xffffffff;
+			return ( Khan.randomSeed = ( seed & 0xfffffff ) ) / 0x10000000;
+		}
+	},
 
 	// How many problems are we doing? (For the fair shuffle bag.)
 	problemCount: 10,
@@ -717,20 +733,3 @@ Khan.loadScripts( [ { src: "https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/j
 		return jQuery.contains( elem.ownerDocument.documentElement, elem );
 	};
 });
-
-// http://burtleburtle.net/bob/hash/integer.html
-// This is also used as a PRNG in the V8 benchmark suite
-
-Khan.randomSeed = parseFloat( Khan.query.seed ) || ( new Date().getTime() & 0xffffffff );
-
-KhanUtil.random = function() {
-	// Robert Jenkins' 32 bit integer hash function.
-	var seed = Khan.randomSeed;
-	seed = ( ( seed + 0x7ed55d16 ) + ( seed << 12 ) ) & 0xffffffff;
-	seed = ( ( seed ^ 0xc761c23c ) ^ ( seed >>> 19 ) ) & 0xffffffff;
-	seed = ( ( seed + 0x165667b1 ) + ( seed << 5 ) ) & 0xffffffff;
-	seed = ( ( seed + 0xd3a2646c ) ^ ( seed << 9 ) ) & 0xffffffff;
-	seed = ( ( seed + 0xfd7046c5 ) + ( seed << 3 ) ) & 0xffffffff;
-	seed = ( ( seed ^ 0xb55a4f09 ) ^ ( seed >>> 16 ) ) & 0xffffffff;
-	return ( Khan.randomSeed = ( seed & 0xfffffff ) ) / 0x10000000;
-};
