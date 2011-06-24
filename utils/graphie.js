c26f8b467cc3f1ba482aba02ae394d067778dd3b
Ben Alpert <spicyjalapeno@gmail.com>
2011-06-24 11:51:40 -0700

    Fix some funny things that happen when plots get to be undefined

--- utils/graphie.js
+++ utils/graphie.js
@@ -25,9 +25,20 @@ var createGraph = function( el ) {
 			if ( point === true ) {
 				return "z";
 			} else {
-				return ( i === 0 ? "M" : "L") + scalePoint( point );
+				return ( i === 0 ? "M" : "L") + boundNumber(scaled[0]) + " " + boundNumber(scaled[1]);
 			}
 		}).join("");
+
+		// Bound a number by 1e-6 and 1e20 to avoid exponents after toString
+		function boundNumber( num ) {
+			if ( num === 0 ) {
+				return num;
+			} else if ( num < 0 ) {
+				return -boundNumber( num );
+			} else {
+				return Math.max( 1e-6, Math.min( num, 1e20 ) );
+			}
+		}
 	};
 
 	var processAttributes = function( attrs ) {
