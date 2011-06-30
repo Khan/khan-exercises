965a31b309eb0f40dfc945841c3d220b53c555ca
Ben Alpert <spicyjalapeno@gmail.com>
2011-06-30 16:05:38 -0700

    Rudimentary arrowhead support

--- utils/graphie.js
+++ utils/graphie.js
@@ -103,6 +103,46 @@ var createGraph = function( el ) {
 		return [ r * Math.cos( th ), r * Math.sin( th ) ];
 	};
 
+	var addArrowheads = function arrows( path ) {
+		var type = path.constructor.prototype;
+
+		if ( type === Raphael.el ) {
+			if ( path.type === "path" && typeof path.arrowheadsDrawn === "undefined" ) {
+				var w = path.attr( "stroke-width" ), s = 0.6 + 0.4 * w;
+				var l = path.getTotalLength();
+
+				if ( l < 0.75 * s ) {
+					// You're weird because that's a *really* short path
+					// Giving up now before I get more confused
+
+				} else {
+					// This makes a lot more sense
+
+					var head = raphael.path( "M-3 4 C-2.75 2.5 0 0.25 0.75 0C0 -0.25 -2.75 -2.5 -3 -4" );
+					var end = path.getPointAtLength( l );
+					var almostTheEnd = path.getPointAtLength( l - 0.75 * s );
+					var angle = Math.atan2( end.y - almostTheEnd.y, end.x - almostTheEnd.x ) * 180 / Math.PI;
+					var attrs = path.attr();
+					delete attrs.path;
+
+					var subpath = path.getSubpath( 0, l - 0.75 * s );
+					subpath = raphael.path( subpath ).attr( attrs );
+					subpath.arrowheadsDrawn = true;
+
+					path.remove();
+					head.rotate( angle, 0.75, 0 ).scale( s, s, 0.75, 0 )
+						.translate( almostTheEnd.x, almostTheEnd.y ).attr( attrs )
+						.attr({ "stroke-linejoin": "round", "stroke-linecap": "round" });
+					head.arrowheadsDrawn = true;
+				}
+			}
+		} else if ( type === Raphael.st ) {
+			for (var i = 0, l = path.items.length; i < l; i++) {
+				arrows( path.items[i] );
+			}
+		}
+	};
+
 	var drawingTools = {
 		circle: function( center, radius ) {
 			return raphael.ellipse.apply( raphael, scalePoint( center ).concat( scaleVector([ radius, radius ]) ) );
@@ -300,6 +340,10 @@ var createGraph = function( el ) {
 			var type = result.constructor.prototype
 			if ( type === Raphael.el || type === Raphael.st ) {
 				result.attr( currentStyle );
+
+				if ( currentStyle.arrowheads ) {
+					addArrowheads( result );
+				}
 			}
 
 			currentStyle = oldStyle;
