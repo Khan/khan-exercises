f7ab339fcdab9b326b2a3d48f67c603a49d09f00
Marcia Lee <marcia.lee@gmail.com>
2011-07-14 10:54:33 -0700

    Change helper to use graphie label styling

--- utils/graphie-helpers.js
+++ utils/graphie-helpers.js
@@ -1,17 +1,12 @@
 // Temporary not really following convention file, see #160
 function drawDigits( digits, startX, startY, color ) {
-	with ( jQuery.tmpl.VARS ) {
-		with ( KhanUtil.currentGraph ) {
-			var set = [];
-			jQuery.each( digits, function( index, digit ) {
-				var colorStrPre = color ? "\\color{" + color + "}{" : "";
-				var colorStrPost = color ? "}" : "";
-				var str = "\\Huge{" + colorStrPre + digit + colorStrPost+ "}";
-				set.push( label( [ startX + index, startY ], str ) );
-			});
-			return set;
-		}
-	}
+	var graph = KhanUtil.currentGraph;
+	var set = [];
+	jQuery.each( digits, function( index, digit ) {
+		var str = "\\Huge{" + digit + "}";
+		set.push( graph.label( [ startX + index, startY ], str, { color: color } ) );
+	});
+	return set;
 }
 
 function numberLine( start, end, step ) {
