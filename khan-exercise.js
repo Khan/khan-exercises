1c494b28ec9b7b00ba07b64eef5ee5d0fedc390b
Ben Alpert <spicyjalapeno@gmail.com>
2011-07-25 19:23:11 -0700

    Answer types overhaul (fix #377, fix #426, fix #399, fix #411)

--- khan-exercise.js
+++ khan-exercise.js
@@ -950,9 +950,9 @@ function makeProblem( id, seed ) {
 		if ( choices.length ) {
 			answerType = "radio";
 
-		// Otherwise we assume a basic text input
+		// Otherwise we assume the smart number type
 		} else {
-			answerType = "text";
+			answerType = "number";
 		}
 	}
 
