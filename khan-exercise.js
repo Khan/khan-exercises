3dec9d1b5b26d8185253f30ca839e205e218ca8a
Ben Alpert <spicyjalapeno@gmail.com>
2011-07-28 11:06:33 -0700

    Fix IE8 for indexOf

--- khan-exercise.js
+++ khan-exercise.js
@@ -124,6 +124,39 @@ var primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43,
 		+ "else that you think is acting a little funky. Thanks for helping "
 		+ "us out!";
 
+// from MDC, thx :)
+if (!Array.prototype.indexOf) {
+	Array.prototype.indexOf = function (searchElement /*, fromIndex */ ) {
+		if (this === void 0 || this === null) {
+			throw new TypeError();
+		}
+		var t = Object(this);
+		var len = t.length >>> 0;
+		if (len === 0) {
+			return -1;
+		}
+		var n = 0;
+		if (arguments.length > 0) {
+			n = Number(arguments[1]);
+			if (n !== n) { // shortcut for verifying if it's NaN
+			n = 0;
+		} else if (n !== 0 && n !== (1 / 0) && n !== -(1 / 0)) {
+			n = (n > 0 || -1) * Math.floor(Math.abs(n));
+		}
+	}
+	if (n >= len) {
+		return -1;
+	}
+	var k = n >= 0 ? n : Math.max(len - Math.abs(n), 0);
+	for (; k < len; k++) {
+		if (k in t && t[k] === searchElement) {
+			return k;
+		}
+	}
+	return -1;
+	}
+}
+
 // Add in the site stylesheets
 if (testMode) {
 	(function(){
