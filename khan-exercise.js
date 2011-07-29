aac015806dcf1f903f7860ffd08118b35d096919
John Resig <jeresig@gmail.com>
2011-07-29 14:52:02 -0400

    Make sure we're pulling in the right state from the cache. Allows us to gracefully handle the user hitting the back button. Fixes #479.

--- khan-exercise.js
+++ khan-exercise.js
@@ -1430,19 +1430,26 @@ function request( method, data, fn, fnError ) {
 
 // Update the visual representation of the points/streak
 function updateData( data ) {
-	// Make sure we have current data
-	data = data || getData();
-
-	// Change users, if needed
-	if ( user !== data.user ) {
+	// Check if we're setting/switching usernames
+	if ( data ) {
 		user = data.user || user;
 		userCRC32 = user != null ? crc32( user ) : null;
 		randomSeed = userCRC32 || randomSeed;
 	}
 
-	// Cache the data locally
-	if ( data && user != null ) {
-		window.localStorage[ "exercise:" + user + ":" + exerciseName ] = JSON.stringify( data );
+	// Make sure we have current data
+	var oldData = getData();
+
+	// Change users, if needed
+	if ( data && (data.total_done >= oldData.total_done || data.user !== oldData.user) ) {
+		// Cache the data locally
+		if ( user != null ) {
+			window.localStorage[ "exercise:" + user + ":" + exerciseName ] = JSON.stringify( data );
+		}
+
+	// If no data is provided then we're just updating the UI
+	} else {
+		data = oldData;
 	}
 
 	// Update the streaks/point bar
