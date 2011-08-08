94a9bc4a6268de94480c6c460abf0f8a39d45be2
Marcos Ojeda <subliminal@gmail.com>
2011-08-08 14:08:10 -0400

    Revert "test for .local domains to trigger test mode"
    This reverts commit 108fe4cbecba2fb6f78365f68ec9856b68f884ab.

--- khan-exercise.js
+++ khan-exercise.js
@@ -39,7 +39,6 @@ var primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43,
 	testMode = (window.location.host.indexOf("localhost") === 0 ||
 				window.location.host.indexOf("127.0.0.1") === 0 ||
 				window.location.host.indexOf("192.168") === 0 ||
-				/\.local$/.test(window.location.host) ||
 				window.location.protocol === "file:") &&
 				/\.html$/.test( window.location.pathname ),
 
