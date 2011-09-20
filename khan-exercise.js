46c971b4bcc34a1c2cce8801b23ab229103fbd2b
John Resig <jeresig@gmail.com>
2011-09-20 16:08:01 -0400

    Make sure that the page doesn't reload on error when we're in a tablet.

--- khan-exercise.js
+++ khan-exercise.js
@@ -1453,15 +1453,19 @@ function prepareSite() {
 			// Error during submit. Cheat, for now, and reload the page in
 			// an attempt to get updated data.
 
-			if ( user != null && exerciseName != null ) {
-				// Before we reload, clear out localStorage's UserExercise.
-				// If there' a discrepancy between server and localStorage such that
-				// problem numbers are out of order or anything else, we want
-				// to restart with whatever the server sends back on reload.
-				delete window.localStorage[ "exercise:" + user + ":" + exerciseName ];
-			}
+			if ( typeof userExercise === "undefined" || !userExercise.tablet ) {
+				if ( user != null && exerciseName != null ) {
+					// Before we reload, clear out localStorage's UserExercise.
+					// If there' a discrepancy between server and localStorage such that
+					// problem numbers are out of order or anything else, we want
+					// to restart with whatever the server sends back on reload.
+					delete window.localStorage[ "exercise:" + user + ":" + exerciseName ];
+				}
 
-			window.location.reload();
+				window.location.reload();
+			} else {
+				// TODO: Implement alternative error handling
+			}
 		});
 
 		if ( pass === true ) {
@@ -2308,7 +2312,7 @@ function loadModules() {
 	}
 }
 
-if ( typeof urlBase !== "undefined" ) {
+if ( typeof userExercise !== "undefined" && userExercise.tablet ) {
 	Khan.loadExercise = loadExercise;
 }
 
