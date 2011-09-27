5befd8a20a8dd26a46a926b54750fd05942837e4
Ben Alpert <spicyjalapeno@gmail.com>
2011-09-27 16:56:06 -0400

    Actually give an id to #report

--- khan-exercise.js
+++ khan-exercise.js
@@ -1630,7 +1630,7 @@ function prepareSite() {
 	// to be more clear that it won't replace the current page.
 	jQuery( "<a>Report a Problem</a>" )
 		.attr( "id", "report" ).addClass( "simple-button action-gradient green" )
-		.replaceAll( jQuery( ".footer-links a:first" ) );
+		.replaceAll( jQuery( ".footer-links #report" ) );
 
 	jQuery( "#report" ).click( function( e ) {
 
