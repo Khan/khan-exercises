bb85dba3b7bdb488ce5d9e87f3538f299c6b6fb4
Julian Pulgarin <jp@julianpulgarin.com>
2011-10-03 02:41:18 -0700

    Show and support branches

--- main/views.py
+++ main/views.py
@@ -11,13 +11,19 @@ import os
 from subprocess import call
 
 def repo(request, template_name="repo.html"):
-
 	with closing(urllib2.urlopen("http://github.com/api/v2/json/pulls/%s/%s" % (settings.SANDCASTLE_USER, settings.SANDCASTLE_REPO))) as u:
 		pull_data = u.read()
+
+
+	with closing(urllib2.urlopen("http://github.com/api/v2/json/repos/show/%s/%s/branches" % (settings.SANDCASTLE_USER, settings.SANDCASTLE_REPO))) as u:
+		branch_data = u.read()
 	
 	pulls = json.loads(pull_data)
+	branches = json.loads(branch_data)
+
 	context = {
 		'pulls': pulls['pulls'],
+		'branches': branches['branches'],
 	}
 
 	return render_to_response(
@@ -27,11 +33,14 @@ def repo(request, template_name="repo.html"):
 	)
 
 
-def sandcastle(request, number=None):
-	with closing(urllib2.urlopen("http://github.com/api/v2/json/pulls/%s/%s/%s" % (settings.SANDCASTLE_USER, settings.SANDCASTLE_REPO, number))) as u:
-		pull_data = u.read()
+def sandcastle(request, number=None, branch=None):
+	user = settings.SANDCASTLE_USER
+
+	if number:
+		with closing(urllib2.urlopen("http://github.com/api/v2/json/pulls/%s/%s/%s" % (settings.SANDCASTLE_USER, settings.SANDCASTLE_REPO, number))) as u:
+			pull_data = u.read()
+		user, branch = json.loads(pull_data)['pull']['head']['label'].split(":")
 	
-	user, branch = json.loads(pull_data)['pull']['head']['label'].split(":")
 	name = "%s:%s" % (user, branch)
 
 	os.chdir(os.path.join(settings.PROJECT_DIR, 'media/castles'))
