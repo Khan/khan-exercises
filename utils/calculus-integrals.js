369adb7c9946ff8fc0f26d45ddd58d8f4f5f3be8
ioddly <ioddly@ioddly.com>
2011-11-24 15:51:14 -0600

    cleaned up indefinite integral exercise a little

--- utils/calculus-integrals.js
+++ utils/calculus-integrals.js
@@ -1,5 +1,3 @@
-// FIXME: 3x^-3 is integrated wrong!
-
 jQuery.extend(KhanUtil, {
 	// Generate a expression suitable for an integration problem using the power rule and constant rules
 	randBasicIntegral: function() {
@@ -50,21 +48,6 @@ jQuery.extend(KhanUtil, {
 				coefs[i] = 0
 		}
 		
-		if(false) {
-			var tmpcoefs = []
-			tmpcoefs[-4] = -3
-			tmpcoefs[-3] = 3
-			tmpcoefs[-2] = -2
-			tmpcoefs[-1] = 0
-			tmpcoefs[0] = 100
-			tmpcoefs[1] = 10
-			tmpcoefs[2] = -6
-			tmpcoefs[3] = 0
-			tmpcoefs[4] = 0 
-			tmpcoefs[5] = 12
-			return new KhanUtil.Polynomial(-4, 5, tmpcoefs)
-		}
-
 		return new KhanUtil.Polynomial(min, max, coefs)
 	},
 
@@ -84,7 +67,7 @@ jQuery.extend(KhanUtil, {
 			return negative ? ["-", fraction] : fraction
 		}
 
-		// We'll create a new expression using this array to store the results of the integration
+		// We'll create a new expression using these arrays to store the results of the integration
 		var coefs = [], wrong_coefs1 = [], wrong_coefs2 = [], wrong_coefs3 = []
 
 		// Perform the actual integration
@@ -93,41 +76,34 @@ jQuery.extend(KhanUtil, {
 			if(poly.coefs[i] == 0)
 				continue
 
-			var answer_coef = 0
-
-			// Wrong answer 1: Does not increment exponents at all
-			var wrong_coef1 = 0
-
-			// Wrong answer 2: Decrements negative exponents
-			var wrong_coef2 = 0
-
-			// Deal with constants
+			// Constant rule
 			if(i == 0) {
 				coefs[i+1] = poly.coefs[i]
 				wrong_coefs1[i] = poly.coefs[i]
 				wrong_coefs2[i+1] = poly.coefs[i]
 				wrong_coefs3[i+1] = poly.coefs[i]
 				continue
+			} else { 
+				// Power rule
+
+				// Instead of dividing and ending up with some gross
+				// floating-point number, we'll attempt to format the answer
+				// using fractions
+				coefs[i+1] = maybeFraction(poly.coefs[i], i+1)
+
+				// Wrong answer 1: Does not increment exponents at all
+				wrong_coefs1[i] = maybeFraction(poly.coefs[i], i)
+
+				// Wrong answer 2: Decrements negative exponents
+				if(i < 0) {
+					wrong_coefs2[i-1] = maybeFraction(poly.coefs[i], i-1)
+				} else {
+					wrong_coefs2[i+1] = maybeFraction(poly.coefs[i], i+1)
+				}
+
+				// Wrong answer 3: Reversed signs
+				wrong_coefs3[i+1] = maybeFraction(-poly.coefs[i], i+1)
 			}
-
-			// Instead of dividing and ending up with some gross
-			// floating-point number, we'll attempt to format the answer
-			// using fractions
-			coefs[i+1] = maybeFraction(poly.coefs[i], i+1)
-
-			// Wrong answer 1: Does not increment exponents at all
-			wrong_coefs1[i] = maybeFraction(poly.coefs[i], i)
-
-			// Wrong answer 2: Decrements negative exponents
-			if(i < 0) {
-				wrong_coefs2[i-1] = maybeFraction(poly.coefs[i], i-1)
-			} else {
-				wrong_coefs2[i+1] = maybeFraction(poly.coefs[i], i+1)
-			}
-
-			// Wrong answer 3: Reversed signs
-			console.log('reversi! ' + (-poly.coefs[i]) + ' ' + (i+1))
-			wrong_coefs3[i+1] = maybeFraction(-poly.coefs[i], i+1)
 		}
 
 		// Generate answers
@@ -143,25 +119,23 @@ jQuery.extend(KhanUtil, {
 		// Wrong answer 4: expression is differentiated instead of integrated
 		this.wrongs.push(KhanUtil.ddxPolynomial(poly))
 
-		this.hint_addition = ''
-
 		// Generate hints
 
 		// This hint shows the sum rule
-		for(var i = poly.minDegree; i <= poly.maxDegree; i++) {
+		this.hint_addition = ''
+
+		for(var i = poly.maxDegree; i >= poly.minDegree; i--) {
 			if(poly.coefs[i] != 0) {
 				var string = "\\int "
 				// Add coefficient, unless it is 1 or -1 then add nothing or -
 				string += Math.abs(poly.coefs[i] != 1) ? poly.coefs[i] : (poly.coefs[i] == 1 ? "" : "-");
-				// Add variable
-				var constant = i == 0
-				string += constant ? "" : poly.variable
-				// Add exponent
-				string += constant ? "" :  "^{" + i + "}"
-
+				// Add variable, exponent if this is not a constant
+				if(i != 0) {
+					string += poly.variable + '^{' + i + '}'
+				}
 				string += " dx "
-
-				if(i != poly.maxDegree)
+				// Unless this is the last term, append a +
+				if(i != poly.minDegree)
 					string += "+"
 				this.hint_addition += string
 			}
@@ -170,35 +144,34 @@ jQuery.extend(KhanUtil, {
 		// This hint shows the integration of each term in the problem
 		this.hint_integration = []
 
-		for(var i = 0; i != poly.getNumberOfTerms(); i++) {
-			var term = poly.getCoefAndDegreeForTerm(i)
+		for(var i = poly.maxDegree; i >= poly.minDegree; i--) {
+			var coef = poly.coefs[i]
 
-			var coef = term.coef
-			var expt = term.degree
+			// Ignore terms set to zero
+			if(coef == 0) continue
+
 			var variable = poly.variable
-			var constant = expt == 0
-
-			var expt_s = constant ? "" : ("^{" + expt + "}")
-
-			var string = "\\int " + coef + (constant ? "" : variable) + expt_s + " dx = "
-
+			var string = ''
 			// Constant rule
-			if(expt == 0) {
-				string += coef + variable
+			if(i == 0) {
+				// Restate the term with the integral symbol
+				string = '\\int ' + coef + ' dx = '
+				string += '\\color{' + KhanUtil.BLUE + '}{' + coef + variable + '}'
 			} else {
 				// Power rule
 
+				// Restate the term with the integral symbol
+				string = '\\int ' + coef + variable + '^{'+ (i == 1 ? "" : i) +'} dx = '
 				// Show increment of exponent
-				string += coef + "\\frac{1}{" + expt + " + 1}" + variable + "^{"+expt+"+1} = "
-
-				// Show multiplication of coefficient
-				string += coef + "\\frac{1}{" + (expt+1) + "}" + variable + "^{"+(expt+1)+ "} = "
-
-				var nterm = this.answer.getCoefAndDegreeForTerm(i)
-				var ncoef = nterm.coef
-				var nexpt = nterm.degree
-
-				string += KhanUtil.expr(ncoef).toString() + variable + "^{" + (expt+1) + "}"
+				string += coef + '\\frac{1}{' + i + '+1}' + variable + '^{'+i+'+1} = '
+				// Now actually increment it
+				string += coef + '\\frac{1}{' + (i+1) + '}' + variable + '^{'+(i+1)+'} = '
+				// Give answer
+				var ncoef = this.answer.coefs[i+1]
+				// Make 1 implicit
+				if(Math.abs(ncoef) == 1)
+					ncoef = ncoef < 0 ? "-" : ""
+				string += '\\color{' + KhanUtil.BLUE + '}{' + KhanUtil.expr(ncoef) + variable + '^{' + (i+1) + '}}'
 			}
 
 			this.hint_integration.push(string)
