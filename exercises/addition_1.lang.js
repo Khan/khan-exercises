4137f5d2f85ab29099124d08da664b835f1dd205
unknown <Philipp@Philipp-TOSH.(none)>
2011-12-02 17:16:53 +0100

    Vertalingen, inclusief global file

--- exercises/addition_1.lang.js
+++ exercises/addition_1.lang.js
@@ -1,6 +1,5 @@
 ({
 	"nl" : {
-		"titel":"Optellen 1",
-		"hint":"Er zijn <var>plural( A, \"blauwe stip\", \"blauwe stippen\" )</var> en <var>plural( B, \"groene stip\", \"groene stippen\" )</var>, voor een totaal van <var>A + B</var> stippen."
+		"hint" : "Er zijn <var>plural( A, \"blauwe stip\", \"blauwe stippen\" )</var> en <var>plural( B, \"groene stip\", \"groene stippen\" )</var>, voor een totaal van <var>A + B</var> stippen."
 	}
 })
---
+++ exercises/lang.js
@@ -0,0 +1,17 @@
+({
+
+	"titles" : {
+		"nl" : {
+			"Addition 1" : "Optellen 1",
+			"Addition 2" : "Optellen 2",
+			"Addition 3" : "Optellen 3"
+		}
+	},
+
+	"globals" : {
+		"nl" : {
+			"Veel" : "voorkomende woorden"
+		}
+	}
+
+})
