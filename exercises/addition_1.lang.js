3bd8726b609121c3e46daebe5434f659d77e97e1
Philipp Gayret <pgayret@gmail.com>
2011-12-07 10:05:10 +0100

    Addition 1 met nederlandse vertaling

--- exercises/addition_1.lang.js
+++ exercises/addition_1.lang.js
@@ -1,5 +1,5 @@
 ({
 	"nl" : {
-		"hint" : "Er zijn <var>plural( A, \"blauwe stip\", \"blauwe stippen\" )</var> en <var>plural( B, \"groene stip\", \"groene stippen\" )</var>, voor een totaal van <var>A + B</var> stippen."
+		"hint" : "Er zijn <var>plural( A, \"blauwe stip\", \"blauwe stippen\" )</var> en <var>plural( B, \"groene stip\", \"groene stippen\" )</var>, in totaal zijn dat <var>A + B</var> stippen."
 	}
 })
