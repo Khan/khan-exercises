883b225a615f746a98a59a13beb89e6b126a510c
Wijnand Baretta <wijnand@perceptum.nl>
2011-12-20 11:06:20 +0100

    Translation vertical_angles

---
+++ exercises/vertical_angles.lang.js
@@ -0,0 +1,8 @@
+({
+	"nl" : {
+		"exercise1"		: 'If we know that the <span class="hint_blue">blue angle is <var>MEASURE</var> degrees</span>, what is the <span class="hint_orange">measure of the orange angle</span>?,
+		"exercise2"			: 'What is the measure of the pink angle?,
+		"hint1"				: 'The pink and blue angles add up to 180 because they are adjacent and form a straight line.',
+		"hint2"				: 'The pink and orange angles also add up to 180 because they are adjacent and form a straight line too.'
+		}
+})
