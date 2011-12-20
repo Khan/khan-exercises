3ec7438d6890f56ae73a0420fb50c92f4a49720e
Wijnand Baretta <wijnand@perceptum.nl>
2011-12-20 11:55:22 +0100

    Added tags to adding_and_subtracting_radicals.html Quote (') in vertical_angles.js

---
+++ exercises/adding_and_subtracting_radicals.lang.js
@@ -0,0 +1,8 @@
+({
+	"nl" : {
+		"exercise1"		: 'Simplify the following expression:',
+		"hint1"				: 'First, try to factor any perfect squares out of the radicals.',
+		"hint2"				: 'Separate the radicals and simplify.',
+		"hint3"				: 'Finally, simplify by combining the terms.'
+		}
+})
