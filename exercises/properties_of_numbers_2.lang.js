bf5e15a0e607b55f1744eb578c90992240dd3f99
unknown <Ronald@Ronald-PC.(none)>
2011-12-21 18:10:27 +0100

    Signed-off-by: unknown <Ronald@Ronald-PC.(none)>

---
+++ exercises/properties_of_numbers_2.lang.js
@@ -0,0 +1,20 @@
+({
+	"nl" : {
+		"exercise1"	: 'By what number can we multiply <code><var>NUM</var></code> to get <code>1</code>?',
+		"exercise2"	: 'What number can we add to <code><var>NUM</var></code> to get <code>0</code>?',
+		"hint1"		: 'Any real number multiplied by <code>1</code> equals itself.',
+		"hint2"		: 'Without performing any multiplication steps, we know that <code><var>NUM</var> \times 1 = <var>NUM</var></code>.',
+		"hint3"		: 'This fact about multiplying by <code>1</code> is known as the identity property of multiplication, and it is useful for finding equivalent fractions.',
+		"hint4"		: 'Any real number plus <code>0</code> equals itself.',
+		"hint5"		: 'Without performing any addition steps, we know that <code><var>NUM</var> + 0 = <var>NUM</var></code>.',
+		"hint6"		: 'This fact about adding by <code>0</code> is known as the identity property of addition.',
+		"hint7"		: 'Any real number <code>x</code> (except <code>0</code>) can be multipled by <code>\dfrac{1}{x}</code> to get <code>1</code>.',
+		"hint8"		: 'Without performing any multiplication or division, we know that <code><var>NUM</var> \times \dfrac{1}{<var>NUM</var>} = 1</code>.',
+		"hint9"		: 'Thus, the answer is <code>\dfrac{1}{<var>NUM</var>}</code>.',
+		"hint10"		: 'This fact about multiplying by <code>\dfrac{1}{x}</code> is known as the multiplicative inverse property.',
+		"hint11"		: 'Adding the negative inverse of a number to that number equals <code>0</code>.',
+		"hint12"		: 'Without performing any addition or subtraction, we know that <code><var>NUM</var> +(<var>-1 * NUM</var>) = 0</code>.',
+		"hint13"		: 'Thus, the answer is <code><var>-1 * NUM</var></code>.',
+		"hint14"		: "This fact about adding negative inverses is known as the additive inverse property."
+		}
+})
