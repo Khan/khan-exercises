1a1e2958de7d4180fa96059f712f073ac32337b6
unknown <Ronald@Ronald-PC.(none)>
2011-12-21 15:01:33 +0100

    Distributive_property translatable
    Signed-off-by: unknown <Ronald@Ronald-PC.(none)>

---
+++ exercises/distributive_property.lang.js
@@ -0,0 +1,27 @@
+({
+	"nl" : {
+		"exercise1"	: 'Use distribution to solve:',
+		"exercise1"	: 'Using the picture below as a guide, what number could replace <code><var>SYMBOL</var></code>?',
+		"exercise1"	: 'There are <code><var>A</var></code> rows, and each row has <code><var>B</var> + <var>C</var> = <var>B+C</var></code> rectangles.',
+		"hint1"		: 'Each row has <code><var>B</var> + <var>C</var> = <var>B+C</var></code> rectangles, and there are <code><var>A</var></code> rows.',
+		"hint2"		: 'Counting by color:',
+		"hint3"		: 'Number of orange rectangles: <code><var>A</var> \times <var>B</var> = <var>A * B</var></code>',
+		"hint4"		: 'Number of blue rectangles: <code><var>A</var> \times <var>C</var> = <var>A* C</var></code>',
+		"hint5"		: 'Total number of rectangles: <code><var>A * B</var> + <var>A * C</var> = <var>RESULT</var></code>',
+		"hint6"		: 'Counting by row:',
+		"hint7"		: 'Number of rows: <code><var>A</var></code>',
+		"hint8"		: 'Number of rectangles in a row: <code><var>B</var> + <var>C</var> = <var>B + C</var></code>',
+		"hint9"		: 'Total number of rectangles: <code><var>A</var> \times <var>B + C</var> = <var>RESULT</var></code>',
+		"hint10"		: 'Both ways of counting result in the same number, and this is known as the distributive property.',
+		"hint11"		: 'Counting by color:',
+		"hint12"		: 'Number of orange rectangles: <code><var>A</var> \times <var>B</var> = <var>A * B</var></code>',
+		"hint13"		: 'Number of blue rectangles: <code><var>A</var> \times <var>C</var> = <var>A* C</var></code>',
+		"hint14"		: 'Total number of rectangles: <code><var>A * B</var> + <var>A * C</var> = <var>RESULT</var></code>',
+		"hint15"		: 'Counting by row:',
+		"hint16"		: 'Number of rows: <code><var>A</var></code>',
+		"hint18"		: 'Number of rectangles in a row: <code><var>B</var> + <var>C</var> = <var>B + C</var></code>',
+		"hint19"		: 'Total number of rectangles: <code><var>A</var> \times <var>B + C</var> = <var>RESULT</var></code>',
+		"hint20"		: 'Both ways of counting result in the same number, and this is known as the distributive property.',
+		"hint21"		: "Thus the symbol <code><var>SYMBOL</var></code> could be replaced with <code><var>MISSING</var></code>."
+		}
+})
