ab5426b68a3e4b7222128a8aced1899887166aa2
daan-perceptum <daan@perceptum.nl>
2011-12-27 14:32:58 +0100

    exercises translatable
    Signed-off-by: daan-perceptum <daan@perceptum.nl>

--- exercises/alternate_interior_angles.lang.js
+++ exercises/alternate_interior_angles.lang.js
@@ -1,10 +1,10 @@
 ({
 	"nl" : {
-		"exercise1" : "De twee horizontale lijnen lopen evenwijdig, en een derde lijn snijdt ze zoals beneden wordt getoond.",
+		"exercise1" : 'De twee horizontale lijnen lopen evenwijdig, en een derde lijn snijdt ze zoals beneden wordt getoond.',
 		"exercise2" : 'Als we weten dat de <span class="hint_blue">blauwe hoek <var>MEASURE</var> graden is</span>, hoe groot is dan <span class="hint_orange">de oranje hoek</span>?',
-		"hint1"	: 	"Opposite angles are equal, so the pink angle measures <var>MEASURE</var> degrees.",
-		"hint2"	:	"The pink and orange angles are corresponding angles, so they are also equal.",
-		"hint3"	:	"The orange angle measures <var>MEASURE</var> degrees.",
+		"hint1"	: 	'Opposite angles are equal, so the pink angle measures <var>MEASURE</var> degrees.',
+		"hint2"	:	'The pink and orange angles are corresponding angles, so they are also equal.',
+		"hint3"	:	'The orange angle measures <var>MEASURE</var> degrees.',
 		'hint4'	:	'Note that the green angle also measures <var>MEASURE</var> degrees. This makes sense because it is opposite the orange angle and corresponds with the blue angle.'
 	}
 })
