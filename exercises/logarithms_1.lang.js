dd68004fe5b0d199897ffdd6cc3f9114d68fcdd6
unknown <Ronald@Ronald-PC.(none)>
2011-12-28 13:53:45 +0100

    logarithm 1-2, mean median and mode, measuring angles, midpoint formula, multiplying expressions 1, multiplying fractions, multiplying fractions 1 --> translatable
    Signed-off-by: unknown <Ronald@Ronald-PC.(none)>

---
+++ exercises/logarithms_1.lang.js
@@ -0,0 +1,9 @@
+({
+	"nl" : {
+		"question1"	: 'If <code>\log_{b}x=y</code>, then <code>b^y=x</code>.',
+		"question2"	: 'First, try to write <code><var>number</var></code>, the number we are taking the logarithm of, as a power of <code><var>base</var></code>, the base of the logarithm.',
+		"question3"	: '<code><var>number</var></code> can be expressed as <code><var>power_string</var></code>.',
+		"question4"	: '<code><var>number</var></code> can be expressed as <code><var>base</var>^<var>answer</var></code>.'
+		
+		}
+})
