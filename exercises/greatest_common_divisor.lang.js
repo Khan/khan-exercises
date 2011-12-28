6f7d3bb00b177033d5e271a9365d2b15a0bacc20
unknown <Ronald@Ronald-PC.(none)>
2011-12-28 11:26:01 +0100

    greatest common divisor, herons formula, inequalities on a numberline, inverse trig functions, inverses of functions --> translatable
    Signed-off-by: unknown <Ronald@Ronald-PC.(none)>

---
+++ exercises/greatest_common_divisor.lang.js
@@ -0,0 +1,13 @@
+({
+	"nl" : {
+		"question1"	: 'What is the greatest common divisor of <var>A</var> and <var>B</var>?',
+		"question2"	: 'Another way to say this is: ',
+		
+		"hint1"		: 'The greatest common divisor is the largest number that is a factor (or divisor) of both <var>A</var> and <var>B</var>.',
+		"hint2"		: 'The only factor (divisor) of 1 is 1.',
+		"hint3"		: 'The factors (divisors) of <var>A</var> are <var>A_FACTORS</var>.',
+		"hint4"		: 'The only factor (divisor) of 1 is 1.',
+		"hint5"		: 'The factors (divisors) of <var>B</var> are <var>B_FACTORS</var>.',
+		"hint6"		: 'Thus, the greatest common divisor of <var>A</var> and <var>B</var> is <var>GCD</var>.'
+		}
+})
---
+++ exercises/herons_formula.lang.js
@@ -0,0 +1,8 @@
+({
+	"nl" : {
+		"question1"	: 'What is the area of this triangle? ( Round to two decimal places )',
+		
+		"hint1"		: "We know all sides of this triangle, so we can use Heron's formula to calculate the area.",
+		"hint2"		: "Heron's formula states that the area of a triangle <code>A=\sqrt{s(s-a)(s-b)(s-c)}</code>"
+		}
+})
