5797272df137f2c4e200f6f4560d619eae749826
unknown <Ronald@Ronald-PC.(none)>
2011-12-28 10:09:26 +0100

    even and odd functions translatable
    Signed-off-by: unknown <Ronald@Ronald-PC.(none)>

---
+++ exercises/even_and_odd_functions.lang.js
@@ -0,0 +1,16 @@
+({
+	"nl" : {
+		"question1"	: '<code>f(x)</code> is graphed below.',
+		"question2"	: 'Is <code>f(x)</code> even, odd, or neither?',
+		"question3"	: 'Even',
+		"question4"	: 'Odd',
+		"question5"	: 'Neither',
+		
+		"hint1"		: '<code>f(-a)\neq f(a)</code>, so <code>f(x)</code> is not even.',
+		"hint2"		: '<code>f(-a)\neq -f(a)</code>, so <code>f(x)</code> is not odd.',
+		"hint3"		: '<code>f(-a)=-f(a)</code> for all of these points, so <code>f(x)</code> is... ?',
+		"hint4"		: '<code>f(-a)=-f(a)</code> for all of these points, so <code>f(x)</code> is <span class="hint_blue">odd</span>.',
+		"hint5"		: '<code>f(-a)=f(a)</code> for all of these points, so <code>f(x)</code> is... ?',
+		"hint6"		: '<code>f(-a)=f(a)</code> for all of these points, so <code>f(x)</code> is <span class="hint_blue">even</span>.'
+		}
+})
