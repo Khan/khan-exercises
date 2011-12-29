6bf508eee723847ea1feea1fb155b9da8cc923a3
RonaldPerceptum <ronald@perceptum.nl>
2011-12-29 15:15:30 +0100

    Power rule, probability 1, product rule, rate problems 1 - 2 --> translatable
    Signed-off-by: RonaldPerceptum <ronald@perceptum.nl>

---
+++ exercises/product_rule.lang.js
@@ -0,0 +1,10 @@
+({
+	"nl" : {
+		"question1"	: 'Find <code>\displaystyle \frac{d}{dx} \bigl((<var>FUNCN.fText</var>)(<var>FUNCD.fText</var>)\bigr)</code>.',
+		
+		"hint1"		: "We know <code>\displaystyle \frac{d}{dx} \bigl( f(x) g(x) \bigr) = f'(x)g(x) + g'(x)f(x)</code>.",
+		"hint2"		: 'In this case,',
+		"hint3"		: 'Differentiate each function:',
+		"hint4"		: 'Thus, the answer is'
+		}
+})
