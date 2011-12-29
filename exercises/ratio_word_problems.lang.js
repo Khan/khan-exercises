0086f86e3d92d1cc9968347ce58b833077905abb
RonaldPerceptum <ronald@perceptum.nl>
2011-12-29 17:25:10 +0100

    ratio word problems Translatable
    Signed-off-by: RonaldPerceptum <ronald@perceptum.nl>

---
+++ exercises/ratio_word_problems.lang.js
@@ -0,0 +1,35 @@
+({
+	"nl" : {
+		"question1"	: 'In <var>course( 1 )</var> class, the girl to boy ratio is <code><var>RATIO_GIRLS</var></code> to <code><var>RATIO_BOYS</var></code>.',
+		"question2"	: 'If there are <code><var>NUM_GIRLS</var></code> girls, how many boys are there?',
+		"question3"	: 'If there are <code><var>NUM_BOYS</var></code> boys, how many girls are there?',
+		"question4"	: 'If there are a total of <code><var>TOTAL</var></code> students, how many girls are there?',
+		"question5"	: 'If there are a total of <code><var>TOTAL</var></code> students, how many boys are there?',
+		"question6"	: 'The scale on a map is <var>SCALE_MAP</var>cm : <var>SCALE_REAL</var>km.',
+		"question7"	: 'If two cities are <var>DISTANCE_MAP</var>cm apart on the map, what is the actual distance in km between the two cities?',
+		"question8"	: 'If the distance between two cities is <var>DISTANCE_REAL</var>km, how far apart in cm are the two cities on the map?',
+		
+		"hint1"		: 'A ratio of <code><var>RATIO_GIRLS</var></code> girls to <code><var>RATIO_BOYS</var></code> boys means that there is a group of <code><var>RATIO_GIRLS</var></code> girls for every group of <code><var>RATIO_BOYS</var></code> boys.',
+		"hint2"		: 'If there are <code><var>NUM_GIRLS</var></code> girls, then there are <code><var>FACTOR</var></code> groups of <code><var>RATIO_GIRLS</var></code> girls each.',
+		"hint3"		: 'Because we know the ratio is <code><var>RATIO_GIRLS</var>:<var>RATIO_BOYS</var></code>, there must be <code><var>FACTOR</var></code> groups of <code><var>RATIO_BOYS</var></code> boys each.',
+		"hint4"		: 'There is a total of <code><var>NUM_BOYS</var></code> boys in <var>course( 1 )</var> class.',
+		"hint5"		: 'A ratio of <code><var>RATIO_GIRLS</var></code> girls to <code><var>RATIO_BOYS</var></code> boys means that there is a group of <code><var>RATIO_GIRLS</var></code> girls for every group of <code><var>RATIO_BOYS</var></code> boys.',
+		"hint6"		: 'If there are <code><var>NUM_BOYS</var></code> boys, then there are <code><var>FACTOR</var></code> groups of <code><var>RATIO_BOYS</var></code> boys each.',
+		"hint7"		: 'Because we know the ratio is <code><var>RATIO_GIRLS</var>:<var>RATIO_BOYS</var></code>, there must be <code><var>FACTOR</var></code> groups of <code><var>RATIO_GIRLS</var></code> girls each.',
+		"hint8"		: 'There is a total of <code><var>NUM_GIRLS</var></code> girls in <var>course( 1 )</var> class.',
+		"hint9"		: 'A ratio of <code><var>RATIO_GIRLS</var></code> girls to <code><var>RATIO_BOYS</var></code> boys means that a set of <code><var>RATIO_GIRLS + RATIO_BOYS</var></code> students will have <code><var>RATIO_GIRLS</var></code> girls and <code><var>RATIO_BOYS</var></code> boys.',
+		"hint10"	: 'A class of <code><var>TOTAL</var></code> students has <code><var>FACTOR</var></code> sets of <code><var>RATIO_GIRLS + RATIO_BOYS</var></code> students.',
+		"hint11"	: 'Because we know that there are <code><var>RATIO_GIRLS</var></code> girls in each set of <code><var>RATIO_GIRLS + RATIO_BOYS</var></code> students, the class must have <code><var>FACTOR</var></code> groups of <code><var>RATIO_GIRLS</var></code> girls each.',
+		"hint12"	: 'There is a total of <code><var>NUM_GIRLS</var></code> girls in <var>course( 1 )</var> class.',
+		"hint13"	: 'A ratio of <code><var>RATIO_GIRLS</var></code> girls to <code><var>RATIO_BOYS</var></code> boys means that a set of <code><var>RATIO_GIRLS + RATIO_BOYS</var></code> students will have <code><var>RATIO_GIRLS</var></code> girls and <code><var>RATIO_BOYS</var></code> boys.',
+		"hint14"	: 'A class of <code><var>TOTAL</var></code> students has <code><var>FACTOR</var></code> sets of <code><var>RATIO_GIRLS + RATIO_BOYS</var></code> students.',
+		"hint15"	: 'Because we know that there are <code><var>RATIO_BOYS</var></code> boys in each set of <code><var>RATIO_GIRLS + RATIO_BOYS</var></code> students, the class must have <code><var>FACTOR</var></code> groups of <code><var>RATIO_BOYS</var></code> boys each.',
+		"hint16"	: 'There is a total of <code><var>NUM_BOYS</var></code> boys in <var>course( 1 )</var> class.',
+		"hint17"	: 'The scale means that every <var>SCALE_MAP</var>cm on the map corresponds to an actual distance of <var>SCALE_REAL</var>km.',
+		"hint18"	: 'A distance of <var>DISTANCE_MAP</var>cm on the map is the same as <var>FACTOR</var> <code>\cdot</code> <var>SCALE_MAP</var>cm.',
+		"hint19"	: 'The actual distance between the two cities is <var>FACTOR</var> <code>\cdot</code> <var>SCALE_REAL</var>km, or <var>DISTANCE_REAL</var>km.',
+		"hint20"	: 'The scale means that every <var>SCALE_MAP</var>cm on the map corresponds to an actual distance of <var>SCALE_REAL</var>km.',
+		"hint21"	: 'An actual distance of <var>DISTANCE_REAL</var>km is the same as <var>FACTOR</var> <code>\cdot</code> <var>SCALE_REAL</var>km.',
+		"hint22"	: 'The distance between the two cities on the map is <var>FACTOR</var> <code>\cdot</code> <var>SCALE_MAP</var>cm, or <var>DISTANCE_MAP</var>cm.'
+		}
+})
