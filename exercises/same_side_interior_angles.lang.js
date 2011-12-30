559a93521a235eda5e75459319b07f72197b72df
RonaldPerceptum <ronald@perceptum.nl>
2011-12-30 13:26:00 +0100

    Exercises Translatable
    Signed-off-by: RonaldPerceptum <ronald@perceptum.nl>

---
+++ exercises/same_side_interior_angles.lang.js
@@ -0,0 +1,12 @@
+({
+	"nl" : {
+		"question1"	: 'The two horizontal lines are parallel, and there is a third line that intersects them as shown below.',
+		"question2"	: 'If we know that the <span class="hint_blue">blue angle is <var>MEASURE</var> degrees</span>, what is the <span class="hint_orange">measure of the orange angle</span>?',
+		
+		"hint1"		: 'The pink angles are adjacent to the blue angle and form a straight line, so we know that they measure <var>180 - MEASURE</var> degrees.',
+		"hint2"		: 'It makes sense that the pink angles are equal because they are opposite each other.',
+		"hint3"		: 'One of the pink angles corresponds with the orange angle, and the other pink angle forms an alternate interior angle.',
+		"hint4"		: 'The orange angle equals the pink angles and measures <var>180 - MEASURE</var> degrees.',
+		"hint5"		: 'Note that the blue and orange angles are supplementary.'
+		}
+})
