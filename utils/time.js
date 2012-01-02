cdeab0f6fcd9a904014360e9b28547c25acab4d2
Stephanie H. Chang <chang.steph.h@gmail.com>
2012-01-02 01:33:46 -0800

    revisecheck for undefined instead of truthiness in time.js

--- utils/time.js
+++ utils/time.js
@@ -58,7 +58,7 @@ jQuery.extend( KhanUtil, {
 			if ( this.showLabels ) {
 				this.drawLabels();
 			}
-			if ( this.hour && this.minute ) {
+			if ( this.hour !== undefined && this.minute !== undefined ) {
 				this.drawHands();
 			}
 			return this.set;
