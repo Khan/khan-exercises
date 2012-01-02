2c83e3044cdb1023fb9458f741ab1f6fb74918a9
RonaldPerceptum <ronald@perceptum.nl>
2012-01-02 14:49:25 +0100

    triangle inequality theorem, triangle types, trig identities, trigonometry 1 - 2, unit circle, writing expreessions 1
    Signed-off-by: RonaldPerceptum <ronald@perceptum.nl>

---
+++ exercises/trigonometry_1.lang.js
@@ -0,0 +1,24 @@
+({
+	"nl" : {
+		"question1"	: '<code>\overline{AC}</code> is <code><var>AC</var></code> units long',
+		"question2"	: '<code>\overline{BC}</code> is <code><var>BC</var></code> units long',
+		"question3"	: '<code>\overline{AB}</code> is <code><var>AB_STRING</var></code> units long',
+		"question4"	: 'What is <code>\sin(\angle <var>ANGLE</var>)</code> ?',
+		"question5"	: 'What is <code>\cos(\angle <var>ANGLE</var>)</code> ?',
+		"question6"	: 'What is <code>\tan(\angle <var>ANGLE</var>)</code> ?',
+		
+		"hint1"		: '<b>S</b>in = <b>O</b>pposite over <b>H</b>ypotenuse',
+		"hint2"		: 'opposite <code>= \overline{<var>OPPOSITE_NAME</var>} = <var>OPPOSITE_VALUE</var></code>',
+		"hint3"		: 'hypotenuse <code>= \overline{<var>HYPOTENUSE_NAME</var>} = <var>AB_STRING</var></code>',
+		"hint4"		: '<code>\overline{AC}</code> is <code><var>AC</var></code> units long',
+		"hint5"		: '<code>\overline{BC}</code> is <code><var>BC</var></code> units long',
+		"hint6"		: '<code>\overline{AB}</code> is <code><var>AB_STRING</var></code> units long',
+		"hint7"		: 'adjacent <code>= \overline{<var>ADJACENT_NAME</var>} = <var>ADJACENT_VALUE</var></code>',
+		"hint8"		: 'hypotenuse <code>= \overline{<var>HYPOTENUSE_NAME</var>} = <var>AB_STRING</var></code>',
+		"hint9"		: '<code>\overline{AC}</code> is <code><var>AC</var></code> units long',
+		"hint10"	: '<code>\overline{BC}</code> is <code><var>BC</var></code> units long',
+		"hint11"	: '<code>\overline{AB}</code> is <code><var>AB_STRING</var></code> units long',
+		"hint12"	: '<b>C</b>os = <b>A</b>djacent over <b>H</b>ypotenuse',
+		"hint13"	: '<b>T</b>an = <b>O</b>pposite over <b>A</b>djacent'
+		}
+})
---
+++ exercises/trigonometry_2.lang.js
@@ -0,0 +1,23 @@
+({
+	"nl" : {
+	
+		"hint1"		: '<code>\overline{AB}</code> is the <b>hypotenuse</b>',
+		"hint2"		: '<code>\overline{<var>OPPOSITE_NAME</var>}</code> is <b>opposite</b> to <code>\angle <var>ANGLE</var></code>',
+		"hint3"		: 'We know the hypotenuse and need to solve for the opposite side so we can use the sine function (SOH)',
+		"hint4"		: '<code>\overline{<var>OPPOSITE_NAME</var>}</code> is the <b>opposite</b> to <code>\angle <var>ANGLE</var></code>',
+		"hint5"		: '<code>\overline{AB}</code> is the <b>hypotenuse</b> (note that it is opposite the right angle)',
+		"hint6"		: 'We know the opposite side and need to solve for the hypotenuse so we can use the sin function (SOH)',
+		"hint7"		: '<code>\overline{AB}</code> is the <b>hypotenuse</b>',
+		"hint8"		: '<code>\overline{<var>ADJACENT_NAME</var>}</code> is <b>adjacent</b> to <code>\angle <var>ANGLE</var></code>',
+		"hint9"		: 'We know the hypotenuse and need to solve for the adjacent side so we can use the cos function (CAH)',
+		"hint10"	: '<code>\overline{<var>ADJACENT_NAME</var>}</code> is <b>adjacent</b> to <code>\angle <var>ANGLE</var></code>',
+		"hint11"		: '<code>\overline{AB}</code> is the <b>hypotenuse</b> (note that it is opposite the right angle)',
+		"hint12"		: 'We know the adjacent side and need to solve for the hypotenuse so we can use the cos function (CAH)',
+		"hint13"		: '<code>\overline{<var>OPPOSITE_NAME</var>}</code> is the <b>opposite</b> to <code>\angle <var>ANGLE</var></code>',
+		"hint14"		: '<code>\overline{<var>ADJACENT_NAME</var>}</code> is <b>adjacent</b> to <code>\angle <var>ANGLE</var></code>',
+		"hint15"		: 'We know the opposite side and need to solve for the adjacent side so we can use the tan function (TOA)',
+		"hint16"		: '<code>\overline{<var>OPPOSITE_NAME</var>}</code> is the <b>opposite</b> to <code>\angle <var>ANGLE</var></code>',
+		"hint17"		: '<code>\overline{<var>ADJACENT_NAME</var>}</code> is <b>adjacent</b> to <code>\angle <var>ANGLE</var></code>',
+		"hint18"		: 'We know the adjacent side and need to solve for the opposite side so we can use the tan function (TOA)'
+		}
+})
