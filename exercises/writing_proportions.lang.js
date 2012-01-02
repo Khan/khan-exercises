85ab79120be87b8b99d19aac6f198146609ae5eb
RonaldPerceptum <ronald@perceptum.nl>
2012-01-02 15:15:31 +0100

    writing proportions
    Signed-off-by: RonaldPerceptum <ronald@perceptum.nl>

---
+++ exercises/writing_proportions.lang.js
@@ -0,0 +1,18 @@
+({
+	"nl" : {
+		"question1"	: 'Which equation would help determine the cost of <var>plural( N2, deskItem( 0 ) )</var>?',
+		"hint1"		: 'There are several equations that could help determine the cost, each with a slightly different approach.',
+		"hint2"		: 'We can write the fact that <var>plural( N1, deskItem( 0 ) )</var> cost $<var>C</var> as a proportion:',
+		"hint3"		: 'Let <code>x</code> represent the unknown cost of <var>plural( N2, deskItem( 0 ) )</var>. Since <var>plural( N2, deskItem( 0 ) )</var> cost <code>x</code>, we have the following proportion:',
+		"hint4"		: 'The cost changes along with the number of <var>deskItem( 0 )</var>s purchased, and so the two proportions are equivalent.',
+		"hint5"		: 'Let <code>x</code> represent the unknown cost of <var>plural( N2, deskItem( 0 ) )</var>. Since <var>plural( N2, deskItem( 0 ) )</var> cost <code>x</code>, we have the following proportion:',
+		"hint6"		: 'We can write the fact that <var>plural( N1, deskItem( 0 ) )</var> cost $<var>C</var> as a proportion:',
+		"hint7"		: 'The cost changes along with the number of <var>deskItem( 0 )</var>s purchased, and so the two proportions are equivalent.',
+		"hint8"		: 'We know the cost of <var>plural( N1, deskItem( 0 ) )</var>, and we want to know the cost of <var>plural( N2, deskItem( 0 ) )</var>. We can write the numbers of <var>deskItem( 0 )</var>s as a proportion:',
+		"hint9"		: 'We know <var>plural( N1, deskItem( 0 ) )</var> costs $<var>C</var>, and we can let <code>x</code> represent the unknown cost of <var>plural( N2, deskItem( 0 ) )</var>. The proportion of these costs can be expressed as:',
+		"hint10"	: 'The cost changes along with the number of <var>deskItem( 0 )</var>s purchased, and so the two proportions are equivalent.',
+		"hint11"	: 'If we let <code>x</code> represent the cost of <var>plural( N2, deskItem( 0 ) )</var>, we have the following proportion:',
+		"hint12"	: 'We have to pay $<var>C</var> for <var>plural( N1, deskItem( 0 ) )</var>, and that can be written as a proportion:',
+		"hint13"	: 'Since the price per folder stays the same, these two proportions are equivalent.'
+		}
+})
