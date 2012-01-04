48bfb77d702140917694019a3c2451ac8803b759
RonaldPerceptum <ronald@perceptum.nl>
2012-01-04 12:25:15 +0100

    New exercises Signed-off-by: RonaldPerceptum <ronald@perceptum.nl>

---
+++ exercises/decimals_on_the_number_line_1.lang.js
@@ -0,0 +1,11 @@
+({
+	"nl" : {
+		"question1"	: 'Move the <strong><code>\color{orange}{\text{orange dot}}</code></strong> to  <strong><code>\color{orange}{<var>SOLUTION</var>}</code></strong> on the number line.',
+		"question2"	: 'Move the orange dot to select your answer.',
+		
+		"hint1"		: "Above we've drawn the number line from 0 to 1, divided into 10 equal pieces.",
+		"hint2"		: 'The line is divided into 10 pieces, which means:',
+		"hint3"		: 'Therefore, moving the <code>\color{orange}{\text{orange dot}}</code> <code>\color{blue}{<var>TICK_MARK_SOLUTION</var>}</code> place<var>plural(TICK_MARK_SOLUTION)</var> will put it at the position <code>\color{orange}{<var>SOLUTION</var>}</code>.',
+		"hint4"		: 'The orange number shows where <code>\color{orange}{<var>SOLUTION</var>}</code> is on the number line.'
+		}
+})
