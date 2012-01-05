e1e0eec422cacf50c54b3babd208d3ee56300a62
Wijnand Baretta <wijnand@perceptum.nl>
2012-01-05 20:26:28 +0100

    Added lang.js files

---
+++ exercises/complementary_angles.lang.js
@@ -0,0 +1,8 @@
+({
+	"nl" : {
+		"question1" : "Als <code>\angle <var>A + O + C</var></code> een rechte hoek is en <code>m <var>ANGLE_ONE</var> = <var>ANGLE</var>^\circ</code>, wat is dan <code>m <var>ANGLE_TWO</var></code>, in graden?"
+		"hint1" : "In de afbeeldinging zien we dat <code><var>ANGLE_BOT</var></code> en <code><var>ANGLE_TOP</var></code> complementaire hoeken zijn."
+		"hint2" : "Daarom is <code>m <var>ANGLE_BOT</var> + m <var>ANGLE_TOP</var> = 90^\circ</code>."
+		"hint3" : "Dus, <code>m <var>ANGLE_TWO</var> = 90^\circ - m <var>ANGLE_ONE</var> = 90^\circ - <var>ANGLE</var>^\circ = <var>90 - ANGLE</var>^\circ</code>."
+	}
+})
---
+++ exercises/place_value.lang.js
@@ -0,0 +1,36 @@
+({
+	"nl" : {
+		"question1" : "What is the place value of <var>DIGIT</var> in <var>NUMBER</var>?"
+		"hint1" : "<var>NUMBER</var> can be represented as follows."
+		"hint2" : "Thus, <var>DIGIT</var> is in the <var>SOLUTION</var> place."
+		"question2" : "What is <var>cardinal( NUMBER )</var> in standard form?"
+		"hint3" : "Add all these parts up:"
+		"hint4" : "(function() {
+							var maxPower = DIGITS.length - 1;
+							return jQuery.map(DIGITS, function(digit, index) {
+								if ( digit === 0 ) {
+									return null;
+								}
+
+								if ( index === DIGITS.length - 1 ) {
+									if ( DIGITS[ index - 1] === 0 ) {
+										ADDENDS.push( digit );
+										return cardinal( digit ) + " is the same as " + digit;
+									}
+								} else if ( index === DIGITS.length - 2 ) {
+									var addend = digit * 10 + DIGITS[ DIGITS.length - 1 ];
+									ADDENDS.push( addend );
+									return cardinal( addend ) + " is the same as " + addend;
+								} else {
+									var words = cardinal( digit ) + " " + powerToPlace(maxPower - index);
+									var input = [ "*", digit, pow(10, maxPower - index) ];
+									var ex = expr( input );
+									var addend = expr( input, true );
+
+									ADDENDS.push( addend );
+									return words + " is the same as " + ex + " = " + addend;
+								}
+							});
+						})()"
+	}
+})
