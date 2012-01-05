1763303e2039ca0545c345f8843780f53e6c043c
RonaldPerceptum <ronald@perceptum.nl>
2012-01-05 16:31:57 +0100

    Translations: adding and subtracting fractions, adding and subtracting mixed numbers 1, angle types, comparing absolute values Translatable: converting repeating decimals to fractions, domain and range 0.5, multiplying_radicals
    Signed-off-by: RonaldPerceptum <ronald@perceptum.nl>

--- exercises/angle_types.lang.js
+++ exercises/angle_types.lang.js
@@ -1,14 +1,14 @@
 ({
 	"nl" : {
-		"question1"	: 'Is the shown angle acute, right, or obtuse?',
-		"list1"		: 'Acute',
-		"list2"		: 'Right',
-		"list2"		: 'Obtuse',
-		"hint1"		: 'The shown angle measures less than <code>90^\circ</code>.',
-		"hint2"		: 'Therefore, it is an acute angle.'
-		"hint3"		: 'The shown angle measures <code>90^\circ</code>.',
-		"hint4"		: 'Therefore, it is a right angle.'
-		"hint5"		: 'The shown angle measures more than <code>90^\circ</code>.',
-		"hint6"		: 'Therefore, it is an obtuse angle.'
+		"question1"	: 'Is de getoonde hoek scherp, recht of stomp?',
+		"list1"		: 'Scherp',
+		"list2"		: 'Recht',
+		"list2"		: 'Stomp',
+		"hint1"		: 'De getoonde hoek is minder dan <code>90^\circ</code>.',
+		"hint2"		: 'Daarom is het een scherpe hoek.'
+		"hint3"		: 'De getoonde hoek is <code>90^\circ</code>.',
+		"hint4"		: 'Daarom is het een rechte hoek.'
+		"hint5"		: 'De getoonde hoek is meer dan <code>90^\circ</code>.',
+		"hint6"		: 'Daarom is het een stompe hoek.'
 		}
 })
---
+++ exercises/domain_and_range_0.5.lang.js
@@ -0,0 +1,39 @@
+({
+	"nl" : {
+		"question1"	: 'The function <code>f(x)</code> is graphed; what is its domain?',
+		"question2"	: 'The function <code>f(x)</code> is graphed; what is its range?',
+		"question3"	: '',
+		"question4"	: '',
+		"question5"	: '',
+		"question6"	: '',
+		"question7"	: '',
+		
+		"problem1"	: '',
+		"problem2"	: '',
+		"problem3"	: '',
+		"problem4"	: '',
+		"problem5"	: '',
+		"problem6"	: '',
+		
+		"hint1"		: 'For what values of <code>x</code> does <code>f(x)</code> have a value?',
+		"hint2"		: 'What values can <code>f(x)</code> have?',
+		"hint3"		: 'Look at the vertical axis as if it were a number line.',
+		"hint4"		: 'Look at the horizontal axis as if it were a number line.',
+		"hint5"		: '',
+		"hint6"		: '',
+		"hint7"		: '',
+		"hint8"		: '',
+		"hint9"		: '',
+		"hint10"	: '',
+		"hint11"	: '',
+		"hint12"	: '',
+		"hint13"	: '',
+		"hint14"	: '',
+		"hint15"	: '',
+		"hint16"	: '',
+		"hint17"	: '',
+		"hint18"	: '',
+		"hint19"	: '',
+		"hint20"	: ''
+		}
+})
