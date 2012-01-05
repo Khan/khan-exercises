3d19ec812f1c316a7c9bd414c0e5f7201166f045
RonaldPerceptum <ronald@perceptum.nl>
2012-01-05 18:09:12 +0100

    Translatable Josh
    Signed-off-by: RonaldPerceptum <ronald@perceptum.nl>

---
+++ exercises/reading_pictographs_1.lang.js
@@ -0,0 +1,34 @@
+({
+	"nl" : {
+		"question1"	: 'Use the pictograph to answer the question below:',
+		"question2"	: ' badges earned by each student',
+		"question3"	: 'Who earned the most <var>BADGE</var> badges?',
+		"question4"	: 'Who earned the fewest <var>BADGE</var> badges?',
+		"question5"	: 'How many <var>BADGE</var> badges did <var>person( PERSON + 1 )</var> earn?',
+		
+		"hint1"		: 'Who has the largest number of <var>BADGE</var> symbols next to <var>his( MOST + 1 )</var> name in the chart?',
+		"hint2"		: '<var>person( MOST + 1 )</var> has the largest number of symbols next to <var>his( MOST + 1)</var> name.',
+		"hint3"		: '<var>person( MOST + 1 )</var> earned the most <var>BADGE</var> badges.',
+		"hint4"		: 'Who has the smallest number of <var>BADGE</var> symbols next to <var>his( LEAST + 1 )</var> name in the chart?',
+		"hint5"		: '<var>person( LEAST + 1 )</var> has the smallest number of symbols next to <var>his( LEAST + 1)</var> name.',
+		"hint6"		: '<var>person( LEAST + 1 )</var> earned the fewest <var>BADGE</var> badges.',
+		"hint7"		: 'The key at the bottom of the pictograph shows that each symbol represents <var>plural( VALUE_PER_IMG, "badge" )</var>.',
+		"hint8"		: "Find <var>person( PERSON + 1 )</var>'s row in the table:"+
+					'<div class="fake_row">'+
+						'<span><var>person( PERSON + 1 )</var></span><span data-each="DATA[ PERSON ] times"><var>FULL_IMAGE</var>'+
+						'</span><span data-each="(8 - DATA[ PERSON ]) times">&nbsp;</span>'+
+					'</div>',
+		"hint9"		: 'There <var>plural( "is", DATA[PERSON] )</var> <var>plural( DATA[ PERSON ], "symbol" )</var>.',
+		"hint10"	: 'person( PERSON + 1 )</var> earned <code><var>ANSWER</var></code> <var>BADGE</var> <var>plural( "badge", ANSWER )</var>.',
+		"hint11"	: '',
+		"hint12"	: '',
+		"hint13"	: '',
+		"hint14"	: '',
+		"hint15"	: '',
+		"hint16"	: '',
+		"hint17"	: '',
+		"hint18"	: '',
+		"hint19"	: '',
+		"hint20"	: ''
+		}
+})
