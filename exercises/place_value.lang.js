724aa471d1fb70fa04537deb6e2dd87b4bbcb75b
Wijnand Baretta <wijnand@perceptum.nl>
2012-01-05 23:48:20 +0100

    Several translations .lamg.js + math.js

--- exercises/place_value.lang.js
+++ exercises/place_value.lang.js
@@ -1,36 +1,34 @@
 ({
 	"nl" : {
-		"question1" : "What is the place value of <var>DIGIT</var> in <var>NUMBER</var>?",
-		"hint1" : "<var>NUMBER</var> can be represented as follows.",
-		"hint2" : "Thus, <var>DIGIT</var> is in the <var>SOLUTION</var> place.",
-		"question2" : "What is <var>cardinal( NUMBER )</var> in standard form?",
-		"hint3" : "Add all these parts up:",
-		"hint4" : '(function() {
-							var maxPower = DIGITS.length - 1;
-							return jQuery.map(DIGITS, function(digit, index) {
-								if ( digit === 0 ) {
-									return null;
-								}
-
-								if ( index === DIGITS.length - 1 ) {
-									if ( DIGITS[ index - 1] === 0 ) {
-										ADDENDS.push( digit );
-										return cardinal( digit ) + " is the same as " + digit;
-									}
-								} else if ( index === DIGITS.length - 2 ) {
-									var addend = digit * 10 + DIGITS[ DIGITS.length - 1 ];
-									ADDENDS.push( addend );
-									return cardinal( addend ) + " is the same as " + addend;
-								} else {
-									var words = cardinal( digit ) + " " + powerToPlace(maxPower - index);
-									var input = [ "*", digit, pow(10, maxPower - index) ];
-									var ex = expr( input );
-									var addend = expr( input, true );
-
-									ADDENDS.push( addend );
-									return words + " is the same as " + ex + " = " + addend;
-								}
-							});
-						})()'
+		"question1" : "Op welke plaats staat <var>DIGIT</var> in <var>NUMBER</var>?",
+		"hint1" : "<var>NUMBER</var> kan als volgt worden uitgeschreven:",
+		"hint2" : "Daar uit volgt dat de <var>DIGIT</var> op de plaats van de <var>SOLUTION</var> staat.",
+		"question2" : "Hoe schrijf je  <var>cardinal( NUMBER )</var> in cijfers?",
+		"hint3" : "Tel al deze getallen bij elkaar op:",
+		"hint4" : '(function() {'+
+					'		var maxPower = DIGITS.length - 1;'+
+					'		return jQuery.map(DIGITS, function(digit, index) {'+
+					'			if ( digit === 0 ) {'+
+					'				return null;'+
+					'			}'+
+					'		if ( index === DIGITS.length - 1 ) {'+
+					'				if ( DIGITS[ index - 1] === 0 ) {'+
+					'					ADDENDS.push( digit );'+
+					'					return cardinal( digit ) + " is hetzelfde als " + digit;'+
+					'				}'+
+					'			} else if ( index === DIGITS.length - 2 ) {'+
+					'				var addend = digit * 10 + DIGITS[ DIGITS.length - 1 ];'+
+					'				ADDENDS.push( addend );'+
+					'				return cardinal( addend ) + " is hetzelfde als " + addend;'+
+					'			} else {'+
+					'				var words = cardinal( digit ) + " " + powerToPlace(maxPower - index);'+
+					'				var input = [ "*", digit, pow(10, maxPower - index) ];'+
+					'				var ex = expr( input );'+
+					'				var addend = expr( input, true );'+
+					'				ADDENDS.push( addend );'+
+					'				return words + " is hetzelfde als " + ex + " = " + addend;'+
+					'			}'+
+					'		});'+
+					'	})()'
 	}
 })
