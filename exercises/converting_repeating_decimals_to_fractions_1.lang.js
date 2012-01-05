808473737eb9728ff0e85761bcce87383cd801a5
RonaldPerceptum <ronald@perceptum.nl>
2012-01-05 13:28:30 +0100

    Converting repeating decimals to fractions -> Translatable
    Signed-off-by: RonaldPerceptum <ronald@perceptum.nl>

---
+++ exercises/converting_repeating_decimals_to_fractions_1.lang.js
@@ -0,0 +1,8 @@
+({
+	"nl" : {
+		"question1"	: 'Rewrite as a simplified fraction.',
+		
+		"hint1"		: 'Let <code>x</code> equal the decimal. Set up two equations such that the digits after the decimal point are identical.',
+		"hint2"		: 'Subtracting the two equations, we have:'
+		}
+})
