dc221d30661d0b9f5eee535e8e071e6c380b9319
Ben Eater <ben@eater.net>
2012-01-05 22:42:02 -0500

    Refactor protractor; works on iPad
    fix #10506; fix #10289; fix #9929; fix #9937; fix #9902; fix #9810;
    fix #9816; fix #9779; fix #9689; fix #9665; fix #9552; fix #9545;
    fix #9473; fix #9429; fix #9370; fix #9110; fix #9090; fix #5231

--- utils/interactive.js
+++ utils/interactive.js
@@ -322,7 +322,7 @@ jQuery.extend( KhanUtil, {
 						}
 
 						// snap to points around circle
-						if ( movablePoint.constraints.fixedDistance.snapPoints ) { 
+						if ( movablePoint.constraints.fixedDistance.snapPoints ) {
 
 							var snapRadians = 2 * Math.PI / movablePoint.constraints.fixedDistance.snapPoints;
 							var radius = movablePoint.constraints.fixedDistance.dist;
@@ -334,8 +334,8 @@ jQuery.extend( KhanUtil, {
 
 							var mouseXrel = mouseX - centerX;
 							var mouseYrel = -mouseY + centerY;
-							var radians = Math.atan(mouseYrel / mouseXrel);  
-							var outsideArcTanRange = mouseXrel < 0;  
+							var radians = Math.atan(mouseYrel / mouseXrel);
+							var outsideArcTanRange = mouseXrel < 0;
 
 							// adjust so that angles increase from 0 to 2 pi as you go around the circle
 							if (outsideArcTanRange) {
@@ -352,7 +352,7 @@ jQuery.extend( KhanUtil, {
 							mouseX = mouseXrel + centerX;
 							mouseY = - mouseYrel + centerY;
 						}
-						
+
 						// coord{X|Y} are the scaled coordinate values
 						var coordX = mouseX / graph.scale[0] + graph.range[0][0];
 						var coordY = graph.range[1][1] - mouseY / graph.scale[1];
@@ -1199,3 +1199,179 @@ jQuery.extend( KhanUtil, {
 	}
 
 });
+
+
+function Protractor( center, r ) {
+	var graph = KhanUtil.currentGraph;
+	this.set = graph.raphael.set();
+
+	this.cx = center[0];
+	this.cy = center[1];
+	var lineColor = "#789";
+	var pro = this;
+
+	var imgPos = graph.scalePoint([ this.cx - r, this.cy + r ]);
+	this.set.push( graph.mouselayer.image( Khan.urlBase + "images/protractor.png", imgPos[0], imgPos[1], 322, 166 ) );
+
+
+	// Customized polar coordinate thingie to make it easier to draw the double-headed arrow thing.
+	// angle is what you'd expect -- use that big protractor on your screen :)
+	// pixels from edge is relative to the edge of the protractor; it's not the full radius
+	var arrowHelper  = function( angle, pixelsFromEdge ) {
+		var scaledRadius = graph.scaleVector( r );
+		var scaledCenter = graph.scalePoint( center );
+		var x = Math.sin( ( angle + 90 ) * Math.PI / 180 ) * ( scaledRadius[0] + pixelsFromEdge ) + scaledCenter[0];
+		var y = Math.cos( ( angle + 90 ) * Math.PI / 180 ) * ( scaledRadius[1] + pixelsFromEdge ) + scaledCenter[1];
+		return x + "," + y;
+	};
+
+	// Draw the double-headed arrow thing that shows users where to click and drag to rotate
+	var arrow = graph.raphael.path(
+		" M" + arrowHelper( 180, 6 ) +
+		" L" + arrowHelper( 180, 2 ) +
+		" L" + arrowHelper( 183, 10 ) +
+		" L" + arrowHelper( 180, 18 ) +
+		" L" + arrowHelper( 180, 14 ) +
+		" A" + ( graph.scaleVector(r)[0] + 10 ) + "," + ( graph.scaleVector(r)[1] + 10 ) + ",0,0,1," + arrowHelper( 170, 14 ) +
+		" L" + arrowHelper( 170, 18 ) +
+		" L" + arrowHelper( 167, 10 ) +
+		" L" + arrowHelper( 170, 2 ) +
+		" L" + arrowHelper( 170, 6 ) +
+		" A" + ( graph.scaleVector(r)[0] + 10 ) + "," + ( graph.scaleVector(r)[1] + 10 ) + ",0,0,0," + arrowHelper( 180, 6 ) +
+		" Z"
+	).attr({
+		"stroke": null,
+		"fill": KhanUtil.ORANGE
+	});
+
+	// add it to the set so it translates with everything else
+	this.set.push( arrow );
+
+	this.centerPoint = KhanUtil.addMovablePoint({
+		coord: center,
+		visible: false
+	});
+
+	// Use a movablePoint for rotation
+	this.rotateHandle = KhanUtil.addMovablePoint({
+		coord: [
+			Math.sin( 275 * Math.PI / 180 ) * ( r + 0.5 ) + this.cx,
+			Math.cos( 275 * Math.PI / 180 ) * ( r + 0.5 ) + this.cy
+		],
+		onMove: function( x, y ) {
+			var angle = Math.atan2( pro.centerPoint.coord[1] - y, pro.centerPoint.coord[0] - x) * 180 / Math.PI;
+			pro.rotate( -angle - 5, true );
+		}
+	});
+
+	// Add a constraint so the point moves in a circle
+	this.rotateHandle.constraints.fixedDistance.dist = r + 0.5;
+	this.rotateHandle.constraints.fixedDistance.point = this.centerPoint;
+
+	// Remove the default dot added by the movablePoint since we have our double-arrow thing
+	this.rotateHandle.visibleShape.remove();
+	// Make the mouse target bigger to encompass the whole area around the double-arrow thing
+	this.rotateHandle.mouseTarget.attr({ scale: 2.0 });
+
+	// Make the arrow-thing grow and shrink with mouseover/out
+	jQuery( this.rotateHandle.mouseTarget[0] ).bind( "vmouseover", function( event ) {
+		arrow.animate({ scale: 1.5 }, 50 );
+	});
+	jQuery( this.rotateHandle.mouseTarget[0] ).bind( "vmouseout", function( event ) {
+		arrow.animate({ scale: 1.0 }, 50 );
+	});
+
+
+	var setNodes = jQuery.map( this.set, function( el ) { return el.node; } );
+	this.makeTranslatable = function makeTranslatable() {
+		jQuery( setNodes ).css( "cursor", "move" );
+
+		jQuery( setNodes ).bind( "vmousedown", function( event ) {
+			event.preventDefault();
+			var startx = event.pageX;
+			var starty = event.pageY;
+
+			jQuery( setNodes ).animate( { opacity: 0.25 }, 150 );
+
+			jQuery( document ).bind ( "vmousemove", function( event ) {
+				var dx = event.pageX - startx;
+				var dy = event.pageY - starty;
+
+				jQuery.each( pro.set.items, function() {
+					this.translate( dx, dy );
+				});
+				pro.centerPoint.setCoord([ pro.centerPoint.coord[0] + dx / graph.scale[0], pro.centerPoint.coord[1] - dy / graph.scale[1] ]);
+				pro.rotateHandle.setCoord([ pro.rotateHandle.coord[0] + dx / graph.scale[0], pro.rotateHandle.coord[1] - dy / graph.scale[1] ]);
+				startx = event.pageX;
+				starty = event.pageY;
+			});
+
+			jQuery( document ).one( "vmouseup", function( event ) {
+				jQuery( setNodes ).animate( { opacity: 0.50 }, 150 );
+				jQuery( document ).unbind( "vmousemove" );
+			});
+		});
+	};
+
+
+	this.rotation = 0;
+
+	this.rotate = function( offset, absolute ) {
+		var center = graph.scalePoint( this.centerPoint.coord );
+
+		if ( absolute ) {
+			this.rotation = 0;
+		}
+
+		this.set.rotate( this.rotation + offset, center[0], center[1] );
+		this.rotation = this.rotation + offset;
+
+		return this;
+	};
+
+	this.moveTo = function moveTo( x, y ) {
+		var graph = KhanUtil.currentGraph;
+		var start = graph.scalePoint( pro.centerPoint.coord );
+		var end = graph.scalePoint([ x, y ]);
+		var time = KhanUtil.getDistance( start, end ) * 2;  // 2ms per pixel
+
+		jQuery({ x: start[0], y: start[1] }).animate({ x: end[0], y: end[1] }, {
+			duration: time,
+			step: function( now, fx ) {
+				var dx = 0;
+				var dy = 0;
+				if ( fx.prop === "x" ) {
+					dx = now - graph.scalePoint( pro.centerPoint.coord )[0];
+				} else if ( fx.prop === "y") {
+					dy = now - graph.scalePoint( pro.centerPoint.coord )[1];
+				}
+				jQuery.each( pro.set.items, function() {
+					this.translate( dx, dy );
+				});
+				pro.centerPoint.setCoord([ pro.centerPoint.coord[0] + dx / graph.scale[0], pro.centerPoint.coord[1] - dy / graph.scale[1] ]);
+				pro.rotateHandle.setCoord([ pro.rotateHandle.coord[0] + dx / graph.scale[0], pro.rotateHandle.coord[1] - dy / graph.scale[1] ]);
+			}
+		});
+	};
+
+	this.rotateTo = function rotateTo( angle ) {
+		if ( Math.abs( this.rotation - angle ) > 180 ) {
+			this.rotation += 360;
+		}
+		var time = Math.abs( this.rotation - angle ) * 5;  // 5ms per deg
+		jQuery({ 0: this.rotation }).animate({ 0: angle }, {
+			duration: time,
+			step: function( now, fx ) {
+				pro.rotate( now, true );
+				pro.rotateHandle.setCoord([
+					Math.sin( ( now + 275 ) * Math.PI / 180 ) * ( r + 0.5 ) + pro.centerPoint.coord[0],
+					Math.cos( ( now + 275 ) * Math.PI / 180 ) * ( r + 0.5 ) + pro.centerPoint.coord[1]
+				]);
+			}
+		});
+	};
+
+	this.set.attr( { opacity: 0.5 } );
+	this.makeTranslatable();
+	return this;
+}
