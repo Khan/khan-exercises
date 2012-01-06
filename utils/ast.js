83f5be440eee1ac7105fa096874c85def674c183
Jeff Dyer <jeffdyer@acm.org>
2012-01-06 14:38:44 -0800

    first crack at implementing the ast spec

--- utils/ast.js
+++ utils/ast.js
@@ -1,10 +1,7 @@
 /*
-   AST library for khan-exercises
+   ASTs for Khan Exercises
 
-   ASTs (abstract syntax trees) are used to store structured information about
-   exercises. The typical example is of an expression 1 + 1 = 2, which is stored
-   as ["=", ["+", 1, 1], 2]. Once constructed, accessor methods can be used to
-   get at the various sub trees.
+   @see http://artcompiler.org/articles/asts-for-khan-exercises.pdf
 
    var nid = ast.fromExpr(["=", ["+", 1, 1], 2]));
    var node = ast.node(nid);  // returns a node object corresponding to nid
@@ -42,12 +39,13 @@ jQuery.extend ( KhanUtil, {
 		var nodePool = [ "unused" ];	// nodePool[0] is reserved
 
 		// Wrapper for navigating ASTs
-		function Node (nid, model) {
-			
+		function NodeClass (model) {
+
 			jQuery.extend ( this, {
 
 				// n.node("lhs").numberValue()
 				node: function (selector, m) {
+					var nid = this.nid();
 					var newNid = nodePool[nid][selector];
 					KhanUtil.assert(selector in nodePool[nid], "invalid selector");
 					return new Node(newNid, (m===void 0) ? model: m);
@@ -109,10 +107,6 @@ jQuery.extend ( KhanUtil, {
 					return model;
 				},
 
-				nid: function () {
-					return nid;
-				},
-
 				nid: function (selector, m) {
 					if (selector===void 0) {
 						return nid;
@@ -126,215 +120,331 @@ jQuery.extend ( KhanUtil, {
 			} ) ;
 		}
 
+		function Node(n) {
+			this.op = n.op;
+			this.args = n.args;
+			this.nid  = n.nid;
+		}
+
+		Node.prototype = new NodeClass();
+
+
 		// maps for fast lookup of nodes
 		var numberMap = { };
 		var stringMap = { };
-		var unaryExprMap = { };
-		var binaryExprMap = { };
+		var nodeMap = { };
 
-		var Kind = {
-			UNARY: 1,
-			BINARY: 2,
-			VAR: 3,
-			INT: 4,
-			NUM: 5,
-			STR: 6,
-		};
-
-		var BinOp = {
+		var OpStr = {
 			ADD: "+",
 			SUB: "-",
-			MUL: "\\times",
-			DIV: "\\div",
+			MUL: "times",
+			DIV: "div",
+			FRAC: "frac",
 			EQL: "=",
-			ATAN2: "ATAN2",
-			CAH: "CAH",
-			SOH: "SOH",
-			TOA: "TOA",
+			ATAN2: "atan2",
+			SQRT: "sqrt",
+			PM: "pm",
+			SIN: "sin",
+			COS: "cos",
+			TAN: "tan",
+			SEC: "sec",
+			LN: "ln",
+			VAR: "var",
 			COMMA: ",",
 			POW: "^",
+			ABS: "abs",
+			PAREN: "()",
+			HIGHLIGHT: "hi",
 		};
 		
-		var UnOp = {
-			ADD: "+",
-			SUB: "-",
-			ABS: "ABS",
-			VAR: "VAR",
-			COS: "\\cos",
-			SIN: "\\sin",
-			TAN: "\\tan",
-			PAREN: "PAREN",
-			BRACKET: "BRACKET",
-			BRACE: "BRACE",
-			HIGHLIGHT: "HI",
-		};
-		
-		function strToOp(str) {
-			switch (str) {
-			case "*":
-				return BinOp.MUL;
-			case "/":
-				return BinOp.DIV;
-			case "COS":
-				return UnOp.COS;
-			case "SIN":
-				return UnOp.SIN;
-			case "TAN":
-				return UnOp.TAN;
-			default:
-				return str;  // probably doesn't need translation
-			}
-	 	};
-			
-			
-		// public ast interface
+		function fromLaTeX(str) {
+			return KhanUtil.parse(str).expr();
+		}
 
-		jQuery.extend ( this, {
+		function toLaTeX(n) {
+			return format(n, "Large", KhanUtil.BLUE);
+		}
 
-			Kind: Kind,
-			BinOp: BinOp,
-			UnOp: UnOp,
-			strToOp: strToOp,
-			
-			// Expression pool for mapping index to expressions
-			nodePool: nodePool,
+		var OpToLaTeX = { }
+		OpToLaTeX[OpStr.ADD] = "+";
+		OpToLaTeX[OpStr.SUB] = "-";
+		OpToLaTeX[OpStr.MUL] = "\\times";
+		OpToLaTeX[OpStr.DIV] = "\\div";
+		OpToLaTeX[OpStr.FRAC] = "\\frac";
+		OpToLaTeX[OpStr.EQL] = "=";
+		OpToLaTeX[OpStr.ATAN2] = "\\atan2";
+		OpToLaTeX[OpStr.POW] = "^";
+		OpToLaTeX[OpStr.PM] = "\\pm";
+		OpToLaTeX[OpStr.SIN] = "\\sin";
+		OpToLaTeX[OpStr.COS] = "\\cos";
+		OpToLaTeX[OpStr.TAN] = "\\tan";
+		OpToLaTeX[OpStr.SEC] = "\\sec";
+		OpToLaTeX[OpStr.LN] = "\\ln";
 
-			node: function (nid, model) {
-				return new Node(nid, model);
-			},
-
-			dumpAll: function () {
-				var s = "";
-				for (var i=1; i < nodePool.length; i++) {
-					s = s + "<p>" + i+": "+this.dump(i) + "</p>";
-				}
-				return s;
-			},
-
-			dump: function (nid) {
-				var node = this.nodePool[nid];
-				var s;
-				switch (node.kind) {
-				case this.Kind.UNARY:
-					s = "UNARY [ " + node.op + ", " + node.expr + " ]";
+		function format(n, textSize, color) {
+			var ast = KhanUtil.ast;
+			var text;
+			if (jQuery.type(n)==="object") {
+			    switch (n.op) {
+				case OpStr.VAR:
+					text = "\\color{"+color+"}{"+n.args[0]+"}";
 					break;
-				case this.Kind.BINARY:
-					s = "BINARY [ " + node.op + ", " + node.lhs + ", " + node.rhs + " ]";
-					break;
-				case this.Kind.INT:
-					s = "INT [ " + node.val + " ]";
-					break;
-				case this.Kind.NUM:
-					s = "NUM [ " + node.val + " ]";
-					break;
-				case this.Kind.STR:
-					s = "STR [ " + node.val + " ]";
-					break;
-				default:
-					s = "UNKNOWN KIND " + node;
-				}
-				return s;
-			},
-			
-			fromExpr: function (expr) {
-				switch (jQuery.type(expr)) {
-				case "number":
-					return this.numberLiteral(expr);
-				case "string":
-					//return parse(expr);
-					return this.stringLiteral(expr);
-				case "array":
-					switch (expr.length) {
-					case 1:
-						return fromExpr(expr[0]);
-					case 2:
-						return this.unaryExpr ( this.strToOp(expr[0]), this.fromExpr (expr[1]) );
-					case 3:
-						// normalize the case of x + -1 to x - 1
-						if (expr[0]==="+" &&
-							jQuery.type(expr[2])==="number" &&
-							expr[2] < 0) {
-							expr[0] = "-";
-							expr[2] = -1*expr[2]
-						}
-						return this.binaryExpr ( this.strToOp(expr[0]), this.fromExpr(expr[1]), this.fromExpr(expr[2]) );
-					default:
-						KhanUtil.assert( false, "ast.fromExpr(): Invalid case." );
-						return void 0;
+			    case OpStr.SUB:
+			    case OpStr.DIV:
+				case OpStr.PM:
+				    text = "\\"+textSize+"{\\color{"+color+"}{"+
+					    format(n.args[0], textSize, color)+"} \\color{#000}{"+ OpToLaTeX[n.op] +"}\\color{"+color+"}{"+
+					    format(n.args[1], textSize, color)+"}}";
+				    break;
+				case OpStr.POW:
+					var lhs = n.args[0];
+					var rhs = n.args[1];
+					var lhsText = format(lhs, textSize, color);
+					var rhsText = format(rhs, textSize, color);
+					// if subexpr is lower precedence, wrap in parens
+					if (lhs.op===OpStr.ADD || lhs.op===OpStr.SUB || 
+						lhs.op===OpStr.MUL || lhs.op===OpStr.DIV || 
+						lhs.op===OpStr.SQRT) {
+						lhsText = "\\"+textSize+"{\\color{#000}{(}}\\color{"+color+"}{"+lhsText+"}\\color{#000}{)}";
 					}
+					text = lhsText + "^{" +	rhsText + "}";
+					break;
+				case OpStr.SIN:
+				case OpStr.COS:
+				case OpStr.TAN:
+				case OpStr.SEC:
+				case OpStr.LN:
+					text = "\\"+textSize+"{\\color{"+"#000"+"}{"+
+						OpToLaTeX[n.op]+"{"+format(n.args[0], textSize, color)+"}}}";
+					break;
+				case OpStr.FRAC:
+					text = "\\"+textSize+"{\\color{"+"#000"+"}{"+
+						"\\dfrac{"+format(n.args[0], textSize, color)+
+						"}{" +format(n.args[1], textSize, color)+"}}}";
+					break;
+				case OpStr.SQRT:
+					switch (n.args.length) {
+					case 1:
+						text = "\\"+textSize+"{\\color{"+"#000"+"}{"+
+							"\\sqrt{"+format(n.args[0], textSize, color)+"}}}";
+						break;
+					case 2:
+						text = "\\"+textSize+"{\\color{"+"#000"+"}{"+
+							"\\sqrt["+format(n.args[0], textSize, color)+
+							"]{" +format(n.args[1], textSize, color)+"}}}";
+						break;
+					}
+					break;
+				case OpStr.COMMA:
+					text = "\\" + textSize + "{\\color{"+color + "}{" +
+						format(n.args[0], textSize, color) + "} \\ \\ \\color{" +
+						color + "}{" + format(n.args[1], textSize, color) + "}}";
+					break;
+				case OpStr.MUL:
+					var lhs = n.args[0];
+					var rhs = n.args[1];
+					if (jQuery.type(lhs)==="number" && lhs===1) {
+						// elide coefficient when it is 1.
+						text = format(rhs, textSize, color);
+					}
+					else if (jQuery.type(lhs)==="number" && lhs===-1) {
+						// elide coefficient when it is 1.
+						text = "\\color{#000}{-}"+format(rhs, textSize, color);
+					}
+					else if ((lhs.args && lhs.args.length===2) || (rhs.args && rhs.args.length===2)) {
+						var lhsText = format(lhs, textSize, color);
+						var rhsText = format(rhs, textSize, color);
+						// if subexpr is lower precedence, wrap in parens
+						if (lhs.op===OpStr.ADD || lhs.op===OpStr.SUB) {
+							lhsText = "\\"+textSize+"{\\color{#000}{(}}\\color{"+color+"}{"+lhsText+"}\\color{#000}{)}";
+						}
+						if (rhs.op===ast.OpStr.ADD || rhs.op===ast.OpStr.SUB) {
+							rhsText = "\\"+textSize+"{\\color{#000}{(}}\\color{"+color+"}{"+rhsText+"}\\color{#000}{)}";
+						}
+						text = lhsText + rhsText;
+					}
+					else if (rhs.op===OpStr.PAREN || jQuery.type(lhs==="number")) {
+						// elide the times symbol if rhs is parenthesized or lhs is a coef
+						text = "\\"+textSize+"{\\color{"+color+"}{"+
+							format(lhs, textSize, color)+"} \\color{"+color+"}{"+
+							format(rhs, textSize, color)+"}}";
+					}
+					else if (rhs.op===OpStr.PAREN || jQuery.type(rhs)==="string") {
+						// elide the times symbol if rhs is parenthesized or a var
+						text = "\\"+textSize+"{\\color{"+color+"}{"+
+							format(lhs, textSize, color)+"} \\color{"+color+"}{"+
+							format(rhs, textSize, color)+"}}";
+					}
+					else {
+						text = "\\"+textSize+"{\\color{"+color+"}{"+
+							format(lhs, textSize, color)+"} \\color{#000}{"+ OpToLaTeX[n.op] +"}\\color{"+color+"}{"+
+							format(rhs, textSize, color)+"}}";
+					}
+					break;
+				case OpStr.ADD:
+					var lhs = n.args[0];
+					var rhs = n.args[1];
+					if (isNeg(rhs)) {
+						// elide the plus symbol if the rhs has alreading minus.
+						text = "\\"+textSize+"{\\color{"+color+"}{"+
+							format(lhs, textSize, color)+"} \\color{"+color+"}{"+
+							format(rhs, textSize, color)+"}}";
+					}
+					else if (isZero(rhs)) {
+						// elide the operator and rhs.
+						text = format(lhs, textSize, color);
+					}
+					else {
+						text = "\\"+textSize+"{\\color{"+color+"}{"+
+							format(lhs, textSize, color)+"} \\color{#000}{"+ OpToLaTeX[n.op] +"}\\color{"+color+"}{"+
+							format(rhs, textSize, color)+"}}";
+					}
+					break;
 				default:
-					KhanUtil.assert( expr.constructor === Node, "ast.fromExpr(): invalid input" );
-					// already a Node so just return it.
-					return expr.nid();
+					text = "\\"+textSize+"{\\color{"+color+"}{"+
+						format(n.args[0], textSize, color)+"} \\color{#000}{"+ OpToLaTeX[n.op] +"}\\color{"+color+"}{"+
+						format(n.args[1], textSize, color)+"}}";
+					break;
 				}
-			},
+			}
+			else {
+				text = "\\color{"+color+"}{"+n+"}";
+			}
+			return text;
+		}
 
-			// Variable node
-			varExpr: function ( text, expr ) {
-				this.nodePool.push( { kind: this.Kind.VAR, text: text, expr: expr } );
-				return this.nodePool.length - 1 ;
-			},
-			
-			intExpr: function ( val ) {
-				this.nodePool.push ( { kind: this.Kind.INT, name: name, val: val } );
-				return this.nodePool.length - 1 ;
-			},
-			
-			numberLiteral: function ( val ) {
-				var nid = numberMap[val];
+
+		function isNeg(n) {
+			if (jQuery.type(n)==="number") {
+				return n < 0;
+			}
+			else {
+				return n.args.length===1 &&	n.op===OpStr.SUB && n.args[0] > 0;  // is unary minus
+			}
+		}
+
+		function isZero(n) {
+			if (jQuery.type(n)==="number") {
+				return n === 0;
+			}
+			else {
+				return n.args.length===1 &&	n.op===OpStr.SUB && n.args[0] === 0;  // is unary minus
+			}
+		}
+	
+		function eval(n) {
+			KhanUtil.assert(false);
+			return void 0;
+		}
+
+		function intern(n) {
+			// nodify number and string literals
+			if (jQuery.type(n) === "number") {
+				var nid = numberMap[n];
 				if (nid === void 0) {
-					this.nodePool.push ( { kind: this.Kind.NUM, val: val } );
-					nid = this.nodePool.length - 1 ;
-					numberMap[val] = nid;
+					nodePool.push({op:"num", args: [n]});
+					nid = nodePool.length - 1;
+					numberMap[n] = nid;
 				}
-				return nid;
-			},
-			
-			stringLiteral: function ( val ) {
-				var nid = stringMap[val];
+			}
+			else if (jQuery.type(n) === "string") {
+				var nid = stringMap[n];
 				if (nid === void 0) {
-					this.nodePool.push ( { kind: this.Kind.STR, val: val } );
-					nid = this.nodePool.length - 1 ;
-					stringMap[val] = nid;
+					nodePool.push({op:"str", args: [n]});
+					nid = nodePool.length - 1;
+					stringMap[n] = nid;
 				}
-				return nid;
-			},
-			
- 			// Unary expression node
-			unaryExpr: function ( opstr, expr ) {
-				var key = opstr+expr;
-				var nid = unaryExprMap[key];
-				if (nid === void 0) {
-					this.nodePool.push(
-						{ kind: this.Kind.UNARY
-						 , op: this.strToOp(opstr)
-						 , expr: expr
-						 , val: void 0
-						});
-					nid = this.nodePool.length - 1 ;
-					unaryExprMap[key] = nid;
+			}
+			else {
+				var op = n.op;
+				var count = n.args.length;
+				var args = "";
+				var args_nids = [ ];
+				for (var i=0; i < count; i++) {
+					args += args_nids[i] = intern(n.args[i]);
 				}
-				return nid;
-			},
-			
-			// Binary expression node
-			binaryExpr: function ( opstr, lhs, rhs ) {
-				var key = opstr+lhs+","+rhs;
-				var nid = binaryExprMap[key];
+				var key = op+count+args;
+				var nid = nodeMap[key];
 				if (nid === void 0) {
 					nodePool.push({
-						kind: Kind.BINARY,
-						op: strToOp(opstr),
-						lhs: lhs,
-						rhs: rhs,
-						val: void 0,
+						op: op,
+						args: args_nids,
 					});
 					nid = nodePool.length - 1 ;
-					binaryExprMap[key] = nid;
+					nodeMap[key] = nid;
 				}
-				return nid;
-			},
+			}
+			return nid;
+		}
 
+		function node(nid) {
+			var n = jQuery.extend(true, {}, nodePool[nid]);
+			// if literal, then unwrap.
+			switch (n.op) {
+			case "NUM":
+			case "STR":
+				n = n.args[0];
+				break;
+			default:
+				for (var i=0; i < n.args.length; i++) {
+					n.args[i] = node(n.args[i]);
+				}
+				break;
+			}
+			return n;
+		}
+
+		function dumpAll() {
+			var s = "";
+			for (var i=1; i < nodePool.length; i++) {
+				var n = nodePool[i];
+				s = s + "<p>" + i+": "+dump(n) + "</p>";
+			}
+			return s;
+		}
+
+		function dump(n) {
+			
+			if (jQuery.type(n) === "object") {
+				switch (n.op) {
+				case "num":
+					var s = n.args[0];
+					break;
+				case "str":
+					var s = "\""+n.args[0]+"\"";
+					break;
+				default:
+					var s = "{ op: \"" + n.op + "\", args: [ ";
+					for (var i=0; i < n.args.length; i++) {
+						if (i > 0) {
+							s += " , ";
+						}
+						s += dump(n.args[i]);
+					}
+					s += " ] }";
+					break;
+				}
+			}
+			else if (jQuery.type(n)==="string") {
+				var s = "\""+n+"\"";
+			}
+			else {
+				var s = n;
+			}
+			return s;
+		}
+
+		// KhanUtil.ast public interface
+
+		jQuery.extend ( this, {
+			OpStr: OpStr,
+			fromLaTeX: fromLaTeX,			
+			toLaTeX: toLaTeX,
+			eval: eval,
+			intern: intern,
+			node: node,
+			dump: dump,
+			dumpAll: dumpAll,
 		} );
 
 		return this;
--- utils/parse.js
+++ utils/parse.js
@@ -5,88 +5,61 @@
 
 jQuery.extend ( KhanUtil, {
 
-
-	// scan expression
-	scan : function (src) {
-
-		var TK_NONE = 0;
-		var TK_COEFF = 'A'.charCodeAt(0);
-		var TK_VAR = 'a'.charCodeAt();
-		var TK_NUM = '0'.charCodeAt();
-		var curIndex = 0;
-		var lexeme = "";
-
-		function start () {
-			var c;
-			lexeme = "";
-			while (curIndex < src.length) {
-				switch ((c = src.charCodeAt(curIndex++))) {
-				case 32:  // space
-				case 9:   // tab
-				case 10:  // new line
-				case 13:  // carriage return
-					continue;
-				case 40:  // TK_LEFTPAREN;
-				case 41:  // TK_RIGHTPAREN;
-				case 42:  // TK_MUL;
-				case 43:  // TK_ADD;
-				case 44:  // TK_COMMA;
-				case 45:  // TK_SUB;
-				case 47:  // TK_DIV;
-				case 94:  // TK_CARET
-					lexeme += String.fromCharCode(c);
-					return c; // char code is the token id
-				default:
-					if (c >= 'A'.charCodeAt(0) && c <= 'Z'.charCodeAt(0)) {
-						lexeme += String.fromCharCode(c);
-						return TK_COEFF;
-					}
-					else if (c >= 'a'.charCodeAt(0) && c <= 'z'.charCodeAt(0)) {
-						lexeme += String.fromCharCode(c);
-						return TK_VAR;
-					}
-					else if (c >= '0'.charCodeAt(0) && c <= '9'.charCodeAt(0)) {
-						//lexeme += String.fromCharCode(c);
-						//c = src.charCodeAt(curIndex++);
-						//return TK_NUM;
-						return number(c);
-					}
-					else {
-						KhanUtil.assert( false, "scan.start(): c="+c);
-						return 0;
-					}
-				}
-			}
-			return 0;
-		}
-
-		function number(c) {
-			while (c >= '0'.charCodeAt(0) && c <= '9'.charCodeAt(0)) {
-				lexeme += String.fromCharCode(c);
-				c = src.charCodeAt(curIndex++);
-			}
-			curIndex--;
-			
-			return TK_NUM;
-		}
-
-		return {
-			start : start ,
-
-			lexeme : function () { return lexeme } ,
-		}
-
-		return this;
-	} ,
-
-	// parse expressions
 	parse : function (src) {
+
+		var TK_NONE         = 0;
+		var TK_ADD          = '+'.charCodeAt(0);
+		var TK_CARET        = '^'.charCodeAt(0);
+		var TK_COS          = 0x105;
+		var TK_DIV          = '/'.charCodeAt(0);
+		var TK_FRAC         = 0x100;
+		var TK_LN           = 0x107;
+		var TK_LEFTBRACE    = '{'.charCodeAt(0);
+		var TK_LEFTBRACKET  = '['.charCodeAt(0);
+		var TK_LEFTPAREN    = '('.charCodeAt(0);
+		var TK_MUL          = '*'.charCodeAt(0);
+		var TK_NUM          = '0'.charCodeAt(0);
+		var TK_PM           = 0x102;
+		var TK_RIGHTBRACE   = '}'.charCodeAt(0);
+		var TK_RIGHTBRACKET = ']'.charCodeAt(0);
+		var TK_RIGHTPAREN   = ')'.charCodeAt(0);
+		var TK_SEC          = 0x106;
+		var TK_SIN          = 0x103;
+		var TK_SQRT         = 0x101;
+		var TK_SUB          = '-'.charCodeAt(0);
+		var TK_TAN          = 0x104;
+		var TK_VAR          = 'a'.charCodeAt(0);
+		var TK_COEFF        = 'A'.charCodeAt(0);
+		var TK_VAR          = 'a'.charCodeAt(0);
+		var TK_NEXT         = 0x108;
+
 		var util = KhanUtil;
 		var ast = util.ast;
-		var scan = util.scan(src);
-		var TK_NONE = 0;
-		var T0=TK_NONE, T1=TK_NONE;
 		var tokenToOperator = [ ];
+
+		var T0=TK_NONE, T1=TK_NONE;
+
+		tokenToOperator[TK_FRAC]  = ast.OpStr.FRAC;
+		tokenToOperator[TK_SQRT]  = ast.OpStr.SQRT;
+		tokenToOperator[TK_ADD]   = ast.OpStr.ADD;
+		tokenToOperator[TK_SUB]   = ast.OpStr.SUB;
+		tokenToOperator[TK_PM]    = ast.OpStr.PM;
+		tokenToOperator[TK_CARET] = ast.OpStr.POW;
+		tokenToOperator[TK_MUL]   = ast.OpStr.MUL;
+		tokenToOperator[TK_DIV]   = ast.OpStr.DIV;
+		tokenToOperator[TK_SIN]   = ast.OpStr.SIN;
+		tokenToOperator[TK_COS]   = ast.OpStr.COS;
+		tokenToOperator[TK_TAN]   = ast.OpStr.TAN;
+		tokenToOperator[TK_SEC]   = ast.OpStr.SEC;
+		tokenToOperator[TK_LN]    = ast.OpStr.LN;
+
+		var scan = scanner(src);
+
+		return {
+			expr : expr ,
+		};
+
+		// begin private functions
 
 		function start() {
 			T0 = scan.start();
@@ -137,30 +110,51 @@ jQuery.extend ( KhanUtil, {
 			return true;
 		}
 
-		var TK_NUM = '0'.charCodeAt(0);
-		var TK_LEFTPAREN = '('.charCodeAt(0);
-		var TK_RIGHTPAREN = ')'.charCodeAt(0);
-
 		function primaryExpr () {
 			var e;
 			var t;
-			switch (hd()) {
+			var op;
+			switch ((t=hd())) {
 			case 'A'.charCodeAt(0):
-				e = ast.stringLiteral(lexeme());
+				e = {op: "var", args: [lexeme()]};
 				next();
 				break;
 			case 'a'.charCodeAt(0):
-				e = ast.stringLiteral(lexeme());
+				e = {op: "var", args: [lexeme()]};
 				next();
 				break;
 			case TK_NUM:
-				e = ast.numberLiteral(lexeme());
+				e = Number(lexeme());
 				next();
 				break;
 			case TK_LEFTPAREN:
+				e = parenExpr();
+				break;
+			case TK_FRAC:
 				next();
-				e = commaExpr();
-				eat(TK_RIGHTPAREN);
+				e = {op: tokenToOperator[TK_FRAC], args: [braceExpr(), braceExpr()]};
+				break;
+			case TK_SQRT:
+				next();
+				switch(hd()) {
+				case TK_LEFTBRACKET:
+					e = {op: tokenToOperator[TK_SQRT], args: [bracketExpr(), braceExpr()]};
+					break;
+				case TK_LEFTBRACE:
+					e = {op: tokenToOperator[TK_SQRT], args: [braceExpr()]};
+					break;
+				default:
+					assert(false);
+					break;
+				}
+				break;
+			case TK_SIN:
+			case TK_COS:
+			case TK_TAN:
+			case TK_SEC:
+			case TK_LN:
+				next();
+				e = {op: tokenToOperator[t], args: [braceExpr()]};
 				break;
 			default:
 				e = void 0;
@@ -169,10 +163,26 @@ jQuery.extend ( KhanUtil, {
 			return e;
 		}
 
-		var TK_ADD = '+'.charCodeAt(0);
-		var TK_SUB = '-'.charCodeAt(0);
-		tokenToOperator[TK_ADD] = ast.BinOp.ADD;
-		tokenToOperator[TK_SUB] = ast.BinOp.SUB;
+		function braceExpr() {
+			eat(TK_LEFTBRACE);
+			var e = commaExpr();
+			eat(TK_RIGHTBRACE);
+			return e;
+		}
+
+		function bracketExpr() {
+			eat(TK_LEFTBRACKET);
+			var e = commaExpr();
+			eat(TK_RIGHTBRACKET);
+			return e;
+		}
+
+		function parenExpr() {
+			eat(TK_LEFTPAREN);
+			var e = commaExpr();
+			eat(TK_RIGHTPAREN);
+			return e;
+		}
 
 		function unaryExpr() {
 			var t;
@@ -181,7 +191,7 @@ jQuery.extend ( KhanUtil, {
 			case TK_ADD:
 			case TK_SUB:
 				next();
-				expr = ast.unaryExpr(tokenToOperator[t], unaryExpr());
+				expr = {op: tokenToOperator[t], args: [unaryExpr()]};
 				break;
 			default:
 				expr = primaryExpr();
@@ -190,38 +200,28 @@ jQuery.extend ( KhanUtil, {
 			return expr;
 		}
 
-		var TK_CARET = '^'.charCodeAt(0);
-		tokenToOperator[TK_CARET] = ast.BinOp.POW;
-
 		function exponentialExpr() {
 			var expr = unaryExpr();
 			var t;
 			while ((t=hd())===TK_CARET) {
 				next();
-				expr = ast.binaryExpr(tokenToOperator[t], expr, unaryExpr());
+				expr = {op: tokenToOperator[t], args: [expr, unaryExpr()]};
 			}
 
 			return expr;
 		}
 
-		var TK_VAR = 'a'.charCodeAt(0);
-		var TK_LEFTPAREN = '('.charCodeAt(0);
-		var TK_MUL = '*'.charCodeAt(0);
-		var TK_DIV = '/'.charCodeAt(0);
-		tokenToOperator[TK_MUL] = ast.BinOp.MUL;
-		tokenToOperator[TK_DIV] = ast.BinOp.DIV;
-		
 		function multiplicativeExpr() {
 			var e = exponentialExpr();
 			var t;
 
 			while((t=hd())===TK_VAR || t===TK_LEFTPAREN) {
-				e = ast.binaryExpr(ast.BinOp.MUL, e, exponentialExpr());
+				e = {op: ast.OpStr.MUL, args: [e, exponentialExpr()]};
 			}
 
 			while (isMultiplicative(t = hd())) {
 				next();
-				e = ast.binaryExpr(tokenToOperator[t], e, exponentialExpr());
+				e = {op: tokenToOperator[t], args: [e, exponentialExpr()]};
 			}
 			return e;
 
@@ -235,12 +235,12 @@ jQuery.extend ( KhanUtil, {
 			var t;
 			while (isAdditive(t = hd())) {
 				next();
-				expr = ast.binaryExpr(tokenToOperator[t], expr, multiplicativeExpr());
+			    expr = {op: tokenToOperator[t], args: [expr, multiplicativeExpr()]};
 			}
 			return expr;
 
 			function isAdditive(t) {
-				return t===TK_ADD || t===TK_SUB;
+				return t===TK_ADD || t===TK_SUB || t===TK_PM;
 			}
 		}
 
@@ -255,9 +255,107 @@ jQuery.extend ( KhanUtil, {
 			return n;
 		}
 
-		return {
-			expr : expr ,
-		};
+		function scanner(src) {
 
-	} ,
+			var curIndex = 0;
+			var lexeme = "";
+
+			var lexemeToToken = [ ];
+
+			lexemeToToken["\\times"] = TK_MUL;
+			lexemeToToken["\\div"]   = TK_DIV;
+			lexemeToToken["\\frac"]  = TK_FRAC;
+			lexemeToToken["\\sqrt"]  = TK_SQRT;
+			lexemeToToken["\\pm"]    = TK_PM;
+			lexemeToToken["\\sin"]   = TK_SIN;
+			lexemeToToken["\\cos"]   = TK_COS;
+			lexemeToToken["\\tan"]   = TK_TAN;
+			lexemeToToken["\\sec"]   = TK_SEC;
+			lexemeToToken["\\ln"]    = TK_LN;
+
+			return {
+				start : start ,
+				lexeme : function () { return lexeme } ,
+			}
+
+			// begin private functions
+
+			function start () {
+				var c;
+				lexeme = "";
+				while (curIndex < src.length) {
+					switch ((c = src.charCodeAt(curIndex++))) {
+					case 32:  // space
+					case 9:   // tab
+					case 10:  // new line
+					case 13:  // carriage return
+						continue;
+					case 92:  // backslash
+						lexeme += String.fromCharCode(c);
+						return latex();
+					case 40:  // left paren
+					case 41:  // right paren
+					case 42:  // asterisk
+					case 43:  // plus
+					case 44:  // comma
+					case 45:  // dash
+					case 47:  // slash
+					case 91:  // left bracket
+					case 93:  // right bracket
+					case 94:  // caret
+					case 123: // left brace
+					case 125: // right brace
+						lexeme += String.fromCharCode(c);
+						return c; // char code is the token id
+					default:
+						if (c >= 'A'.charCodeAt(0) && c <= 'Z'.charCodeAt(0)) {
+							lexeme += String.fromCharCode(c);
+							return TK_COEFF;
+						}
+						else if (c >= 'a'.charCodeAt(0) && c <= 'z'.charCodeAt(0)) {
+							lexeme += String.fromCharCode(c);
+							return TK_VAR;
+						}
+						else if (c >= '0'.charCodeAt(0) && c <= '9'.charCodeAt(0)) {
+							//lexeme += String.fromCharCode(c);
+							//c = src.charCodeAt(curIndex++);
+							//return TK_NUM;
+							return number(c);
+						}
+						else {
+							KhanUtil.assert( false, "scan.start(): c="+c);
+							return 0;
+						}
+					}
+				}
+				return 0;
+			}
+
+			function number(c) {
+				while (c >= '0'.charCodeAt(0) && c <= '9'.charCodeAt(0)) {
+					lexeme += String.fromCharCode(c);
+					c = src.charCodeAt(curIndex++);
+				}
+				curIndex--;
+				
+				return TK_NUM;
+			}
+
+			function latex() {
+				var c = src.charCodeAt(curIndex++);
+				while (c >= 'a'.charCodeAt(0) && c <= 'z'.charCodeAt(0)) {
+					lexeme += String.fromCharCode(c);
+					c = src.charCodeAt(curIndex++);
+				}
+				curIndex--;
+
+				var tk = lexemeToToken[lexeme];
+				if (tk===void 0) {
+					tk = TK_VAR;   // e.g. \\theta
+				}
+				return tk;
+			}
+
+		}
+	},
 });
