7b3cef472b4404962f283ed415610073f751622b
RonaldPerceptum <ronald@perceptum.nl>
2012-01-10 16:11:25 +0100

    improved translations
    Signed-off-by: RonaldPerceptum <ronald@perceptum.nl>

--- exercises/recognizing_fractions_0.5.lang.js
+++ exercises/recognizing_fractions_0.5.lang.js
@@ -4,7 +4,7 @@
 		"exercise2"	: 'Welk gedeelte van deze staaf is rood gekleurd?',
 		"hint1"		: 'Tel de rode partjes om de teller te bepalen.',
 		"hint2"		: 'Er <var>plural( "is", NUM_1 )</var> <var>plural( NUM_1, "rood partje", "rode partjes")</var>.',
-		"hint3"		: 'Tel het totale aantal partjes (<code>\color{#e00}{\text{rode partjes}}</code> + <code>\color{#999}{\text{grijze partjes}}</code>) om de noemer te bepalen.',
+		"hint3"		: 'Tel het totale aantal partjes (<code>\color{#e00}{\text{Red Slices}}</code> + <code>\color{#999}{\text{Gray Slices}}</code>) om de noemer te bepalen.',
 		"hint4"		: 'Er zijn totaal <var>DEN_1</var> partjes.',
 		"hint5"		: 'Het antwoord moet dus zijn <code>\dfrac{\color{#e00}{<var>NUM_1</var>}}{\color{#666}{\text{<var>DEN_1</var>}}}</code>.',
 		"hint6"		: 'Tel de rode blokjes om de teller te bepalen.',
