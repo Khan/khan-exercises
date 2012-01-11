c89f662be5b521573ea7143aa9e8530915905684
Wijnand Baretta <wijnand@perceptum.nl>
2012-01-11 22:20:00 +0100

    reading_pictographs translate

--- exercises/reading_pictographs_1.lang.js
+++ exercises/reading_pictographs_1.lang.js
@@ -1,7 +1,7 @@
 ({
 	"nl" : {
 		"question1"	: 'Gebruik de infographic om onderstaande vraag te beantwoorden:',
-		"question2"	: ' <i>badges</i> verdiend  per student',
+		"question2"	: '<span><span style="text-transform:capitalize"><var>BADGE</var></span> badges verdiend per student</span>',
 		"question3"	: 'Wie heeft de meeste <var>BADGE</var> badges verdiend?',
 		"question4"	: 'Wie heeft de minste <var>BADGE</var> badges verdiend?',
 		"question5"	: 'Hoeveel <var>BADGE</var> badges heeft <var>person( PERSON + 1 )</var> verdiend?',
@@ -12,7 +12,7 @@
 		"hint5"		: '<var>person( LEAST + 1 )</var> heeft de minste symbolen naast <var>his( LEAST + 1)</var> naam staan.',
 		"hint6"		: '<var>person( LEAST + 1 )</var> heeft de minste <var>BADGE</var> badges verdiend.',
 		"hint7"		: 'Onderaan de infographic staat dat elk symbool een aantal van <var>plural( VALUE_PER_IMG, "badge" )</var> vertegenwoordigd.',
-		"hint8"		: "Kijk naar de rij van <var>person( PERSON + 1 )</var> in de tabel: "+
+		"hint8"		: 'Kijk naar de rij van <var>person( PERSON + 1 )</var> in de tabel: '+
 					'<div class="fake_row">'+
 						'<span><var>person( PERSON + 1 )</var></span><span data-each="DATA[ PERSON ] times"><var>FULL_IMAGE</var> '+
 						'</span><span data-each="(8 - DATA[ PERSON ]) times">&nbsp;</span> '+
