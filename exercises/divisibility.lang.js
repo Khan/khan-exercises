dde4ae4539b1886c0670b1d34dc277666b163c5f
Wijnand Baretta <wijnand@perceptum.nl>
2012-01-13 00:46:21 +0100

    translation divisibility

--- exercises/divisibility.lang.js
+++ exercises/divisibility.lang.js
@@ -1,11 +1,11 @@
 ({
 	"nl" : {
-		"question1"	: 'All numbers divisible by <code><var>A</var></code> and <code><var>B</var></code> are also divisible by which of the following?',
+		"question1"	: 'Alle getallen deelbaar door  <code><var>A</var></code> en <code><var>B</var></code> zijn ook deelbaar door welk getal?',
 		"question2"	: '<li data-each="WRONGS as index, wrong"><code><var>wrong</var></code></li>',
 		
-		"hint1"		: 'The prime factorization of <code><var>A</var></code> is <code><var>getPrimeFactorization( A ).join( "\\times" )</var></code>.',
-		"hint2"		: 'The prime factorization of <code><var>B</var></code> is <code><var>getPrimeFactorization( B ).join( "\\times" )</var></code>.',
-		"hint3"		: 'So, any number divisible by both must have <code><var>LCM_FACTORIZATION.join( "\\times" )</var></code> as part of its prime factorization.',
-		"hint4"		: 'Such a number is divisible by <code><var>ANSWER</var></code>, since the prime factorization of <code><var>ANSWER</var></code> is <code><var>getPrimeFactorization( ANSWER ).join( "\\times" )</var></code>.'
+		"hint1"		: '<code><var>A</var></code> ontbinden in priemfactoren geeft <code><var>getPrimeFactorization( A ).join( "\\\\times" )</var></code>.',
+		"hint2"		: '<code><var>B</var></code> ontbinden in priemfactoren geeft <code><var>getPrimeFactorization( B ).join( "\\\\times" )</var></code>.',
+		"hint3"		: 'Dus elk getal deelbaar door zowel <code><var>A</var></code> als <code><var>B</var></code> moet een gedeelte van <code><var>LCM_FACTORIZATION.join( "\\\\times" )</var></code> als priemfactoren hebben.',
+		"hint4"		: 'Zo\'n getal is ook deelbaar door <code><var>ANSWER</var></code>, omdat <code><var>getPrimeFactorization( ANSWER ).join( "\\\\times" )</var></code> de priemfactoren van <code><var>ANSWER</var></code> zijn.'
 		}
 })
