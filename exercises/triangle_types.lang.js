62b0ab344496e455b64bb542743f0300407494e4
RonaldPerceptum <ronald@perceptum.nl>
2012-01-16 14:48:24 +0100

    Triangle types fix
    Signed-off-by: RonaldPerceptum <ronald@perceptum.nl>

--- exercises/triangle_types.lang.js
+++ exercises/triangle_types.lang.js
@@ -1,6 +1,20 @@
 ({
 	"nl" : {
 		"question1"	: 'Is deze driehoek gelijkzijdig, gelijkbenig of ongelijkzijdig?',
+			"vars1"	: '<var id="TYPES">[ "Equilateral", "Isosceles", "Scalene" ]</var>'+
+			'<var id="TYPE">randFromArray( TYPES )</var>'+
+			'<var id="TRIANGLE">function(){'+
+								'if ( TYPE === "Equilateral" ){'+
+									'return eqTriangle();'+
+								'}'+
+								'if ( TYPE === "Isosceles" ){'+
+									'return isoTriangle();'+
+								'}'+
+								'if ( TYPE === "Scalene" ){'+
+									'return scTriangle();'+
+								'}'+
+							'}()'+ 
+			'</var>',
 		
 		"hint1"		: 'We weten al twee hoeken van deze driehoek, dus we kunnen de derde hoek berekenen door af te trekken van <code>180</code>',
 		"hint2"		: 'Omdat alledrie de hoeken verschillend zijn, is deze driehoek ongelijkzijdig.',
