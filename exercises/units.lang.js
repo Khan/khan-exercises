832c9b7b09aed32f58684f76929fd441a302a024
Harm-Simon Hegge <harm@perceptum.nl>
2012-01-18 12:38:29 +0100

    Units vertaald

--- exercises/units.lang.js
+++ exercises/units.lang.js
@@ -1,28 +1,28 @@
 ({
 	"nl" : {
 		"question1"		: '['+
-					'{ str: "milli", math: "\\dfrac{1}{1000}", inverse: "1000" },'+
-					'{ str: "centi", math: "\\dfrac{1}{100}", inverse: "100" },'+
-					'{ str: "deci", math: "\\dfrac{1}{10}", inverse: "10" },'+
+					'{ str: "milli", math: "\\\\dfrac{1}{1000}", inverse: "1000" },'+
+					'{ str: "centi", math: "\\\\dfrac{1}{100}", inverse: "100" },'+
+					'{ str: "deci", math: "\\\\dfrac{1}{10}", inverse: "10" },'+
 					'{ str: "", math: "1" },'+
-					'{ str: "deca", math: "10", inverse: "\\dfrac{1}{10}" },'+
-					'{ str: "hecto", math: "100", inverse: "\\dfrac{1}{100}" },'+
-					'{ str: "kilo", math: "1000", inverse: "\\dfrac{1}{1000}" } ]',
+					'{ str: "deca", math: "10", inverse: "\\\\dfrac{1}{10}" },'+
+					'{ str: "hecto", math: "100", inverse: "\\\\dfrac{1}{100}" },'+
+					'{ str: "kilo", math: "1000", inverse: "\\\\dfrac{1}{1000}" } ]',
 		"question2"		: 'shuffle( [ "meter", "liter", "gram", "watt" ] ).shift()',
-		"question3"		: 'How many <var>END_UNIT</var>s are in <var>QUANTITY + " " + START_UNIT</var>s?',
-		"question4"		: 'The following table shows a few prefixes and their meanings.',
-		"question5"		: '<th>Prefix</th><th>Meaning</th>',
-		"question6"		: 'Looking at the table, we see that 1 <var>START_UNIT</var> corresponds to <code><var>START_PREFIX.math</var></code> <var>UNIT</var>s.',
-		"question7"		: 'We can also see that, 1 <var>END_UNIT</var> corresponds to <code><var>END_PREFIX.math</var></code> <var>UNIT</var>s.',
+		"question3"		: 'Hoeveel <var>END_UNIT</var> zit er in <var>QUANTITY + " " + START_UNIT</var>?',
+		"question4"		: 'In de tabel staan de voorvoegsels en hun betekenis.',
+		"question5"		: '<th>Voorvoegsel &nbsp;&nbsp;</th><th>hoeveelheid</th>',
+		"question6"		: 'In de tabel zie je dat 1 <var>START_UNIT</var> overeenkomt met <code><var>START_PREFIX.math</var></code> <var>UNIT</var>.',
+		"question7"		: 'We zien ook dat 1 <var>END_UNIT</var> overeenkomt met <code><var>END_PREFIX.math</var></code> <var>UNIT</var>.',
 		"question8"		: '['+
-					'{ str: "mile", plural: "miles", multiplier: 63360 },'+
-					'{ str: "foot", plural: "feet", multiplier: 12 },'+
-					'{ str: "inch", plural: "inches", multiplier: 1 } ]',
+					'{ str: "kilometer", plural: "kilometer", multiplier: 100000 },'+
+					'{ str: "meter", plural: "meter", multiplier: 100 },'+
+					'{ str: "centimeter", plural: "centimeter", multiplier: 1 } ]',
 		"question9"		: '['+
-					'{ str: "hour", plural: "hours", multiplier: 3600 },'+
-					'{ str: "minute", plural: "minutes", multiplier: 60 },'+
-					'{ str: "second", plural: "seconds", multiplier: 1 }'+
+					'{ str: "uur", plural: "uur", multiplier: 3600 },'+
+					'{ str: "minuut", plural: "minuten", multiplier: 60 },'+
+					'{ str: "seconde", plural: "seconden", multiplier: 1 }'+
 				']',
-		"question10"	: 'An alien rocketship is traveling at a speed of <code><var>commafy( START_SPEED )</var></code> <var>D_UNITS[START_D_INDEX].plural</var> per <var>T_UNITS[START_T_INDEX].str</var>. At this speed, how many <var>D_UNITS[END_D_INDEX].plural</var> will it travel in 1 <var>T_UNITS[END_T_INDEX].str</var>? Round to the nearest thousandth.'
+		"question10"	: 'Een buitenaards ruimteschip beweegt met een snelheid van <code><var>commafy( START_SPEED )</var></code> <var>D_UNITS[START_D_INDEX].plural</var> per <var>T_UNITS[START_T_INDEX].str</var> door de ruimte.<br/>Hoeveel <var>D_UNITS[END_D_INDEX].plural</var> legt het ruimteschip bij deze snelheid af in 1 <var>T_UNITS[END_T_INDEX].str</var>? Rond af op duizendste nauwkeurig.'
 		}
 })
