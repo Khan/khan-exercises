835a505b48e3cc158f81d0b5b661af3162fa741d
Harm-Simon Hegge <harm@perceptum.nl>
2012-01-20 14:24:01 +0100

    order of werkwoorden changed

--- utils/NL.js
+++ utils/NL.js
@@ -2,16 +2,12 @@ jQuery.extend( KhanUtil, {
 
 	NL: {
 		werkwoordmap: [
-				//Type 1 Stam is werkwoord zonder EN op het einde
 				//Type 1
 			[
-				["fietsen", "fiets"]
-				// Type 2 Stam zonder EN op het einde, open klinker (LO-PEN) wordt dubbele klinker (LOOP)
 				["lopen", "loop"],
 				["kopen", "koop"],
 				["programmeren", "programmeer"]
 			],
-				// Type 3 Stam zonder EN op het einde, dubbele medeklinker wordt enkele medeklinker
 				// Type 2
 			[
 				["fietsen", "fiets"]
