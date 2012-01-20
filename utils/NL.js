9e63e14f59c02744ae7ea51a929c9c8d2ed2fc8d
Harm-Simon Hegge <harm@perceptum.nl>
2012-01-20 14:19:48 +0100

    NL.js

--- utils/NL.js
+++ utils/NL.js
@@ -2,25 +2,22 @@ jQuery.extend( KhanUtil, {
 
 	NL: {
 		werkwoordmap: [
-				//Type 1
+				//Type 1 Stam is werkwoord zonder EN op het einde
 			[
+				["fietsen", "fiets"]
+			],
+				// Type 2 Stam zonder EN op het einde, open klinker (LO-PEN) wordt dubbele klinker (LOOP)
+			[
+
 				["lopen", "loop"],
 				["kopen", "koop"],
 				["programmeren", "programmeer"]
 			],
-				// Type 2
-			[
-				["fietsen", "fiets"]
-			],
-				// Type 3
+				// Type 3 Stam zonder EN op het einde, dubbele medeklinker wordt enkele medeklinker
 			[
 				["kennen", "ken"],
 				["kunnen", "kun"],
 				["willen", "wil"]
-			],
-				// Type 4
-			[
-				["ben", "zijn"]
 			]
 		]
 	},
