5e677004f18999e0e06a05ec85ddc2b2d74b6749
RonaldPerceptum <ronald@perceptum.nl>
2012-01-20 14:24:30 +0100

    expressing ratios as fractions fix
    Signed-off-by: RonaldPerceptum <ronald@perceptum.nl>

--- exercises/expressing_ratios_as_fractions.lang.js
+++ exercises/expressing_ratios_as_fractions.lang.js
@@ -5,7 +5,7 @@
 		"hint1"		: 'Verhoudingen (of ratio\'s) kunnen op verschillende manieren geschreven worden.',
 		"hint2"		: 'Je kunt een verhouding schrijven als twee getallen, gescheiden door een dubbele punt.',
 		"hint3"		: 'Je kunt het schrijven in een zin, zoals:',
-		"hint4"		: '<code><var>NUM</var> \text{ staat tot } <var>DENOM</var></code>',
+		"hint4"		: '<code><var>NUM</var>\ { staat tot } <var>DENOM</var></code>',
 		"hint5"		:'Of je kan de verhoudsing schrijven als een breuk.',
 		"hint6"		: 'De verhouding van <var>fruit( 0 )</var>s tot <var>fruit( 1 )</var>s gechreven als een vereenvoudigde breuk is <code><var>fractionReduce( NUM, DENOM )</var></code>.'
 		}
