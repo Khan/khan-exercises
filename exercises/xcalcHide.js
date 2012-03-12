7d7b2b9d432dbf4b64ba9b84ce3f3a0794f12a08
Elizabeth Yip <elyip.dembart@gmail.com>
2012-03-11 18:18:39 -0700

    update A_variance,add,binomial,poisson, calculator

---
+++ exercises/xcalcHide.js
@@ -0,0 +1,8 @@
+function xcalcHide(){
+     if (document.getElementById('xcalc').style.visibility == 'hidden') {
+       document.getElementById('xcalc').style.visibility = 'visible';
+       }
+       else {
+document.getElementById('xcalc').style.visibility = 'hidden';
+        }
+      } 
