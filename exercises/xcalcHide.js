d93400733e9ce314ba7dffee0f42afc0c958b9b8
Elizabeth Yip <elyip.dembart@gmail.com>
2012-03-13 00:10:15 -0700

    replace iframe with form

--- exercises/xcalcHide.js
+++ exercises/xcalcHide.js
@@ -6,3 +6,25 @@ function xcalcHide(){
 document.getElementById('xcalc').style.visibility = 'hidden';
         }
       } 
+function sq(x){
+    return x*x;}
+function sqrt(x){
+  return Math.sqrt(x);}
+function pow(y,x){    
+    return Math.pow(y,x);}
+function exp(x){
+    return Math.exp(x);}
+function log(x){
+    return Math.log(x);}
+function fact(x){
+   if (x == 0  || x == 1)
+       return 1;
+   var ans = x*fact(x-1);
+   return ans;}
+function Results (form) {
+    var TestVar = form.inputbox.value;
+    Ans=eval(TestVar);
+    form.inputbox.value=(TestVar + " = " + Ans);
+    return Ans;}
+function Klear (form){
+    form.inputbox.value=("");}
