66235468f515772a2c27cc0b171c5c6d0cc14201
Emily Eisenberg <xymostech@gmail.com>
2012-06-14 15:12:56 -0700

    Refactor angsegcongruency to make it more general
    Now, lines are defined separately, and angle clicky-things are defined
    by their intersection with two lines. This will provide for more general
    congruency exercises

--- utils/angsegcongruency.js
+++ utils/angsegcongruency.js
@@ -1,188 +1,251 @@
 $.extend(KhanUtil, {
-    addAngles: function(options) {
-        var angles = $.extend(true, {
+    addCongruency: function(options) {
+        var congruency = $.extend(true, {
             x1: 0,
             x2: 10,
             y1: 0,
-            y2: 3,
-            angle: 60
+            y2: 3
         }, options);
+
+        // ensure that x1 < x2, y1 < y2
+        if (congruency.x1 > congruency.x2) {
+            var hold = congruency.x1;
+            congruency.x1 = congruency.x2;
+            congruency.x2 = hold;
+        }
+        if (congruency.y1 > congruency.y2) {
+            var hold = congruency.y1;
+            congruency.y1 = congruency.y2;
+            congruency.y2 = hold;
+        }
+
         var graph = KhanUtil.currentGraph;
 
-        // Draw the parallel lines
-        graph.line([angles.x1, angles.y1], [angles.x2, angles.y1]);
-        graph.line([angles.x1, angles.y2], [angles.x2, angles.y2]);
+        congruency.addLine = function(options) {
+            var line = $.extend(true, {
+                start: [0, 0],
+                extend: true
+            }, options);
 
-        var slope = Math.tan(KhanUtil.toRadians(angles.angle));
-        var middle = [(angles.x2 - angles.x1) / 2, (angles.y1 + angles.y2) / 2];
-
-        // Takes a y position, and returns the x position of the point
-        var invfunc = function(y) {
-            return (y - middle[1]) / slope + middle[0];
-        };
-
-        var bottomIntersect = [invfunc(angles.y1), angles.y1];
-        var topIntersect = [invfunc(angles.y2), angles.y2];
-
-        // scale = -1: returns end
-        // scale =  0: returns (start + end) / 2
-        // scale =  1: returns start
-        var scaleLine = function(start, end, scale) {
-            return [(scale / 2 + 0.5) * start[0] + (0.5 - scale / 2) * end[0],
-                    (scale / 2 + 0.5) * start[1] + (0.5 - scale / 2) * end[1]];
-        };
-
-        // Draw the diagonal line
-        graph.line(scaleLine(bottomIntersect, topIntersect, 1.6),
-                   scaleLine(bottomIntersect, topIntersect, -1.6));
-
-        // Add an arc to the diagram
-        var addArc = function(position, radius, startAng, endAng) {
-            // Create the initial arc
-            var arc = {
-                arc: graph.arc(position, radius, startAng, endAng),
-                position: position,
-                radius: radius,
-                start: startAng,
-                end: endAng,
-                set: false
-            };
-
-            // Add a movable point for clicking
-            var aveAngle = KhanUtil.toRadians((endAng + startAng) / 2);
-
-            var pointPos = position.slice();
-            pointPos[0] += Math.cos(aveAngle) * 0.8;
-            pointPos[1] += Math.sin(aveAngle) * 0.8;
-
-            arc.point = KhanUtil.addMovablePoint({
-                            coord: pointPos
-                        });
-            // Increase the point's size
-            arc.point.mouseTarget.attr({ r: 30.0 });
-            // Make it not move
-            arc.point.onMove = function(x, y) {
-                return pointPos;
-            };
-
-            // Make a clicky pointer
-            $(arc.point.mouseTarget[0]).css("cursor", "pointer");
-
-            // Replace the shape with our arc
-            arc.point.visibleShape.remove();
-            arc.point.visibleShape = arc.arc;
-
-            // Colors for different states
-            arc.unsetNormal = {
-                stroke: KhanUtil.GRAY,
-                "stroke-width": 2,
-                opacity: 0.1
-            };
-            arc.unsetHighlight = {
-                stroke: KhanUtil.BLUE,
-                "stroke-width": 2,
-                opacity: 0.4
-            };
-            arc.setNormal = {
-                stroke: KhanUtil.BLUE,
-                "stroke-width": 3,
-                opacity: 0.9
-            };
-            arc.setHighlight = {
-                stroke: KhanUtil.BLUE,
-                "stroke-width": 3,
-                opacity: 1.0
-            };
-            arc.forceHighlight = {
-                stroke: KhanUtil.ORANGE,
-                "stroke-width": 3,
-                opacity: 1.0
-            };
-
-            // Set the default styles
-            arc.point.normalStyle = arc.unsetNormal;
-            arc.point.highlightStyle = arc.unsetHighlight;
-
-            // Remake the arc (meaning removing and re-creating it)
-            arc.remake = function(radius) {
-                this.radius = radius;
-                this.arc.remove();
-                this.arc = graph.arc(this.position, this.radius, this.start, this.end);
-                this.arc.attr(this.point.highlightStyle);
-                this.point.visibleShape = this.arc;
-            };
-
-            // Toggle between set and not-set
-            arc.toggle = function() {
-                if (this.set) {
-                    this.point.normalStyle = this.unsetNormal;
-                    this.point.highlightStyle = this.unsetHighlight;
-                    this.remake(0.6);
-                } else {
-                    this.point.normalStyle = this.setNormal;
-                    this.point.highlightStyle = this.setHighlight;
-                    this.remake(0.6);
-                }
-                this.set = !this.set;
-            };
-
-            // Add a click event
-            $(arc.point.mouseTarget[0]).bind("vmouseup", function(event) {
-                arc.toggle();
-            });
-
-            // Force the arc to be set, for a pre-given point
-            arc.force = function() {
-                this.point.highlightStyle = this.forceHighlight;
-                this.remake(0.6);
-                // Remove the mouse target entirely
-                $(this.point.mouseTarget[0]).unbind();
-                this.point.mouseTarget.remove();
-            };
-
-            // Hint an angle by changing its unset color
-            arc.hint = function(color) {
-                this.unsetNormal.stroke = color;
-                this.unsetNormal.opacity = 0.4;
-                this.unsetHighlight.opacity = 0.5;
-                this.arc.attr(this.unsetNormal);
+            if (line.end != null) {
+                line.radAngle = Math.atan2(line.end[1] - line.start[1],
+                                           line.end[0] - line.start[0]);
+                line.angle = KhanUtil.toDegrees(line.radAngle);
+            } else if (line.angle != null) {
+                line.radAngle = KhanUtil.toRadians(line.angle);
+                line.end = [Math.cos(line.radAngle) + line.start[0],
+                            Math.sin(line.radAngle) + line.start[1]];
             }
 
-            return arc;
+            line.slope = (line.end[1] - line.start[1]) /
+                         (line.end[0] - line.start[0]);
+
+            line.func = function(x) {
+                return line.start[1] + line.slope * (x - line.start[0]);
+            };
+
+            line.invfunc = function(y) {
+                return line.start[0] + (y - line.start[1]) / line.slope;
+            };
+
+            if (line.extend === true) {
+                var y1int = line.func(congruency.x1);
+
+                if (y1int >= congruency.y1 && y1int <= congruency.y2) {
+                    line.start = [congruency.x1, y1int];
+                } else if (y1int > congruency.y2) {
+                    line.start = [line.invfunc(congruency.y2), congruency.y2];
+                } else {
+                    line.start = [line.invfunc(congruency.y1), congruency.y1];
+                }
+
+                var y2int = line.func(congruency.x2);
+
+                if (y2int >= congruency.y1 && y2int <= congruency.y2) {
+                    line.end = [congruency.x2, y2int];
+                } else if (y2int > congruency.y2) {
+                    line.end = [line.invfunc(congruency.y2), congruency.y2];
+                } else {
+                    line.end = [line.invfunc(congruency.y1), congruency.y1];
+                }
+            }
+
+            graph.line(line.start, line.end);
+
+            return line;
         };
 
-        // The default style
-        graph.style({ stroke: KhanUtil.GRAY, opacity: 0.1 });
+        congruency.intersect = function(line1, line2, options) {
+            var ang = $.extend(true, {
+                line1: line1,
+                line2: line2,
+                radius: 0.6,
+                show: [true, true, true, true]
+            }, options);
 
+            if (ang.line1.slope === ang.line2.slope) {
+                return false;
+            }
 
-        // Add all eight angles, and store in an array
-        angles.angles = [];
+            ang.coord = [0, 0];
 
-        angles.angles.push(addArc(bottomIntersect, 0.6, 0, angles.angle));
-        angles.angles.push(addArc(bottomIntersect, 0.6, angles.angle, 180));
-        angles.angles.push(addArc(bottomIntersect, 0.6, 180, 180 + angles.angle));
-        angles.angles.push(addArc(bottomIntersect, 0.6, 180 + angles.angle, 360));
+            ang.coord[0] = (ang.line1.slope * ang.line1.start[0]
+                            - ang.line2.slope * ang.line2.start[0]
+                            + ang.line2.start[1] - ang.line1.start[1]) /
+                           (ang.line1.slope - ang.line2.slope);
+            ang.coord[1] = ang.line1.func(ang.coord[0]);
 
-        angles.angles.push(addArc(topIntersect, 0.6, 0, angles.angle));
-        angles.angles.push(addArc(topIntersect, 0.6, angles.angle, 180));
-        angles.angles.push(addArc(topIntersect, 0.6, 180, 180 + angles.angle));
-        angles.angles.push(addArc(topIntersect, 0.6, 180 + angles.angle, 360));
+            ang.addArc = function(pos, radius, start, end) {
+                var arc = {
+                    pos: pos,
+                    radius: radius,
+                    start: start,
+                    end: end,
+                    state: 0,
+                    max: 1,
+                    shown: false,
+                    stuck: false
+                };
 
-        // Exposed function to force setting of arcs
-        angles.setAngle = function(index) {
-            this.angles[index].force();
+                if (arc.start > arc.end) {
+                    var hold = arc.start;
+                    arc.start = arc.end;
+                    arc.end = hold;
+                }
+
+                arc.angle = arc.end - arc.start;
+
+                // Add a movable point for clicking
+                var aveAngle = KhanUtil.toRadians((arc.start + arc.end) / 2);
+
+                var pointPos = arc.pos.slice();
+                pointPos[0] += Math.cos(aveAngle) * arc.radius;
+                pointPos[1] += Math.sin(aveAngle) * arc.radius;
+
+                arc.point = KhanUtil.addMovablePoint({
+                    coord: pointPos
+                });
+                // Make it not move
+                arc.point.onMove = function(x, y) {
+                    return pointPos;
+                };
+
+                // Make a clicky pointer
+                $(arc.point.mouseTarget[0]).css("cursor", "pointer");
+
+                // Increase the point's size
+                var pointRadius = Math.sin(KhanUtil.toRadians(arc.angle) / 2)
+                                  * arc.radius * graph.scale[0];
+                arc.point.mouseTarget.attr({ r: pointRadius });
+
+                // replace the shape with our arc
+                arc.point.visibleShape.remove();
+
+                arc.unsetNormal = {
+                    stroke: KhanUtil.GRAY,
+                    "stroke-width": 2,
+                    opacity: 0.1
+                };
+                arc.unsetHighlight = {
+                    stroke: KhanUtil.GRAY,
+                    "stroke-width": 2,
+                    opacity: 0.4
+                };
+                arc.setNormal = {
+                    stroke: KhanUtil.BLUE,
+                    "stroke-width": 3,
+                    opacity: 0.9
+                };
+                arc.setHighlight = {
+                    stroke: KhanUtil.BLUE,
+                    "stroke-width": 3,
+                    opacity: 1.0
+                };
+
+                arc.point.normalStyle = arc.unsetNormal;
+                arc.point.highlightStyle = arc.unsetHighlight;
+
+                arc.draw = function() {
+                    if (this.arc != null) {
+                        this.arc.remove();
+                    }
+                    this.arc = graph.arc(this.pos, this.radius,
+                                         this.start, this.end);
+                    this.point.visibleShape = this.arc;
+                    this.arc.attr(this.point.normalStyle);
+                };
+
+                arc.draw();
+
+                arc.set = function(state) {
+                    arc.state = state;
+
+                    if (arc.state === 0) {
+                        arc.point.normalStyle = arc.unsetNormal;
+                        arc.point.highlightStyle = arc.unsetHighlight;
+                    } else {
+                        arc.point.normalStyle = arc.setNormal;
+                        arc.point.highlightStyle = arc.setHighlight;
+                    }
+
+                    arc.draw();
+                }
+
+                arc.click = function(event) {
+                    arc.set((arc.state === arc.max) ? 0 : arc.state + 1);
+                };
+
+                $(arc.point.mouseTarget[0]).bind("vmouseup", arc.click);
+
+                arc.stick = function() {
+                    $(arc.point.mouseTarget[0]).unbind();
+                    this.point.mouseTarget.remove();
+                };
+
+                arc.styleUnset = function(options) {
+                    $.extend(true, this.unsetNormal, options);
+                    $.extend(true, this.unsetHighlight, options);
+                    this.draw();
+                };
+
+                arc.styleSet = function(options) {
+                    $.extend(true, this.setNormal, options);
+                    $.extend(true, this.setHighlight, options);
+                    this.draw();
+                };
+
+                return arc;
+            };
+
+            var startAngle = ang.line1.angle;
+            var diffAngle = ang.line2.angle - ang.line1.angle;
+
+            ang.ang = [];
+
+            if (ang.show[0]) {
+                ang.ang[0] = ang.addArc(ang.coord, ang.radius,
+                                        startAngle,
+                                        startAngle + diffAngle);
+            }
+            if (ang.show[1]) {
+                ang.ang[1] = ang.addArc(ang.coord, ang.radius,
+                                        startAngle + diffAngle,
+                                        startAngle + 180);
+            }
+            if (ang.show[2]) {
+                ang.ang[2] = ang.addArc(ang.coord, ang.radius,
+                                        startAngle + 180,
+                                        startAngle + 180 + diffAngle);
+            }
+            if (ang.show[3]) {
+                ang.ang[3] = ang.addArc(ang.coord, ang.radius,
+                                        startAngle + 180 + diffAngle,
+                                        startAngle + 360);
+            }
+
+            return ang;
         };
 
-        // Exposed function for checking if angles are set
-        angles.isSet = function(index) {
-            return this.angles[index].set;
-        };
-
-        // Exposed function for hinting an angle
-        angles.hint = function(index, color) {
-            this.angles[index].hint(color);
-        };
-
-        return angles;
+        return congruency;
     }
 });
