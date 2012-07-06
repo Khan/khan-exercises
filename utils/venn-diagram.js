2454f889c21a3d611b3f208475210379c7860a9c
Vit Stanislav <slaweet@seznam.cz>
2012-07-06 20:49:29 +0200

    exercise 0.5 added

--- utils/venn-diagram.js
+++ utils/venn-diagram.js
@@ -1,4 +1,3 @@
-    var SETS = ['A','B','C'];
     var OPPS = ['\\cup', '\\cap', '-'];
     var COLORS = [KhanUtil.BLUE, KhanUtil.GREEN, KhanUtil.ORANGE];
 
@@ -14,123 +13,137 @@
         };
 
 $.extend(KhanUtil, {
-    vennDiagram: function(labels, data) {
-    var SELECTED_OPACITY = 0.5;
+    SETS: ['A','B','C'],
+
+    vennDiagram: function(data, clickable) {
+        // @param data is array of amounts in regions in format: [A,B,C, AB,BC,AC, ABC]
+
+        var SELECTED_OPACITY = 0.5;
 
 
-    /*
-    * source: http://code.google.com/p/js-venn/
-    * gets the intersecting points between two circles
-    */
-    var intersectingPoints = function(c1,c2) {
-        // serious maths follow... Beware
-        // Formulas nabbed from http://2000clicks.com/mathhelp/GeometryConicSectionCircleIntersection.aspx
-        var d = Math.sqrt(Math.pow((c2.x - c1.x),2) + Math.pow(c2.y - c1.y,2));
-        var d_sq = Math.pow(d,2); // d squared
-        var K = (.25)*Math.sqrt((Math.pow(c1.r+c2.r,2)-d_sq)*(d_sq-Math.pow(c1.r-c2.r,2)));
-        // split up the equations for readability
-        var xpart1 = (.5)*(c2.x+c1.x) + ((.5)*(c2.x-c1.x)*(Math.pow(c1.r,2) - Math.pow(c2.r,2))/d_sq);
-        var xpart2 = 2*(c2.y-c1.y)*K/d_sq;
+        /*
+        * gets the intersecting points between two circles
+        * source: http://code.google.com/p/js-venn/
+        */
+        var intersectingPoints = function(c1,c2) {
+            // serious maths follow... Beware
+            // Formulas nabbed from http://2000clicks.com/mathhelp/GeometryConicSectionCircleIntersection.aspx
+            var d = Math.sqrt(Math.pow((c2.x - c1.x),2) + Math.pow(c2.y - c1.y,2));
+            var d_sq = Math.pow(d,2); // d squared
+            var K = (.25)*Math.sqrt((Math.pow(c1.r+c2.r,2)-d_sq)*(d_sq-Math.pow(c1.r-c2.r,2)));
+            // split up the equations for readability
+            var xpart1 = (.5)*(c2.x+c1.x) + ((.5)*(c2.x-c1.x)*(Math.pow(c1.r,2) - Math.pow(c2.r,2))/d_sq);
+            var xpart2 = 2*(c2.y-c1.y)*K/d_sq;
 
-        var ypart1 = (.5)*(c2.y+c1.y) + ((.5)*(c2.y-c1.y)*(Math.pow(c1.r,2) - Math.pow(c2.r,2))/d_sq);
-        var ypart2 = 2*(c2.x-c1.x)*K/d_sq;
+            var ypart1 = (.5)*(c2.y+c1.y) + ((.5)*(c2.y-c1.y)*(Math.pow(c1.r,2) - Math.pow(c2.r,2))/d_sq);
+            var ypart2 = 2*(c2.x-c1.x)*K/d_sq;
+            
+            var points = [];
+            points.push([xpart1 + xpart2, 
+                         ypart1 - ypart2]);
+            points.push([xpart1 - xpart2,
+                         ypart1 + ypart2]);
+            return points;
+        }
+
+        var scaleAndJoin = function(x) {
+            var temp = KhanUtil.currentGraph.scalePoint(x);
+            return temp.join(" ");
+        }
+
+        var arcString = function(end, radius, largeAngle, sweep) {
+            var radii = KhanUtil.currentGraph.scaleVector(radius);
+            var retstring =  "A" + radii.join(" ") + " 0 " + ( largeAngle ? 1 : 0 ) + " " + sweep + " " + scaleAndJoin(end)  ;
+            return retstring;
+        }
+
+        var pointIsOnSideOfLineWithCenter = function(line, point) {
+            var a = line[0][1] - line[1][1];
+            var b = line[1][0] - line[0][0];
+            var c = -a*line[0][0] - b*line[0][1];
+            var retval = ((a*point[0] + b*point[1] + c)  > 0)
+            return retval;
+        }
+
+        var vennIntersectionString = function(points, circles, sweeps) {
+            var pathString = "M" + scaleAndJoin(points[0]);
+            $.each(points, function(i, point) {
+                var c = circles[(i+0) % circles.length];
+                var largeAngle = !pointIsOnSideOfLineWithCenter([points[i], points[(i+1) % points.length]],[c.x, c.y]) && !sweeps[i];
+                pathString += arcString (points[(i+1) % points.length], circles[i].r, largeAngle, sweeps[i]);
+            });
+            return pathString;
+        }
+
+        var vennIntersection = function(points, circles, sweeps, label, clickable) {
+            var graph = KhanUtil.currentGraph;
+            var set = graph.raphael.set();
+            var pathString = vennIntersectionString (points, circles, sweeps);
+            if (sweeps[0] == 0 && !clickable) {
+                set.push(graph.label(getIntersectionCenter(points, circles, sweeps), '\\color{#555}{' +label+ '}'));
+            }
+            var pathObject = graph.raphael.path(pathString);
+            set.push(pathObject);
+            var bbox = pathObject.getBBox();
+            pathObject.attr({fill: '#aaa', 'fill-opacity': 0.0, stroke: 0, 'stroke-fill': "transparent"});
+            pathObject.hover(function(){
+                this.highlight((this.selected ? SELECTED_OPACITY : 0) + 0.3)
+            },
+            function(){
+                this.highlight(this.selected ? SELECTED_OPACITY : 0);
+            });
+            pathObject.highlight = function(opacity) {
+                this.animate({'fill-opacity': opacity}, 200);
+            }
+            pathObject.select = function(bool) {
+                this.selected = bool;
+                this.highlight(this.selected ? SELECTED_OPACITY : 0);
+            }
+            if (clickable) {
+                pathObject.attr({cursor: 'pointer'});
+                pathObject.click(function(){
+                    this.select(!this.selected);
+                });
+            }
+            return pathObject;
+        }
         
-        var points = [];
-        points.push([xpart1 + xpart2, 
-                     ypart1 - ypart2]);
-        points.push([xpart1 - xpart2,
-                     ypart1 + ypart2]);
-        return points;
-    }
-
-    var scaleAndJoin = function(x) {
-        var temp = KhanUtil.currentGraph.scalePoint(x);
-        return temp.join(" ");
-    }
-
-    var arcString = function(end, radius, largeAngle, sweep) {
-        var radii = KhanUtil.currentGraph.scaleVector(radius);
-        var retstring =  "A" + radii.join(" ") + " 0 " + ( largeAngle ? 1 : 0 ) + " " + sweep + " " + scaleAndJoin(end)  ;
-        return retstring;
-    }
-
-    var pointIsOnSideOfLineWithCenter = function(line, point) {
-        var a = line[0][1] - line[1][1];
-        var b = line[1][0] - line[0][0];
-        var c = -a*line[0][0] - b*line[0][1];
-        var retval = ((a*point[0] + b*point[1] + c)  > 0)
-        return retval;
-    }
-
-    var vennIntersectionString = function(points, circles, sweeps) {
-        var pathString = "M" + scaleAndJoin(points[0]);
-        $.each(points, function(i, point) {
-            var c = circles[(i+0) % circles.length];
-            var largeAngle = !pointIsOnSideOfLineWithCenter([points[i], points[(i+1) % points.length]],[c.x, c.y]) && !sweeps[i];
-            pathString += arcString (points[(i+1) % points.length], circles[i].r, largeAngle, sweeps[i]);
-        });
-        return pathString;
-    }
-
-    var vennIntersection = function(points, circles, sweeps, label) {
-        var graph = KhanUtil.currentGraph;
-        var set = graph.raphael.set();
-        var pathString = vennIntersectionString (points, circles, sweeps);
-        if (sweeps[0] == 0) {
-            set.push(graph.label(getIntersectionCenter(points, circles, sweeps), '\\color{#555}{' +label+ '}'));
+        var getIntersectionCenter = function(points) {
+            return [(points[0][0] + points[1][0] + points[2][0])/3, (points[0][1] + points[1][1] + points[2][1])/3];
         }
-        var pathObject = graph.raphael.path(pathString);
-        set.push(pathObject);
-        var bbox = pathObject.getBBox();
-        pathObject.attr({fill: '#aaa', 'fill-opacity': 0.0, stroke: 0, 'stroke-fill': "transparent"});
-        pathObject.hover(function(){
-            this.opacity = this.attr('fill-opacity');
-            this.highlight(this.opacity + 0.3)
-        },
-        function(){
-            this.highlight(this.opacity);
-        });
-        pathObject.highlight = function(opacity) {
-            this.animate({'fill-opacity': opacity}, 200);
+
+        var drawIntersection = function(points, circles, label, i,inOrOut, sweeps, clickable) {
+            var ps = [];
+            var cs = [];
+            $.each(points, function(j, p2) {
+                ps.push(points[(i+j) % points.length][inOrOut[(j+1) % inOrOut.length]]);
+                cs.push(circles[(i+j+1) % circles.length]);
+            });
+            var intersection = vennIntersection(ps,cs, sweeps, label, clickable)
+            return intersection;
         }
-        return pathObject;
-    }
-    
-    var getIntersectionCenter = function(points) {
-        return [(points[0][0] + points[1][0] + points[2][0])/3, (points[0][1] + points[1][1] + points[2][1])/3];
-    }
 
-    var drawIntersection = function(points, circles, label, i,inOrOut, sweeps) {
-        var ps = [];
-        var cs = [];
-        $.each(points, function(j, p2) {
-            ps.push(points[(i+j) % points.length][inOrOut[(j+1) % inOrOut.length]]);
-            cs.push(circles[(i+j+1) % circles.length]);
-        });
-        var intersection = vennIntersection(ps,cs, sweeps, label)
-        return intersection;
-    }
+        var drawIntersections = function(circles, labels, clickable) {
+            var points = [];
+            $.each(circles, function(i, c) {
+                points.push(intersectingPoints(c, circles[(i+1)%circles.length]));
+            });
+            var intersections = [];
+            $.each(points, function(i, p) {
+                intersections[i] = (drawIntersection(points, circles, labels[i],    i,[0,0,1], [1,1,0], clickable));
+                intersections[(i+2) % 3 + 3] = (drawIntersection(points, circles, labels[(i+2) % 3 + 3],  i,[0,1,1], [0,1,1], clickable));
+            });
+            intersections.push(drawIntersection(points, circles, labels[6],  0,[1,1,1], [0,0,0], clickable));
+            return intersections;
+        }
 
-    var drawIntersections = function(circles, labels) {
-        var points = [];
-        $.each(circles, function(i, c) {
-            points.push(intersectingPoints(c, circles[(i+1)%circles.length]));
-        });
-        var intersections = [];
-        $.each(points, function(i, p) {
-            intersections[i] = (drawIntersection(points, circles, labels[i],    i,[0,0,1], [1,1,0]));
-            intersections[(i+2) % 3 + 3] = (drawIntersection(points, circles, labels[(i+2) % 3 + 3],  i,[0,1,1], [0,1,1]));
-        });
-        intersections.push(drawIntersection(points, circles, labels[6],  0,[1,1,1], [0,0,0]));
-        return intersections;
-
-    }
+        var labels = this.SETS;
         var graph = this.currentGraph;
         var set = graph.raphael.set();
 
         var c =[];
         $.each(labels, function(i, l) {
-            c[i] = {r: Math.sqrt(arraySum(arraySelect(data,SUBSETS[l])))/2};
+            c[i] = {r: Math.sqrt(KhanUtil.arraySum(KhanUtil.arraySelect(data,SUBSETS[l])))/2};
         });
 
         c[0].x = c[0].r + 1;
@@ -140,7 +153,7 @@ $.extend(KhanUtil, {
         c[2].x = c[0].r + c[2].r/2 + 1;
         c[2].y = c[0].r + c[2].r + 1;
 
-        var regions = drawIntersections(c, data);
+        var regions = drawIntersections(c, data, clickable);
 
         $.each(c, function(i, circle) {
             set.push(graph.circle([circle.x, circle.y],circle.r, {
@@ -166,30 +179,42 @@ $.extend(KhanUtil, {
         labelPos.push([c[2].x, c[2].y + c[2].r/2]);
 
         $.each(labelPos, function(i, pos) {
-            set.push(graph.label(pos,"\\color{#333}{" + data[i] +"}"));
+            if (!clickable) {
+                set.push(graph.label(pos,"\\color{#333}{" + data[i] +"}"));
+            }
         });
+
         var venn = {
             regions: regions,
 
             selectRegions: function(indexes, reset) {
                if (reset) {
                    $.each(this.regions, function(i, region) {
-                       region.highlight(0);
+                       region.select(false);
                    });
                }
                $.each(this.getRegions(indexes), function(i, region) {
-                   region.highlight(SELECTED_OPACITY);
+                   region.select(true);
                });
             },
 
             getRegions: function(indexes) {
-                return arraySelect(this.regions, indexes);
-            }        }
+                return KhanUtil.arraySelect(this.regions, indexes);
+            },
+
+            getSelectedRegionIndexes: function() {
+                var ret = [];
+                $.each(this.regions, function(i, region) {
+                    region.selected && ret.push(i);
+                });
+                return ret;
+            }
+        }
         return venn;
     },
 
     randSubsetExpression: function(count) {
-        var sets = this.shuffle(SETS);
+        var sets = this.shuffle(this.SETS);
         var ret = [sets[0]];
         for (var i = 1; i < count; i++) {
             ret.push(this.randFromArray(OPPS));
@@ -202,7 +227,7 @@ $.extend(KhanUtil, {
     },
 
     colorSets: function(str) {
-        $.each (SETS, function(i, set) { 
+        $.each (this.SETS, function(i, set) { 
             str = str.replace(new RegExp(set, 'g'),"\\color{"+COLORS[i] +"}{"+ set +"}");
         });
         return str;
@@ -273,25 +298,24 @@ $.extend(KhanUtil, {
                 }
                 return [];
         }
+    },
+
+    arraySum: function(a) {
+        var sum = 0;
+        $.each(a, function(i, item) {
+            sum += item;
+        });
+        return sum;
+    },
+
+    arraySelect: function(array, indexes) {
+        var ret = [];
+        $.each(indexes, function(i, index) {
+            if (array[index] !== undefined) {
+                ret.push(array[index]);
+            }
+        });
+        return ret;
     }
-
 });
-
-var arraySum = function(a) {
-    var sum = 0;
-    $.each(a, function(i, item) {
-        sum += item;
-    });
-    return sum;
-}
-
-var arraySelect = function(array, indexes) {
-    var ret = [];
-    $.each(indexes, function(i, index) {
-        if (array[index] !== undefined) {
-            ret.push(array[index]);
-        }
-    });
-    return ret;
-}
 
