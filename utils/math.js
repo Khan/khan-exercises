8a699371ba9af28a499d848fd4cab787870558e3
Justin Helps <justin.r.helps@gmail.com>
2013-03-22 14:24:15 -0500

    Added naming_polynomials to address SB tags HS.1.D.1.SR.2-3. Also changed randRangeUnique to include the entire range.

--- utils/math.js
+++ utils/math.js
@@ -290,7 +290,7 @@ $.extend(KhanUtil, {
             return KhanUtil.randRange(min, max);
         } else {
             var toReturn = [];
-            for (var i = min; i < max; i++) {
+            for (var i = min; i <= max; i++) {
                 toReturn.push(i);
             }
 
@@ -305,7 +305,7 @@ $.extend(KhanUtil, {
             return KhanUtil.randRangeNonZero(min, max);
         } else {
             var toReturn = [];
-            for (var i = min; i < max; i++) {
+            for (var i = min; i <= max; i++) {
                 if (i === 0) {
                     continue;
                 }
