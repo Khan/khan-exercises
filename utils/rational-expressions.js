17ccf945067bbc4c4b8062dc42130e47016bc893
petercollingridge <peter.collingridge@gmail.com>
2013-03-25 19:10:44 +0000

    Start a rational expression object

--- utils/rational-expressions.js
+++ utils/rational-expressions.js
@@ -1,5 +1,4 @@
 $.extend(KhanUtil, {
-
 	getExpressionRegex: function(coefficient, vari, constant) {
         // Capture Ax + B or B + Ax, either A or B can be 0
         
@@ -36,6 +35,74 @@ $.extend(KhanUtil, {
         }
 
         return regex;
-	}
+	},
 
+    // Term in an expression
+    // e.g. [5, x, 2] = 5x^2
+    Term: function(value) {
+        this.variables = [];
+
+        if (typeof value === "number") {
+            this.coefficient = value;
+        } else {
+            this.coefficient = value[0];
+            this.variables = value.slice(1);
+        }
+
+        // includeSign if term is not the first in an expression
+        this.toString = function(includeSign) {
+            if (this.coefficient === 0) {
+                return "";
+            }
+
+            if (includeSign) {
+                var s = this.coefficient > 1 ? " + " + this.coefficient : " - " + (-this.coefficient);
+            } else {
+                var s = this.coefficient;
+            }
+
+            for (var i = 0; i < this.variables.length; i+=2) {
+                var degree = this.variables[i + 1];
+                
+                if (degree === 0) {
+                    continue;
+                }
+                s += this.variables[i];
+                if (degree > 1) {
+                    s += "^" + degree;
+                }
+            }
+            return this.coefficient;
+        }
+    },
+
+    // Array of terms which are added together
+    // Terms can be numbers, of [variable, degree]
+    // e.g. [5, [1, x]] = 5 + x
+    // e.g. [5, [2, x, 2]] = 5 + 2x^2
+    // e.g. [5, [2, x, 2, y]] = 5 + 2x^2y
+    RationalExpression: function(terms) {
+        this.terms = terms;
+
+        this.multiply = function(value) {
+            var terms = [];
+
+            for (var i = 0; i < this.terms.length; i++) {
+                terms[i] = this.terms[i].coefficient * value;
+            }
+
+            return new KhanUtil.RationalExpression(terms);
+        };
+
+        this.toString = function() {
+            var s = this.terms[0].toString();
+
+            for (var i = 1; i < this.terms.length; i++) {
+                s += this.terms[i].toString(true)
+            }
+            return s;
+        };
+
+        return this;
+    }
 });
