9e78faefc1c669b7bf67c7fd230c577a7d12b66c
petercollingridge <peter.collingridge@gmail.com>
2013-03-25 21:17:30 +0000

    Remove unnecessary rational expression term object

--- utils/rational-expressions.js
+++ utils/rational-expressions.js
@@ -35,44 +35,5 @@ $.extend(KhanUtil, {
         }
 
         return regex;
-	},
-
-    // Term in an expression
-    // e.g. [5, x, 2] = 5x^2
-    Term: function(value) {
-        this.variables = [];
-
-        if (typeof value === "number") {
-            this.coefficient = value;
-        } else {
-            this.coefficient = value[0];
-            this.variables = value.slice(1);
-        }
-
-        // includeSign if term is not the first in an expression
-        this.toString = function(includeSign) {
-            if (this.coefficient === 0) {
-                return "";
-            }
-
-            if (includeSign) {
-                var s = this.coefficient > 1 ? " + " + this.coefficient : " - " + (-this.coefficient);
-            } else {
-                var s = this.coefficient;
-            }
-
-            for (var i = 0; i < this.variables.length; i+=2) {
-                var degree = this.variables[i + 1];
-                
-                if (degree === 0) {
-                    continue;
-                }
-                s += this.variables[i];
-                if (degree > 1) {
-                    s += "^" + degree;
-                }
-            }
-            return this.coefficient;
-        }
-    }
+	}
 });
