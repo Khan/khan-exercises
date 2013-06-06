87d65907a83862d7ed24e376c60db9b8b61259bf
Ben Alpert <alpert@khanacademy.org>
2013-06-06 01:35:09 -0700

    Don't add classes to #solutionarea
    They don't get removed! This causes problems in topic mode. (Also make
    the inputs a bit wider since they don't need to be tiny.)
    
    Test Plan:
    Did a problem in pythagorean_theorem_1 and it accepted my answer.
    
    Auditors: eater

--- utils/answer-types.js
+++ utils/answer-types.js
@@ -708,10 +708,11 @@ Khan.answerTypes = $.extend(Khan.answerTypes, {
                 rad = $('<input type="text">');
             }
             // Make them look pretty
-            solutionarea.addClass("radical")
+            $("<div class='radical'>")
                 .append($("<span>").append(inte))
                 .append('<span class="surd">&radic;</span>')
-                .append($("<span>").append(rad).addClass("overline"));
+                .append($("<span>").append(rad).addClass("overline"))
+                .appendTo(solutionarea);
 
             var ansSquared = parseFloat(solutionText);
             var ans = KhanUtil.splitRadical(ansSquared);
