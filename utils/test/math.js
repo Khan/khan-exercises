96d5005c172235dc0d65cd0d5f9241fb76a3e0ba
mauk81 <mauk@gmx.net>
2013-11-03 00:54:09 +0100

    add some test for math util
    I did a bunch of refactoring before merging this leading to lots of conflicts, but I think I got everything in.
    
    Closes #104802

--- utils/test/math.js
+++ utils/test/math.js
@@ -1,35 +1,35 @@
 (function(){
 module("math" );
 
-asyncTest( "math miscellanea", 44, function() {
+asyncTest( "math miscellanea", 60, function() {
     deepEqual( KhanUtil.digits(376), [ 6, 7, 3 ], "digits(376)" );
     deepEqual( KhanUtil.integerToDigits(376), [ 3, 7, 6 ], "integerToDigits(376)" );
 
-    equal( KhanUtil.getGCD(216, 1024), 8, "gcd(216, 1024)" );
-    equal( KhanUtil.getGCD(512341, 2325183), 1, "gcd(512341, 2325183)" );
-    equal( KhanUtil.getGCD(53110108, 109775188), 68, "gcd(53110108, 109775188)" );
-    equal( KhanUtil.getGCD(-21, 14), 7, "gcd(-21, 14)" );
-    equal( KhanUtil.getGCD(-21, -14), 7, "gcd(-21, -14)" );
-    equal( KhanUtil.getGCD(123, 1), 1, "gcd(123, 1)" );
-    equal( KhanUtil.getGCD(123, 1), 1, "gcd(123, 1)" );
-    equal( KhanUtil.getGCD(123, 123), 123, "gcd(123, 123)" );
-    equal( KhanUtil.getGCD(169, 26, -52), 13, "gcd(169, 26, -52)" );
+    strictEqual( KhanUtil.getGCD(216, 1024), 8, "gcd(216, 1024)" );
+    strictEqual( KhanUtil.getGCD(512341, 2325183), 1, "gcd(512341, 2325183)" );
+    strictEqual( KhanUtil.getGCD(53110108, 109775188), 68, "gcd(53110108, 109775188)" );
+    strictEqual( KhanUtil.getGCD(-21, 14), 7, "gcd(-21, 14)" );
+    strictEqual( KhanUtil.getGCD(-21, -14), 7, "gcd(-21, -14)" );
+    strictEqual( KhanUtil.getGCD(123, 1), 1, "gcd(123, 1)" );
+    strictEqual( KhanUtil.getGCD(123, 1), 1, "gcd(123, 1)" );
+    strictEqual( KhanUtil.getGCD(123, 123), 123, "gcd(123, 123)" );
+    strictEqual( KhanUtil.getGCD(169, 26, -52), 13, "gcd(169, 26, -52)" );
 
-    equal( KhanUtil.getLCM(216, 1024), 27648, "lcm(216, 1024)" );
-    equal( KhanUtil.getLCM(216, -1024), 27648, "lcm(216, -1024)" );
-    equal( KhanUtil.getLCM(1, 2, 3, 4, 5, 6, 7, 8, 9, 10), 2520, "lcm(1..10)" );
+    strictEqual( KhanUtil.getLCM(216, 1024), 27648, "lcm(216, 1024)" );
+    strictEqual( KhanUtil.getLCM(216, -1024), 27648, "lcm(216, -1024)" );
+    strictEqual( KhanUtil.getLCM(1, 2, 3, 4, 5, 6, 7, 8, 9, 10), 2520, "lcm(1..10)" );
 
-    equal( KhanUtil.isPrime(1), false, "primeq 1" );
-    equal( KhanUtil.isPrime(2), true, "primeq 2" );
-    equal( KhanUtil.isPrime(216), false, "primeq 216" );
-    equal( KhanUtil.isPrime(127), true, "primeq 127" );
-    equal( KhanUtil.isPrime(129), false, "primeq 129" );
+    strictEqual( KhanUtil.isPrime(1), false, "primeq 1" );
+    strictEqual( KhanUtil.isPrime(2), true, "primeq 2" );
+    strictEqual( KhanUtil.isPrime(216), false, "primeq 216" );
+    strictEqual( KhanUtil.isPrime(127), true, "primeq 127" );
+    strictEqual( KhanUtil.isPrime(129), false, "primeq 129" );
 
-    equal( KhanUtil.isOdd(0), false, "oddq 0" );
-    equal( KhanUtil.isOdd(1), true, "oddq 1" );
+    strictEqual( KhanUtil.isOdd(0), false, "oddq 0" );
+    strictEqual( KhanUtil.isOdd(1), true, "oddq 1" );
 
-    equal( KhanUtil.isEven(0), true, "evenq 0" );
-    equal( KhanUtil.isEven(1), false, "evenq 1" );
+    strictEqual( KhanUtil.isEven(0), true, "evenq 0" );
+    strictEqual( KhanUtil.isEven(1), false, "evenq 1" );
 
     deepEqual( KhanUtil.getPrimeFactorization( 6 ), [ 2, 3 ], "factor 6" );
     deepEqual( KhanUtil.getPrimeFactorization( 23 ), [ 23 ], "factor 23" );
@@ -45,8 +45,25 @@ asyncTest( "math miscellanea", 44, function() {
     deepEqual( KhanUtil.getMultiples( 7, 80 ), [ 7, 14, 21, 28, 35, 42, 49, 56, 63, 70, 77 ], "multiples 7, 80" );
     deepEqual( KhanUtil.getMultiples( 7, 83 ), [ 7, 14, 21, 28, 35, 42, 49, 56, 63, 70, 77 ], "multiples 7, 83" );
 
-    equal( KhanUtil.roundTo( 2, Math.PI ), 3.14, "roundTo 2, pi" );
-    equal( KhanUtil.roundTo( 0, Math.PI ), 3, "roundTo 0, pi" );
+    strictEqual( KhanUtil.roundTo( 2, Math.PI ), 3.14, "roundTo 2, pi" );
+    strictEqual( KhanUtil.roundTo( 0, Math.PI ), 3, "roundTo 0, pi" );
+    strictEqual( KhanUtil.roundTo( 1, 0.15 ), 0.2, "roundTo half up" );
+    strictEqual( KhanUtil.roundTo( 1, 0.14 ), 0.1, "roundTo down" );
+
+    strictEqual( KhanUtil.floorTo( 1, 0.19 ), 0.1, "floorTo" );
+    strictEqual( KhanUtil.ceilTo( 1, 0.11 ), 0.2, "ceilTo" );
+
+    strictEqual( KhanUtil.truncate_to_max( 0.16, 1 ), 0.2, "truncate_to_max round up" );
+    strictEqual( KhanUtil.truncate_to_max( 0.15, 1 ), 0.1, "truncate_to_max round precision problem" );
+    strictEqual( KhanUtil.truncate_to_max( 0.14, 1 ), 0.1, "truncate_to_max round down" );
+    strictEqual( KhanUtil.truncate_to_max( 0.15, 3 ), 0.15, "truncate_to_max ignore" );
+
+    strictEqual( KhanUtil.isInt( 0 ), true, "0 is int" );
+    strictEqual( KhanUtil.isInt( "99" ), true, "'99' is int" );
+    strictEqual( KhanUtil.isInt( 0.5 ), false, "0.5 isn't int" );
+    strictEqual( KhanUtil.isInt( "9.9" ), false, "'9.9' isn't int" );
+    strictEqual( KhanUtil.isInt( "9,9" ), false, "'9,9' isn't int" );
+    strictEqual( KhanUtil.isInt( "a" ), false, "'a' isn't int" );
 
     deepEqual( KhanUtil.toFraction( 4/8 ), [ 1, 2 ], "4/8" );
     deepEqual( KhanUtil.toFraction( 0.666 ), [ 333, 500 ], "0.666" );
@@ -59,6 +76,9 @@ asyncTest( "math miscellanea", 44, function() {
     deepEqual( KhanUtil.sortNumbers([ 134, 17, 2, 46 ]), [ 2, 17, 46, 134 ], "sort some stuff" );
     deepEqual( KhanUtil.sortNumbers( KhanUtil.shuffle( KhanUtil.primes ) ),
         KhanUtil.primes, "shuffle then sort the primes" );
+
+    deepEqual( KhanUtil.splitRadical( 24 ), [ 2, 6 ], "sqrt(24) = 2*sqrt(6)" );
+    deepEqual( KhanUtil.splitCube( 24 ), [ 2, 3 ], "sqrt[3](24) = 2*sqrt[3](3)" );
 
     start();
 });
