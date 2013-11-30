d38eb615b0ea967f884806cc39bd325912ddf6fd
mauk81 <mauk@gmx.net>
2013-11-30 04:52:56 +0100

    revert (commit of wrong file/removed used code)

--- utils/proofs.js
+++ utils/proofs.js
@@ -464,6 +464,100 @@ window.outputKnownProof = function() {
     return proofText;
 };
 
+// returns a proof with a few blanks, blank statement fields will be wrapped by a div with id formatted according to divName
+window.outputFillBlanksProof = function() {
+    var reasonCodes = {"SSS" : 0, "ASA" : 1, "SAS" : 2, "AAS" : 3, "corresponding parts of congruent triangles are congruent" : 4,
+     "vertical angles are congruent" : 5, "alternate interior angles are congruent" : 6};
+    var proofText = "<h3>Givens</h3>";
+    var blanks = 0;
+    var blankStatements = 0;
+
+    var unsortedKeyList = _.clone(finishedEqualitiesList);
+    var finishedKeysList = sortEqualityList(unsortedKeyList.reverse(), finishedEqualities);
+    var finishedKeys = _.map(finishedKeysList, function(key) { return key.toString(); });
+
+    var numberGivens = 0;
+    _.each(finishedKeys, function(key) {
+        if (finishedEqualities[key] === "given") {
+            numberGivens++;
+        }
+    });
+
+    var newEqualities = {};
+
+    for (var i = 0; i < finishedKeys.length; i++) {
+        if (finishedEqualities[finishedKeys[i]].substring(0, 4) !== "Same") {
+            if (finishedEqualities[finishedKeys[i]] === "given") {
+                numberGivens--;
+                if (i % 3 !== 2) {
+                    proofText += "<div style=\"float:left\" class=\"" + divName(finishedKeys[i]) + "\">";
+                } else {
+                    proofText += "<div style=\"float:left\" class=\"" + divName(finishedKeys[i]) + "\">";
+                }
+                proofText += prettifyEquality(finishedKeys[i]);
+                if (numberGivens > 1) {
+                    proofText += "<code>, \\ </code> </div>";
+                } else if (numberGivens > 0) {
+                    proofText += "<code>, \\  </code>and<code>\\  </code></div>";
+                } else {
+                    proofText += "</div><br><br><h3 style=\"clear:both\">Proof</h3>";
+                }
+                newEqualities[finishedKeys[i]] = "given";
+                //knownEqualities[finishedKeys[i].reverse()] = "given";
+            } else {
+                if (KhanUtil.random() < 0.2) {
+                    proofText += "<div class=\"" + divName(finishedKeys[i]) + "\">";
+                    proofText += prettifyEquality(finishedKeys[i]);
+                    proofText += " because <select class=\"missing missingReason\" id=\"" + finishedKeys[i] + "\">" +
+                        "<option></option>" +
+                        "<option value=\"SSS\">side-side-side congruence</option>" +
+                        "<option value=\"ASA\">angle-side-angle congruence</option>" +
+                        "<option value=\"SAS\">side-angle-side congruence</option>" +
+                        "<option value=\"AAS\">angle-angle-side congruence</option>" +
+                        "<option>corresponding parts of congruent triangles are congruent</option>" +
+                        "<option>vertical angles are congruent</option>" +
+                        "<option>alternate interior angles are congruent</option>" +
+                        "</select> </div>" + "<br>";
+                    blanks++;
+                } else if (KhanUtil.random() < 0.2) {
+                    if (finishedKeys[i][0] === "t") {
+                        proofText += "<div class=\"missing\" id=\"" + "t-" + i + "\">" +
+                            "<code> \\bigtriangleup </code> <input class=\"missingStatement\"></input>" +
+                            "<code> = \\bigtriangleup </code> <input class=\"missingStatement\"></input>";
+                    } else if (finishedKeys[i][0] === "s") {
+                        proofText += "<div class=\"missing\" id=\"" + "s-" + i + "\">" +
+                            "<input class=\"missingStatement\"></input>" +
+                            "<code> = </code><input class=\"missingStatement\"></input>";
+                    } else {
+                        proofText += "<div class=\"missing\" id=\"" + "a-" + i + "\">" +
+                            "<code> \\angle </code> <input class=\"missingStatement\"></input>" +
+                            "<code> = \\angle </code> <input class=\"missingStatement\"></input>";
+                    }
+                    proofText += " because " + finishedEqualities[finishedKeys[i]] + "</div><br>";
+                    blanks++;
+                    blankStatements++;
+                } else {
+                    proofText += "<div class=\"" + divName(finishedKeys[i]) + "\">";
+                    proofText += prettifyEquality(finishedKeys[i]);
+                    proofText += " because " + finishedEqualities[finishedKeys[i]] + "</div>" + "<br>";
+                    newEqualities[finishedKeys[i]] = finishedEqualities[finishedKeys[i]];
+                    // knownEqualities[finishedKeys[i].reverse()] = finishedEqualities[finishedKeys[i]];
+                }
+            }
+        }
+
+    }
+
+    if (blanks < 2 || blankStatements < 1) {
+        return outputFillBlanksProof();
+    }
+
+    knownEqualities = _.extend(knownEqualities, newEqualities);
+
+    return [proofText, blanks];
+
+};
+
 // given a div id (formatted according to divName), checks to make sure that the inputs in that
 // div correspond to that div name, for use in "fill-in-the-blank" proofs
 // returns true if the statements were filled in correctly, false otherwise
