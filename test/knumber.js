fc32705b56b07a4e86d1d0ec994724b6efceb234
Jack Toole <jack@khanacademy.org>
2014-03-21 14:40:36 -0700

    Fix toFraction
    Summary:
    Evidentally 283.33 % 1 = .3299999999999841
    
    Also, using our previous method, 1/1000 took 1000 steps to get to.
    
    This adds (a) a fix to that, (b) a worst case running time of 100 steps
    
    Anything beyond that is a useless fraction.
    
    It accounts for all fractions where n < d < 100
    ```
    for (var x = 1; x < 100; x++) {
      for (var y = x+1; y < 100; y++) {
        var z = KhanUtil.toFraction(x/y);
        if(x/y !== z[0]/z[1]) {
          console.log(x, y, z);
        }
      }
    }
    ```
    
    The first non-exact fraction is for d >= 141
    
    Test Plan:
    Try out:
    (1) 283.333333333333333
    (2) 283.33
    (3) .0001
    
    Reviewers: brianmerlob
    
    Reviewed By: brianmerlob
    
    Subscribers: alpert
    
    Differential Revision: http://phabricator.khanacademy.org/D7519

--- test/knumber.js
+++ test/knumber.js
@@ -35,4 +35,54 @@
         start();
     });
 
+    asyncTest('isInteger(-2.8) should be false', 1, function() {
+        strictEqual(number.isInteger(-2.8), false);
+        start();
+    });
+
+    asyncTest('isInteger(-2) should be true', 1, function() {
+        strictEqual(number.isInteger(-2), true);
+        start();
+    });
+
+    asyncTest('toFraction(-2) should be -2/1', 1, function() {
+        deepEqual(number.toFraction(-2), [-2, 1]);
+        start();
+    });
+
+    asyncTest('toFraction(-2.5) should be -5/2', 1, function() {
+        deepEqual(number.toFraction(-2.5), [-5, 2]);
+        start();
+    });
+
+    asyncTest('toFraction(2/3) should be 2/3', 1, function() {
+        deepEqual(number.toFraction(2/3), [2, 3]);
+        start();
+    });
+
+    asyncTest('toFraction(283.33...) should be 850/3', 1, function() {
+        deepEqual(number.toFraction(283 + 1/3), [850, 3]);
+        start();
+    });
+
+    asyncTest('toFraction(0) should be 0/1', 1, function() {
+        deepEqual(number.toFraction(0), [0, 1]);
+        start();
+    });
+
+    asyncTest('toFraction(pi) should be pi/1', 1, function() {
+        deepEqual(number.toFraction(Math.PI), [Math.PI, 1]);
+        start();
+    });
+
+    asyncTest('toFraction(0.66) should be 33/50', 1, function() {
+        deepEqual(number.toFraction(0.66), [33, 50]);
+        start();
+    });
+
+    asyncTest('toFraction(0.66, 0.01) should be 2/3', 1, function() {
+        deepEqual(number.toFraction(0.66, 0.01), [2, 3]);
+        start();
+    });
+
 })();
