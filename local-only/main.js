ecaf039022881c17d56965b45a140ff2d6655955
Ben Eater <eater@khanacademy.org>
2014-11-25 12:28:28 -0500

    Adjust another require dependency for local mode
    kas depends on underscore
    
    Test Plan: Mash ⌘R and notice a lack of JS errors on the console.
    
    Auditors: emily

--- local-only/main.js
+++ local-only/main.js
@@ -8,7 +8,6 @@ requirejs([
     "jquery",
     "../local-only/katex/katex.js",
     "../local-only/underscore.js",
-    "../local-only/kas.js",
     "../local-only/jed.js",
     "../local-only/localeplanet/icu." + getLang() + ".js"
 ], function($, katex) {
@@ -17,12 +16,13 @@ requirejs([
     // their return values
     window.katex = katex;
 
-    // These scripts depend on jQuery, so we wait to load them
+    // These scripts depend on jQuery or underscore, so we wait to load them
     requirejs([
         "../exercises-stub.js",
         "../local-only/jquery-migrate-1.1.1.js",
         "../local-only/jquery-ui.js",
         "../local-only/jquery.qtip.js",
+        "../local-only/kas.js",
         "../local-only/i18n.js"
     ], function() {
         requirejs([
