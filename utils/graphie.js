52402da052c96aa57522ab1825156074ba94c4bf
Kevin Barabash <kevinb@khanacademy.org>
2015-10-08 13:37:33 -0700

    Add processText function.
    Summary:
    This adds a `processText` function analogous to the
    `processMath` function which allows labels which aren't mathy to be
    re-rendered as non-mathy.
    
    This is used for graphie label jipt, so we can re-render text labels
    with the translated text.
    
    Test Plan:
     - Visit http://exercises.ka.local/exercises/division_1.html
     - Take all the hints.
     - See that the "We can make a row of n circles" and "How many circles
       are there if we make m rows?" labels look good.
     - In the console, run
       `label = KhanUtil.currentGraph.label([0, 0], "test", "center", false)`
     - See that a label appears over the division, centered on the left
       side.
     - Run `label.processText("this is a longer string")`, see that the new
       string is rendered and is appropriately centered.
    
    Reviewers: eater, emily
    
    Reviewed By: eater
    
    Subscribers: kevinb
    
    Differential Revision: https://phabricator.khanacademy.org/D21232

--- utils/graphie.js
+++ utils/graphie.js
@@ -653,10 +653,6 @@ KhanUtil.createGraphie = function(el) {
 
             var $span = $("<span>").addClass("graphie-label");
 
-            if (!latex) {
-                $span.html(text);
-            }
-
             var pad = currentStyle["label-distance"];
 
             // TODO(alpert): Isn't currentStyle applied afterwards
@@ -689,12 +685,17 @@ KhanUtil.createGraphie = function(el) {
                 });
             };
 
-            if (latex) {
-                $span.processMath(text, /* force */ false);
-            } else {
+            $span.processText = function(text) {
+                $span.html(text);
                 var width = span.scrollWidth;
                 var height = span.scrollHeight;
                 setLabelMargins(span, [width, height]);
+            };
+
+            if (latex) {
+                $span.processMath(text, /* force */ false);
+            } else {
+                $span.processText(text);
             }
 
             return $span;
